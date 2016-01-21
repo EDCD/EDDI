670ae714532a088209f54045998aafda579aafcf
mcdee <Jim@mcdee.net>
2016-01-21 14:53:24 +0000

    Start of coriolis integration

---
+++ EliteDangerousDataProviderVAPlugin/Coriolis.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliteDangerousDataProviderVAPlugin
+{
+    public class Coriolis
+    {
+        public static string ShipUri(Ship ship)
+        {
+            string uri = "http://coriolis.io/outfit/";
+            uri += ShipModel(ship.model);
+            uri += "/";
+            uri += ShipBulkheads(ship.bulkheads);
+            uri += ship.powerPlant;
+            uri += ship.thrusters;
+            uri += "5E4E7E6E5C---1717----04044j030t--002h.Iw18eQ==.Aw18eQ==";
+            return uri;
+        }
+
+        // Translate ship model to that used by coriolis
+        public static string ShipModel(string model)
+        {
+            return "python";
+        }
+
+        public static string ShipBulkheads(string bulkheads)
+        {
+            switch (bulkheads)
+            {
+                case "Lightweight Alloy":
+                    return "0";
+                case "Reinforced Alloy":
+                    return "1";
+                case "Military Grade Composite":
+                    return "2";
+                case "Mirrored Surface Composite":
+                    return "3";
+                case "Reactive Surface Composite":
+                    return "4";
+                default:
+                    return "0";
+            }
+        }
+    }
+}
---
+++ EliteDangerousDataProviderVAPlugin/Ship.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliteDangerousDataProviderVAPlugin
+{
+    public class Ship
+    {
+        public string model { get; set; }
+        public string bulkheads { get; set;  }
+        public decimal bulkheadsIntegrity { get; set; }
+        public string powerPlant { get; set; }
+        public decimal powerPlantIntegrity { get; set; }
+        public string thrusters { get; set; }
+        public int thrustersPriority { get; set; }
+        public bool thrustersEnabled { get; set; }
+        public decimal thrustersIntegrity { get; set; }
+    }
+}
