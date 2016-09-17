00d9e71674c780c6276b6d8f3c9b654f32295573
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2016-09-17 09:27:11 +0100

    Updates for correct location and name of journal.

--- EliteDangerousJournalMonitor/JournalMonitor.cs
+++ EliteDangerousJournalMonitor/JournalMonitor.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -18,7 +19,7 @@ namespace EliteDangerousJournalMonitor
     {
         private static Regex JsonRegex = new Regex(@"^{.*}$");
 
-        public JournalMonitor() : base(NetLogConfiguration.FromFile().path, @"^journal\.log$", result => HandleJournalEntry(result, Eddi.Instance.eventHandler)) {}
+        public JournalMonitor() : base(GetSavedGamesDir(), @"^journal\.[0-9\.]\.+log$", result => HandleJournalEntry(result, Eddi.Instance.eventHandler)) {}
 
         private static void HandleJournalEntry(string line, Action<Event> callback)
         {
@@ -485,5 +486,22 @@ namespace EliteDangerousJournalMonitor
         {
             return null;
         }
+
+        private static string GetSavedGamesDir()
+        {
+            IntPtr path;
+            int result = SHGetKnownFolderPath(new Guid("4C5C32FF-BB9D-43B0-B5B4-2D72E54EAAA4"), 0, new IntPtr(0), out path);
+            if (result >= 0)
+            {
+                return Marshal.PtrToStringUni(path) + @"\Frontier Developments\Elite Dangerous";
+            }
+            else
+            {
+                throw new ExternalException("Failed to find the saved games directory.", result);
+            }
+        }
+
+        [DllImport("Shell32.dll")]
+        private static extern int SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)]Guid rfid, uint dwFlags, IntPtr hToken, out IntPtr ppszPath);
     }
 }
