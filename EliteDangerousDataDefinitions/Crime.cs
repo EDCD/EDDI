e596bd988d01fce16c6e4edba62cd9dd10ffb3b9
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2016-09-19 20:21:06 +0100

    Update list of crimes.

--- EliteDangerousDataDefinitions/Crime.cs
+++ EliteDangerousDataDefinitions/Crime.cs
@@ -28,23 +28,23 @@ namespace EliteDangerousDataDefinitions
 
         public static readonly Crime None = new Crime("none", "None");
         public static readonly Crime Assault = new Crime("assault", "Assault");
-        public static readonly Crime Murder = new Crime("Murder", "Murder");
-        public static readonly Crime Piracy = new Crime("Piracy", "Piracy");
-        public static readonly Crime Interdiction = new Crime("Interdiction", "Interdiction");
-        public static readonly Crime IllegalCargo = new Crime("IllegalCargo", "Illegal cargo");
-        public static readonly Crime DisobeyPolice = new Crime("DisobeyPolice", "Disobey police");
+        public static readonly Crime Murder = new Crime("murder", "Murder");
+        public static readonly Crime Piracy = new Crime("piracy", "Piracy");
+        public static readonly Crime Interdiction = new Crime("interdiction", "Interdiction");
+        public static readonly Crime IllegalCargo = new Crime("illegalCargo", "Illegal cargo");
+        public static readonly Crime DisobeyPolice = new Crime("disobeyPolice", "Disobey police");
         public static readonly Crime FireInNoFireZone = new Crime("fireInNoFireZone", "Fire in no-fire zone");
         public static readonly Crime FireInStation = new Crime("fireInStation", "Fire in station");
-        public static readonly Crime DumpingDangerous = new Crime("DumpingDangerous", "Dumping dangerous cargo");
-        public static readonly Crime DumpingNearStation = new Crime("DumpingNearStation", "Dumping cargo near a station");
-        public static readonly Crime BlockingAirlockMinor = new Crime("DockingMinor_BlockingAirlock", "Blocking an airlock (minor)");
-        public static readonly Crime BlockingAirlockMajor = new Crime("DockingMajor_BlockingAirlock", "Blocking an airlock (major)");
-        public static readonly Crime BlockingLandingPadMinor = new Crime("DockingMinor_BlockingLandingPad", "Blocking a landing pad (minor)");
-        public static readonly Crime BlockingLandingPadMajor = new Crime("DockingMajor_BlockingLandingPad", "Blocking a landing pad (major)");
-        public static readonly Crime TrespassMinor = new Crime("DockingMinor_Trespass", "Trespass (minor)");
-        public static readonly Crime TrespassMajor = new Crime("DockingMajor_Trespass", "Trespass (major)");
-        public static readonly Crime Collided = new Crime("CollidedAtSpeedInNoFireZone", "Collided");
-        public static readonly Crime CollidedWithDamage = new Crime("CollidedAtSpeedInNoFireZone_HullDamage", "Collided with damage");
+        public static readonly Crime DumpingDangerous = new Crime("dumpingDangerous", "Dumping dangerous cargo");
+        public static readonly Crime DumpingNearStation = new Crime("dumpingNearStation", "Dumping cargo near a station");
+        public static readonly Crime BlockingAirlockMinor = new Crime("dockingMinor_BlockingAirlock", "Blocking an airlock (minor)");
+        public static readonly Crime BlockingAirlockMajor = new Crime("dockingMajor_BlockingAirlock", "Blocking an airlock (major)");
+        public static readonly Crime BlockingLandingPadMinor = new Crime("dockingMinor_BlockingLandingPad", "Blocking a landing pad (minor)");
+        public static readonly Crime BlockingLandingPadMajor = new Crime("dockingMajor_BlockingLandingPad", "Blocking a landing pad (major)");
+        public static readonly Crime TrespassMinor = new Crime("dockingMinor_Trespass", "Trespass (minor)");
+        public static readonly Crime TrespassMajor = new Crime("dockingMajor_Trespass", "Trespass (major)");
+        public static readonly Crime Collided = new Crime("collidedAtSpeedInNoFireZone", "Collided");
+        public static readonly Crime CollidedWithDamage = new Crime("collidedAtSpeedInNoFireZone_HullDamage", "Collided with damage");
 
         public static Crime FromName(string from)
         {
