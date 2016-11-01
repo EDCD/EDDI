8fd0e6356cc603db7953ced7782cf1d0a1ee8e83
Michael Werle <micha@michaelwerle.com>
2016-11-01 22:50:28 +0000

    Add comprehensive body-scan unit test.

--- Tests/JournalMonitorTests.cs
+++ Tests/JournalMonitorTests.cs
@@ -18,6 +18,37 @@ namespace Tests
         }
 
         [TestMethod]
+        public void TestJournalPlanetScan1()
+        {
+            string line = @"{ ""timestamp"":""2016 - 11 - 01T18: 49:07Z"", ""event"":""Scan"", ""BodyName"":""Grea Bloae HH-T d4-44 4"", ""DistanceFromArrivalLS"":703.763611, ""TidalLock"":false, ""TerraformState"":""Terraformable"", ""PlanetClass"":""High metal content body"", ""Atmosphere"":""hot thick carbon dioxide atmosphere"", ""Volcanism"":""minor metallic magma volcanism"", ""MassEM"":2.171783, ""Radius"":7622170.500000, ""SurfaceGravity"":14.899396, ""SurfaceTemperature"":836.165466, ""SurfacePressure"":33000114.000000, ""Landable"":false, ""SemiMajorAxis"":210957926400.000000, ""Eccentricity"":0.000248, ""OrbitalInclination"":0.015659, ""Periapsis"":104.416656, ""OrbitalPeriod"":48801056.000000, ""RotationPeriod"":79442.242188 }";
+            Event theEvent = JournalMonitor.ParseJournalEntry(line);
+            Assert.IsNotNull(theEvent);
+            //Assert.IsInstanceOfType(theEvent, BodyScannedEvent);
+            BodyScannedEvent ev = theEvent as BodyScannedEvent;
+            Assert.IsNotNull(ev);
+            Assert.AreEqual(ev.name, "Grea Bloae HH-T d4-44 4");
+            Assert.AreEqual(ev.distancefromarrival, (decimal)703.763611);
+            Assert.IsFalse(ev.tidallylocked);
+            Assert.AreEqual(ev.terraformstate, "Terraformable");
+            Assert.AreEqual(ev.bodyclass, "High metal content body");
+            Assert.AreEqual(ev.atmosphere, "hot thick carbon dioxide atmosphere");
+            Assert.AreEqual(ev.volcanism, "minor metallic magma volcanism");
+            //Assert.IsTrue(ev.massEM == (decimal)2.171783)
+            //Assert.IsTrue(ev.radius  == (decimal)7622170.500000);
+            Assert.AreEqual(ev.gravity, (decimal)14.899396);
+            Assert.AreEqual(ev.temperature, (decimal)836.165466);
+            Assert.AreEqual(ev.pressure, (decimal)33000114.000000);
+            Assert.IsFalse(ev.landable);
+            Assert.AreEqual(ev.semimajoraxis, (decimal)210957926400.000000);
+            Assert.AreEqual(ev.eccentricity, (decimal)0.000248);
+            Assert.AreEqual(ev.orbitalinclination, (decimal)0.015659);
+            Assert.AreEqual(ev.periapsis, (decimal)104.416656);
+            Assert.AreEqual(ev.orbitalperiod, (decimal)48801056.000000);
+            Assert.AreEqual(ev.rotationperiod, (decimal)79442.242188);
+            Console.WriteLine(JsonConvert.SerializeObject(theEvent));
+        }
+
+        [TestMethod]
         public void TestJournalStarScan1()
         {
             string line = @"{ ""timestamp"":""2016-10-27T08:51:23Z"", ""event"":""Scan"", ""BodyName"":""Vela Dark Region FG-Y d3"", ""DistanceFromArrivalLS"":0.000000, ""StarType"":""K"", ""StellarMass"":0.960938, ""Radius"":692146368.000000, ""AbsoluteMagnitude"":5.375961, ""Age_MY"":230, ""SurfaceTemperature"":5108.000000, ""RotationPeriod"":393121.093750, ""Rings"":[ { ""Name"":""Vela Dark Region FG-Y d3 A Belt"", ""RingClass"":""eRingClass_MetalRich"", ""MassMT"":1.2262e+10, ""InnerRad"":1.2288e+09, ""OuterRad"":2.3812e+09 } ] }";
