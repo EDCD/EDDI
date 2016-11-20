167393ddf7c3de10f7789f87d00478cf655a1df1
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2016-11-20 07:32:33 +0000

    Obtain details on default browser

--- Utilities/Net.cs
+++ Utilities/Net.cs
@@ -114,16 +114,16 @@ namespace Utilities
 
             try
             {
-                //Read default browser path from userChoiceLKey
+                // Read default browser path from userChoiceLKey
                 userChoiceKey = Registry.CurrentUser.OpenSubKey(urlAssociation + @"\UserChoice", false);
 
-                //If user choice was not found, try machine default
+                // If user choice was not found, try machine default
                 if (userChoiceKey == null)
                 {
-                    //Read default browser path from Win XP registry key
+                    // Read default browser path from Win XP registry key
                     var browserKey = Registry.ClassesRoot.OpenSubKey(@"HTTP\shell\open\command", false);
 
-                    //If browser path wasn’t found, try Win Vista (and newer) registry key
+                    // If browser path wasn’t found, try Win Vista (and newer) registry key
                     if (browserKey == null)
                     {
                         browserKey =
@@ -132,6 +132,7 @@ namespace Utilities
                     }
                     var path = CleanifyBrowserPath(browserKey.GetValue(null) as string);
                     browserKey.Close();
+                    Logging.Debug("Browser path (1) is " + path);
                     return path;
                 }
                 else
@@ -145,13 +146,14 @@ namespace Utilities
                     var kp = Registry.ClassesRoot.OpenSubKey(concreteBrowserKey, false);
                     browserPath = CleanifyBrowserPath(kp.GetValue(null) as string);
                     kp.Close();
+                    Logging.Debug("Browser path (2) is " + browserPath);
                     return browserPath;
                 }
             }
             catch (Exception ex)
             {
                 Logging.Error("Failed to find default browser: ", ex);
-                return "exporer.exe";
+                return "explorer.exe";
             }
         }
 
