43c5e0101a990da1ff8d31f054c551f9976596d4
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2016-11-28 09:11:20 +0000

    Fix documentation for VoiceAttack ratings

---
+++ Events/MarketInformationUpdatedEvent.cs
@@ -0,0 +1,28 @@
+﻿using EddiDataDefinitions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace EddiEvents
+{
+    public class MarketInformationUpdatedEvent : Event
+    {
+        public const string NAME = "Market information updated";
+        public const string DESCRIPTION = "Triggered when market information for the currently docked station has been updated";
+        public static MarketInformationUpdatedEvent SAMPLE = new MarketInformationUpdatedEvent(DateTime.Now);
+
+        public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
+
+        static MarketInformationUpdatedEvent()
+        {
+        }
+
+        public MarketInformationUpdatedEvent(DateTime timestamp) : base(timestamp, NAME)
+        {
+        }
+    }
+}
