155edd344e9be0c6574ae238a2cf5a92d31ece8a
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2016-11-28 20:20:54 +0000

    Bump version number.

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -12,7 +12,7 @@ namespace Utilities
     public class Constants
     {
         public const string EDDI_NAME = "EDDI";
-        public const string EDDI_VERSION = "2.0.12";
+        public const string EDDI_VERSION = "2.0.13";
         public static readonly string DATA_DIR = Environment.GetEnvironmentVariable("AppData") + "\\" + EDDI_NAME;
 
         public const string ENVIRONMENT_WITCH_SPACE = "Witch space";
