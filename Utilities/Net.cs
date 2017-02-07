f6f9c294eb10f9fd560757c18f6b5f924c09f10d
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2017-02-07 17:11:42 +0000

    Pre-release tidy-ups

--- Utilities/Net.cs
+++ Utilities/Net.cs
@@ -92,7 +92,7 @@ namespace Utilities
                 else
                 {
                     Logging.Warn("Bad response, error code " + wex.Status);
-                    throw wex;
+                    throw;
                 }
             }
             Logging.Debug("Response is " + JsonConvert.SerializeObject(response));
