d1ff423422b1569e5bb273d37708d8b776460161
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2017-02-22 22:09:04 +0000

    Tidy-ups

--- Events/MessageReceivedEvent.cs
+++ Events/MessageReceivedEvent.cs
@@ -11,7 +11,7 @@ namespace EddiEvents
     {
         public const string NAME = "Message received";
         public const string DESCRIPTION = "Triggered when you receive a message";
-        public const string SAMPLE = "{\"timestamp\":\"2016-06-10T14:32:03Z\",\"event\":\"ReceiveText\",\"From\":\"HRC1\",\"Message\":\"Hello\"}";
+        public const string SAMPLE = "{\"timestamp\":\"2016-06-10T14:32:03Z\",\"event\":\"ReceiveText\",\"From\":\"$cmdr_decorate:#name=HRC1;\",\"Message\":\"Hello\"}";
         public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
 
         static MessageReceivedEvent()
--- Events/TradeVoucherRedeemedEvent.cs
+++ Events/TradeVoucherRedeemedEvent.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 
 namespace EddiEvents
 {
-    public class BondRedeemedEvent : Event
+    public class TradeVoucherRedeemedEvent : Event
     {
-        public const string NAME = "Bond redeemed";
-        public const string DESCRIPTION = "Triggered when you redeem a bond voucher";
-        public const string SAMPLE = @"{ ""timestamp"":""2016-12-29T10:10:11Z"", ""event"":""RedeemVoucher"", ""Type"":""CombatBond"",""Faction"":""The Pilots Federation"",""Amount"":1000 }";
+        public const string NAME = "Trade voucher redeemed";
+        public const string DESCRIPTION = "Triggered when you redeem a trade voucher";
+        public const string SAMPLE = @"{ ""timestamp"":""2016-12-29T10:10:11Z"", ""event"":""RedeemVoucher"", ""Type"":""trade"",""Faction"":""The Pilots Federation"",""Amount"":1000 }";
 
         public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
 
-        static BondRedeemedEvent()
+        static TradeVoucherRedeemedEvent()
         {
-            VARIABLES.Add("faction", "The name of the faction who issued the bond");
+            VARIABLES.Add("faction", "The name of the faction who issued the trade voucher");
             VARIABLES.Add("amount", "The amount rewarded (after any broker fees)");
         }
 
@@ -27,7 +27,7 @@ namespace EddiEvents
         [JsonProperty("amount")]
         public long amount { get; private set; }
 
-        public BondRedeemedEvent(DateTime timestamp, string faction, long amount) : base(timestamp, NAME)
+        public TradeVoucherRedeemedEvent(DateTime timestamp, string faction, long amount) : base(timestamp, NAME)
         {
             this.faction = faction;
             this.amount = amount;
