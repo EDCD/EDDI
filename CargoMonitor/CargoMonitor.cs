edccef072a71d3941aa8f33ce324e8609b05e46d
cmdrmcdonald <Cmdr.McDonald@gmail.com>
2017-02-28 14:52:05 +0000

    Add cargo monitor skeleton

---
+++ CargoMonitor/CargoMonitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EddiCargoMonitor
+{
+    /// <summary>
+    /// A monitor that keeps track of cargo
+    /// </summary>
+    public class CargoMonitor
+    {
+    }
+}
