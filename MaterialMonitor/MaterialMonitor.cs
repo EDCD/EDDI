5cb9396f31f38ad4f1d9af1e08d968e1a3097d57
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2017-03-03 11:54:01 +0000

    Add material monitor configuation window.

--- MaterialMonitor/MaterialMonitor.cs
+++ MaterialMonitor/MaterialMonitor.cs
@@ -9,6 +9,7 @@ using EddiEvents;
 using Utilities;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace EddiMaterialMonitor
 {
@@ -20,8 +21,8 @@ namespace EddiMaterialMonitor
         // The file to log speech
         public static readonly string LogFile = Constants.DATA_DIR + @"\materialsresponder.out";
 
-        private MaterialMonitorConfiguration configuration;
-        private IDictionary<string, MaterialAmount> inventory;
+        // Observable collection for us to handle
+        public ObservableCollection<MaterialAmount> inventory = new ObservableCollection<MaterialAmount>();
 
         public string MonitorName()
         {
@@ -40,54 +41,9 @@ namespace EddiMaterialMonitor
 
         public MaterialMonitor()
         {
-            configuration = MaterialMonitorConfiguration.FromFile();
-            // Easier for us to use this internally as a dictionary
-            inventory = configuration.materials.ToDictionary(m => m.material);
-            
-            //if (configuration != null && configuration.Personality != null)
-            //{
-            //    personality = Personality.FromName(configuration.Personality);
-            //}
-            //if (personality == null)
-            //{ 
-            //    personality = Personality.Default();
-            //}
-            //scriptResolver = new ScriptResolver(personality.Scripts);
-            //subtitles = configuration.Subtitles;
-            //subtitlesOnly = configuration.SubtitlesOnly;
+            readMaterials();
             Logging.Info("Initialised " + MonitorName() + " " + MonitorVersion());
         }
-
-        ///// <summary>
-        ///// Change the personality for the speech responder
-        ///// </summary>
-        ///// <returns>true if the speech responder is now using the new personality, otherwise false</returns>
-        //public bool SetPersonality(string newPersonality)
-        //{
-        //    SpeechResponderConfiguration configuration = SpeechResponderConfiguration.FromFile();
-        //    if (newPersonality == configuration.Personality)
-        //    {
-        //        // Already set to this personality
-        //        return true;
-        //    }
-
-        //    // Ensure that this personality exists
-        //    Personality personality = Personality.FromName(newPersonality);
-        //    if (personality != null)
-        //    {
-        //        // Yes it does; use it
-        //        configuration.Personality = newPersonality;
-        //        configuration.ToFile();
-        //        scriptResolver = new ScriptResolver(personality.Scripts);
-        //        Logging.Debug("Changed personality to " + newPersonality);
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        // No it does not; ignore it
-        //        return false;
-        //    }
-        //}
 
         public void Start()
         {
@@ -100,8 +56,7 @@ namespace EddiMaterialMonitor
 
         public void Reload()
         {
-            configuration = MaterialMonitorConfiguration.FromFile();
-            inventory = configuration.materials.ToDictionary(m => m.material);
+            readMaterials();
             Logging.Info("Reloaded " + MonitorName() + " " + MonitorVersion());
         }
 
@@ -119,7 +74,7 @@ namespace EddiMaterialMonitor
             {
                 handleMaterialInventoryEvent((MaterialInventoryEvent)@event);
             }
-            else if (@event is MaterialDiscardedEvent)
+            else if (@event is MaterialCollectedEvent)
             {
                 handleMaterialCollectedEvent((MaterialCollectedEvent)@event);
             }
@@ -151,11 +106,11 @@ namespace EddiMaterialMonitor
             }
 
             // Also remove any items for which we have neither inventory nor limits
-            inventory = inventory.Where(i => (i.Value.amount != 0 || i.Value.desired.HasValue || i.Value.minimum.HasValue || i.Value.maximum.HasValue)).ToDictionary(i => i.Key, i => i.Value);
+            // Later
+            //inventory new ObservableCollection<MaterialAmount>(inventory.Where(i => (i.amount != 0 || i.desired.HasValue || i.minimum.HasValue || i.maximum.HasValue)));
 
             // Update configuration information
-            configuration.materials = inventory.Values.ToList();
-            configuration.ToFile();
+            writeMaterials();
         }
 
         private void handleMaterialCollectedEvent(MaterialCollectedEvent @event)
@@ -195,16 +150,17 @@ namespace EddiMaterialMonitor
         private void incMaterial(string name, int amount)
         {
             Material material = Material.FromName(name);
-            MaterialAmount ma;
-            if (!inventory.TryGetValue(name, out ma))
+            MaterialAmount ma = inventory.Where(inv => inv.material == material.name).FirstOrDefault();
+            if (ma == null)
             {
                 // No information for the current material - create one and set it to 0
                 ma = new MaterialAmount(material, 0);
+                inventory.Add(ma);
             }
+
             int previous = ma.amount;
-            ma.amount -= amount;
-            Logging.Debug(name + ": " + previous + "->" + ma.amount);
-            inventory[name] = ma;
+            ma.amount += amount;
+            Logging.Debug(ma.material + ": " + previous + "->" + ma.amount);
 
             if (ma.maximum.HasValue)
             {
@@ -223,9 +179,7 @@ namespace EddiMaterialMonitor
                 }
             }
 
-            // Update configuration information
-            configuration.materials = inventory.Values.ToList();
-            configuration.ToFile();
+            writeMaterials();
         }
 
         /// <summary>
@@ -234,16 +188,17 @@ namespace EddiMaterialMonitor
         private void decMaterial(string name, int amount)
         {
             Material material = Material.FromName(name);
-            MaterialAmount ma;
-            if (!inventory.TryGetValue(name, out ma))
+            MaterialAmount ma = inventory.Where(inv => inv.material == material.name).FirstOrDefault();
+            if (ma == null)
             {
                 // No information for the current material - create one and set it to amount
                 ma = new MaterialAmount(material, amount);
+                inventory.Add(ma);
             }
+
             int previous = ma.amount;
             ma.amount -= amount;
-            Logging.Debug(name + ": " + previous + "->" + ma.amount);
-            inventory[name] = ma;
+            Logging.Debug(ma.material + ": " + previous + "->" + ma.amount);
 
             // We have limits for this material; carry out relevant checks
             if (ma.minimum.HasValue)
@@ -262,9 +217,8 @@ namespace EddiMaterialMonitor
                     EDDI.Instance.eventHandler(new MaterialThresholdEvent(DateTime.Now, Material.FromName(name), "Desired", (int)ma.desired, ma.amount, "Decrease"));
                 }
             }
-            // Update configuration information
-            configuration.materials = inventory.Values.ToList();
-            configuration.ToFile();
+
+            writeMaterials();
         }
 
         /// <summary>
@@ -273,12 +227,13 @@ namespace EddiMaterialMonitor
         private void setMaterial(string name, int amount)
         {
             Material material = Material.FromName(name);
-            MaterialAmount ma;
-            if (!inventory.TryGetValue(name, out ma))
+            MaterialAmount ma = inventory.Where(inv => inv.material == material.name).FirstOrDefault();
+            if (ma == null)
             {
                 // No information for the current material - create one and set it to amount
                 ma = new MaterialAmount(material, amount);
-                inventory[name] = ma;
+                Logging.Debug(ma.material + ": " + ma.amount);
+                inventory.Add(ma);
             }
             ma.amount = amount;
         }
@@ -286,9 +241,54 @@ namespace EddiMaterialMonitor
         public IDictionary<string, object> GetVariables()
         {
             IDictionary<string, object> variables = new Dictionary<string, object>();
-            variables["materials"] = configuration.materials;
+            variables["materials"] = inventory;
 
             return variables;
+        }
+
+        public void writeMaterials()
+        {
+            // Write material configuration with current inventory
+            MaterialMonitorConfiguration configuration = new MaterialMonitorConfiguration();
+            configuration.materials = inventory;
+            configuration.ToFile();
+        }
+
+        private void readMaterials()
+        {
+            // Obtain current inventory from  configuration
+            MaterialMonitorConfiguration configuration = MaterialMonitorConfiguration.FromFile();
+
+            // Build a new inventory
+            List<MaterialAmount> newInventory = new List<MaterialAmount>();
+
+            // Start with the materials we have in the log
+            foreach (MaterialAmount ma in configuration.materials)
+            {
+                newInventory.Add(ma);
+            }
+
+            // Add in any new materials
+            foreach (Material material in Material.MATERIALS)
+            {
+                MaterialAmount ma = newInventory.Where(inv => inv.material == material.name).FirstOrDefault();
+                if (ma == null)
+                {
+                    // We don't have this one - add it
+                    ma = new MaterialAmount(material, 0);
+                    newInventory.Add(ma);
+                }
+            }
+
+            // Now order the list by name
+            newInventory = newInventory.OrderBy(m => m.material).ToList();
+
+            // Update the inventory 
+            inventory.Clear();
+            foreach (MaterialAmount ma in newInventory)
+            {
+                inventory.Add(ma);
+            }
         }
     }
 }
