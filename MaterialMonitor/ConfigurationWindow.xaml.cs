a05bf74e49b769aaf8ed48e07bf0a69ab767b258
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2017-03-03 14:42:27 +0000

    Link configuration window to material monitor.

--- MaterialMonitor/ConfigurationWindow.xaml.cs
+++ MaterialMonitor/ConfigurationWindow.xaml.cs
@@ -25,4 +25,39 @@ namespace EddiMaterialMonitor
             monitor.writeMaterials();
         }
     }
+
+    public class DataGridNumericColumn : DataGridTextColumn
+    {
+        protected override object PrepareCellForEdit(FrameworkElement editingElement, RoutedEventArgs editingEventArgs)
+        {
+            TextBox edit = editingElement as TextBox;
+            edit.PreviewTextInput += OnPreviewTextInput;
+
+            return base.PrepareCellForEdit(editingElement, editingEventArgs);
+        }
+
+        private void OnPaste(object sender, DataObjectPastingEventArgs e)
+        {
+            var data = e.SourceDataObject.GetData(DataFormats.Text);
+            if (!IsDataValid(data)) e.CancelCommand();
+        }
+
+        void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsDataValid(e.Text);
+        }
+
+        bool IsDataValid(object data)
+        {
+            try
+            {
+                Convert.ToInt32(data);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
 }
