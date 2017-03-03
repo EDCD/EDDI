46e9eacc3ea5e17cc262cc965bfde80702c637d0
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2017-03-03 11:54:01 +0000

    Add material monitor configuation window.

--- MaterialMonitor/ConfigurationWindow.xaml.cs
+++ MaterialMonitor/ConfigurationWindow.xaml.cs
@@ -1,23 +1,6 @@
-<<<<<<< 7cbcc8de1cbcbf428f25b18ac4a1658a1f0ba9cf
 ﻿using Eddi;
 using System.Windows.Controls;
 using System.Windows.Data;
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
->>>>>>> Add material monitor.
 
 namespace EddiMaterialMonitor
 {
@@ -26,7 +9,6 @@ namespace EddiMaterialMonitor
     /// </summary>
     public partial class ConfigurationWindow : UserControl
     {
-<<<<<<< 7cbcc8de1cbcbf428f25b18ac4a1658a1f0ba9cf
         MaterialMonitor monitor;
 
         public ConfigurationWindow()
@@ -41,11 +23,6 @@ namespace EddiMaterialMonitor
         {
             // Update the material monitor's information
             monitor.writeMaterials();
-=======
-        public ConfigurationWindow()
-        {
-            InitializeComponent();
->>>>>>> Add material monitor.
         }
     }
 }
