fd4456286a72da56412bb0e3eae935ff261a9c16
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2017-03-05 19:33:44 +0000

    Update loadout event

--- DataDefinitions/Hardpoint.cs
+++ DataDefinitions/Hardpoint.cs
@@ -3,6 +3,8 @@
     /// <summary>An external mounting on a ship</summary>
     public class Hardpoint
     {
+        /// <summary>The name of the hardpoint</summary>
+        public string name { get; set; }
         /// <summary>The size of the hardpoint</summary
         public int size { get; set; }
         /// <summary>The module residing on the hardpoint (can be null)</summary>
