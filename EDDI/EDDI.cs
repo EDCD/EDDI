2eacc0a22b452c4b1447a5ae2a91d57b843a46b8
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2017-03-08 20:06:03 +0000

    Enforce playercontrolled in Touchdown event.

--- EDDI/EDDI.cs
+++ EDDI/EDDI.cs
@@ -1068,24 +1068,16 @@ namespace Eddi
 
         private bool eventEnteredSupercruise(EnteredSupercruiseEvent theEvent)
         {
-            if (Environment == null || Environment != Constants.ENVIRONMENT_SUPERCRUISE)
-            {
-                Environment = Constants.ENVIRONMENT_SUPERCRUISE;
-                updateCurrentSystem(theEvent.system);
-                return true;
-            }
-            return false;
+            Environment = Constants.ENVIRONMENT_SUPERCRUISE;
+            updateCurrentSystem(theEvent.system);
+            return true;
         }
 
         private bool eventEnteredNormalSpace(EnteredNormalSpaceEvent theEvent)
         {
-            if (Environment == null || Environment != Constants.ENVIRONMENT_NORMAL_SPACE)
-            {
-                Environment = Constants.ENVIRONMENT_NORMAL_SPACE;
-                updateCurrentSystem(theEvent.system);
-                return true;
-            }
-            return false;
+            Environment = Constants.ENVIRONMENT_NORMAL_SPACE;
+            updateCurrentSystem(theEvent.system);
+            return true;
         }
 
         private bool eventShipDelivered(ShipDeliveredEvent theEvent)
--- JournalMonitor/JournalMonitor.cs
+++ JournalMonitor/JournalMonitor.cs
@@ -98,7 +98,10 @@ namespace EddiJournalMonitor
                                 decimal latitude = getDecimal(data, "Latitude");
                                 decimal longitude = getDecimal(data, "Longitude");
                                 bool? playercontrolled = getOptionalBool(data, "PlayerControlled");
-                                journalEvent = new TouchdownEvent(timestamp, longitude, latitude, playercontrolled);
+                                // Default player controlled to true
+                                if (playercontrolled == null) { playercontrolled = true; }
+
+                                journalEvent = new TouchdownEvent(timestamp, longitude, latitude, (bool)playercontrolled);
                             }
                             handled = true;
                             break;
