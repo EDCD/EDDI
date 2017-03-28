69545af619c6a5831d3406fd2862653832bc4572
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2017-03-28 12:57:25 +0100

    Updates for voucher redeemed events

--- Events/BondRedeemedEvent.cs
+++ Events/BondRedeemedEvent.cs
@@ -11,25 +11,25 @@ namespace EddiEvents
     {
         public const string NAME = "Bond redeemed";
         public const string DESCRIPTION = "Triggered when you redeem a combat bond";
-        public const string SAMPLE = @"{ ""timestamp"":""2016-12-29T10:10:11Z"", ""event"":""RedeemVoucher"", ""Type"":""CombatBond"",""Faction"":""The Pilots Federation"",""Amount"":1000 }";
+        public const string SAMPLE = @"{ ""timestamp"":""2016-12-29T10:10:11Z"", ""event"":""RedeemVoucher"", ""Type"":""CombatBond"",""Amount"":2000,""Factions"":[{""Faction"":""The Pilots Federation"",""Amount"":1000},{""Faction"":""The Dark Wheel"",""Amount"":500},{""Faction"":""Los Chupacabras"",""Amount"":500}]}";
 
         public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
 
         static BondRedeemedEvent()
         {
-            VARIABLES.Add("faction", "The name of the faction who issued the bond");
+            VARIABLES.Add("rewards", "The rewards obtained broken down by faction");
             VARIABLES.Add("amount", "The amount rewarded (after any broker fees)");
         }
 
-        [JsonProperty("faction")]
-        public string faction { get; private set; }
+        [JsonProperty("rewards")]
+        public List<Reward> rewards { get; private set; }
 
         [JsonProperty("amount")]
         public long amount { get; private set; }
 
-        public BondRedeemedEvent(DateTime timestamp, string faction, long amount) : base(timestamp, NAME)
+        public BondRedeemedEvent(DateTime timestamp, List<Reward> rewards, long amount) : base(timestamp, NAME)
         {
-            this.faction = faction;
+            this.rewards = rewards;
             this.amount = amount;
         }
     }
--- Events/DataVoucherRedeemed.cs
+++ Events/DataVoucherRedeemed.cs
@@ -11,25 +11,25 @@ namespace EddiEvents
     {
         public const string NAME = "Data voucher redeemed";
         public const string DESCRIPTION = "Triggered when you redeem a data voucher";
-        public const string SAMPLE = @"{ ""timestamp"":""2016-12-29T10:10:11Z"", ""event"":""RedeemVoucher"", ""Type"":""settlement"",""Faction"":""The Pilots Federation"",""Amount"":1000 }";
+        public const string SAMPLE = @"{ ""timestamp"":""2016-12-29T10:10:11Z"", ""event"":""RedeemVoucher"", ""Type"":""settlement"",""Amount"":1000,""Factions"":[{""Faction"":""The Pilots Federation"",""Amount"":1000}] }";
 
         public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
 
         static DataVoucherRedeemedEvent()
         {
-            VARIABLES.Add("faction", "The name of the faction who issued the data voucher");
+            VARIABLES.Add("rewards", "The rewards obtained broken down by faction");
             VARIABLES.Add("amount", "The amount rewarded (after any broker fees)");
         }
 
-        [JsonProperty("faction")]
-        public string faction { get; private set; }
+        [JsonProperty("rewards")]
+        public List<Reward> rewards { get; private set; }
 
         [JsonProperty("amount")]
         public long amount { get; private set; }
 
-        public DataVoucherRedeemedEvent(DateTime timestamp, string faction, long amount) : base(timestamp, NAME)
+        public DataVoucherRedeemedEvent(DateTime timestamp, List<Reward> rewards, long amount) : base(timestamp, NAME)
         {
-            this.faction = faction;
+            this.rewards = rewards;
             this.amount = amount;
         }
     }
