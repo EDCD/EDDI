64ffa62c0cf9bbfd60524468d7cae89068e1d43d
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2017-04-03 16:40:53 +0100

    Skeleton of cargo monitor

--- Events/CargoInventoryEvent.cs
+++ Events/CargoInventoryEvent.cs
@@ -12,7 +12,7 @@ namespace EddiEvents
     {
         public const string NAME = "Cargo inventory";
         public const string DESCRIPTION = "Triggered when you obtain an inventory of your cargo";
-        public const string SAMPLE = @"{ ""timestamp"":""2017-03-31T10:38:23Z"", ""event"":""Cargo"", ""Inventory"":[ { ""Name"":""fish"", ""Count"":64 } ] }";
+        public const string SAMPLE = @"{ ""timestamp"":""2017-04-03T16:15:32Z"", ""event"":""Cargo"", ""Inventory"":[ { ""Name"":""fish"", ""Count"":10 }, { ""Name"":""beer"", ""Count"":12 }, { ""Name"":""tobacco"", ""Count"":8 }, { ""Name"":""drones"", ""Count"":20 } ] }";
         public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
 
         static CargoInventoryEvent()
