53c7cc5c0d2052942701a4587a24a91a79b737ba
Tkael <dragoncharmer84@hotmail.com>
2017-08-17 22:06:53 -0700

    Create ShipSoldOnRebuyEvent.cs

---
+++ ShipMonitor/ShipSoldOnRebuyEvent.cs
@@ -0,0 +1,47 @@
+using EddiDataDefinitions;
+using EddiEvents;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EddiShipMonitor
+{
+    public class ShipSoldOnRebuyEvent : Event
+    {
+        public const string NAME = "Ship sold on rebuy";
+        public const string DESCRIPTION = "Triggered when you sell a ship to raise funds on the insurance / rebuy screen";
+        public const string SAMPLE = "{ "timestamp":"2017-07-20T08:56:39Z", "event":"SellShipOnRebuy", "ShipType":"Dolphin", "System":"Shinrarta Dezhra", "SellShipId":4, "ShipPrice":4110183 }";
+        public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
+
+        static ShipSoldOnRebuyEvent()
+        {
+            VARIABLES.Add("ship", "The ship that was sold");
+            VARIABLES.Add("system", "The system where the ship was sold");            
+            VARIABLES.Add("shipid", "The ID of the ship that was sold");
+            VARIABLES.Add("price", "The price for which the ship was sold");
+        }
+
+        [JsonProperty("ship")]
+        public string ship { get; private set; }
+
+        [JsonProperty("system")]
+        public string system { get; private set; }
+
+        [JsonProperty("shipid")]
+        public int? shipid { get; private set; }
+
+        [JsonProperty("price")]
+        public long price { get; private set; }
+
+        public ShipSoldEvent(DateTime timestamp, string ship, string system, int shipId, long price) : base(timestamp, NAME)
+        {
+            this.ship = ship;
+            this.system = system;            
+            this.shipid = shipId;
+            this.price = price;
+        }
+    }
+}
