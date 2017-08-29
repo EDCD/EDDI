d06bf60c8c1e0cec0a23e81d8cb000d6d5aee5dd
Syrrianyl <31447889+Syrrianyl@users.noreply.github.com>
2017-08-29 20:38:58 +0200

    Translation and not exactly the same

--- DataDefinitions/Role.cs
+++ DataDefinitions/Role.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,43 +14,45 @@ namespace EddiDataDefinitions
     {
         public static readonly List<string> ROLES = new List<string>();
 
-        public static readonly string Assassination = "Assassination";
-        public static readonly string BountyHunting = "Bounty hunting";
+        public static readonly string Assassination = "Assassinat";
+        public static readonly string BountyHunting = "Chasse à prime";
         public static readonly string Combat = "Combat";
-        public static readonly string CombatSupport = "Combat support";
+        public static readonly string CombatSupport = "Support au combat";
         public static readonly string Exploration = "Exploration";
-        public static readonly string Journalism = "Journalism";
-        public static readonly string Mining = "Mining";
-        public static readonly string MultiCrew = "MultiCrew";
-        public static readonly string MultiPurpose = "Multi-purpose";
-        public static readonly string Piracy = "Piracy";
-        public static readonly string Racing = "Racing";
-        public static readonly string Refueling = "Refueling";
-        public static readonly string Science = "Science";
-        public static readonly string SearchAndRescue = "Search and rescue";
-        public static readonly string Smuggling = "Smuggling";
-        public static readonly string Trading = "Trading";
+        public static readonly string Mining = "Minage";
+        public static readonly string MultiPurpose = "Multi-rôle";
+        public static readonly string Piracy = "Piratage";
+        public static readonly string Smuggling = "Contrebande";
+        public static readonly string Trading = "Commerce";
         public static readonly string Taxi = "Taxi";
+        public static readonly string Medicorps = "Médi-Corps";
+        public static readonly string Fuel = "Fuel R.A.T.S.";
+        public static readonly string Press = "Photo/Presse";
+        public static readonly string Brothel = "Lupanar de luxe";
+        public static readonly string Course = "Course";
+        public static readonly string SaveRescue = "Sauvetage - Secours";
+	
+		
 
         static Role()
         {
             ROLES.Add(Assassination);
             ROLES.Add(BountyHunting);
             ROLES.Add(Combat);
-            ROLES.Add(CombatSupport);
-            ROLES.Add(Exploration);
-            ROLES.Add(Journalism);
-            ROLES.Add(Mining);
-            ROLES.Add(MultiCrew);
-            ROLES.Add(MultiPurpose);
-            ROLES.Add(Piracy);
-            ROLES.Add(Racing);
-            ROLES.Add(Refueling);
-            ROLES.Add(Science);
-            ROLES.Add(SearchAndRescue);
-            ROLES.Add(Smuggling);
-            ROLES.Add(Taxi);
             ROLES.Add(Trading);
+            ROLES.Add(Smuggling);
+            ROLES.Add(Course);
+            ROLES.Add(Exploration);
+            ROLES.Add(Fuel);
+            ROLES.Add(Brothel);
+            ROLES.Add(Medicorps);
+            ROLES.Add(Mining);
+            ROLES.Add(MultiPurpose);
+            ROLES.Add(Press);
+            ROLES.Add(Piracy);
+            ROLES.Add(SaveRescue);
+            ROLES.Add(CombatSupport);
+            ROLES.Add(Taxi);
         }
     }
 }
