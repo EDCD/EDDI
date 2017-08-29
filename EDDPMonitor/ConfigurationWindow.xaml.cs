b12958684e1861b2057d881ea59dbe746a1e3f2d
Syrrianyl <31447889+Syrrianyl@users.noreply.github.com>
2017-08-29 20:17:21 +0200

    Translate UI in french

--- EDDPMonitor/ConfigurationWindow.xaml.cs
+++ EDDPMonitor/ConfigurationWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Eddi;
+using Eddi;
 using EddiDataDefinitions;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -42,7 +42,7 @@ namespace EddiEddpMonitor
 
             // Make a list of states plus a (anything) state that maps to NULL
             StatesPlusNone = new List<KeyValuePair<string, string>>();
-            StatesPlusNone.Add(new KeyValuePair<string, string>("(anything)", null));
+            StatesPlusNone.Add(new KeyValuePair<string, string>("(Tout)", null));
             StatesPlusNone.AddRange(State.STATES.Select(x => new KeyValuePair<string, string>(x.name, x.name)));
 
             configurationFromFile();
