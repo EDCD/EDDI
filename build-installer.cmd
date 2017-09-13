c32ea0a3aac060e5102cc3a2f4598b190130df66
Richard Buckle <richardb@sailmaker.co.uk>
2017-09-13 22:52:47 +0100

    #73 add installer script

---
+++ build-installer.cmd
@@ -0,0 +1,3 @@
+rem Requires Inno Setup from http://www.jrsoftware.org/isdl.php
+rem Assumes the Inno Setup dir, by default "%ProgramFiles(x86)%\Inno Setup 5" is in your path
+Compil32 /cc Installer.iss
