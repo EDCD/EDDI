5a2d05a45fd47c9fa43d8307c20b396073c5587e
Richard Buckle <richardb@sailmaker.co.uk>
2017-09-13 23:06:23 +0100

    #73 `build-installer.cmd` now zips the PDB files

--- build-installer.cmd
+++ build-installer.cmd
@@ -1,3 +1,7 @@
 rem Requires Inno Setup from http://www.jrsoftware.org/isdl.php
 rem Assumes the Inno Setup dir, by default "%ProgramFiles(x86)%\Inno Setup 5" is in your path
 Compil32 /cc Installer.iss
+
+rem Requires 7Zip from http://www.7-zip.org/download.html
+rem Assumes the 7Zip dir, by default "C:\Program Files\7-Zip" is in your path
+7z a -r bin\Installer\PDBs.zip bin\Release\*.pdb
