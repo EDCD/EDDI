2ac2e99684ebc6b10a1b1f44fd5edddf8f49cacf
StivlWrith <32070483+StivlWrith@users.noreply.github.com>
2017-09-26 23:41:19 +0200

    Add Gender and PowerPlay in "Commander Details"

--- EDDI/MainWindow.xaml.cs
+++ EDDI/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Eddi
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    { 
         private Profile profile;
 
         private bool fromVA;
