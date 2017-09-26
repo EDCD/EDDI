e6a4a1e5bfcfe67b9ef5ebb04d9cbced0ec0b46d
StivlWrith <32070483+StivlWrith@users.noreply.github.com>
2017-09-26 21:42:11 +0200

    up to date with EDCD/EDDI

--- ShipMonitor/ModuleStoredEvent.cs
+++ ShipMonitor/ModuleStoredEvent.cs
@@ -9,38 +9,38 @@ using System.Threading.Tasks;
 
 namespace EddiShipMonitor
 {
-    public class ModuleStoredEvent : Event
+    public class ModuleSoldFromStorage : Event
     {
-        public const string NAME = "Module stored";
-        public const string DESCRIPTION = "Triggered when you store a module";
-        public const string SAMPLE = "{ \"timestamp\":\"2016-06-10T14:32:03Z\", \"event\":\"ModuleStore\", \"Slot\":\"MediumHardpoint2\", \"Ship\":\"cobramkiii\", \"ShipID\":1, \"StoredItem\":\"hpt_pulselaser_fixed_medium\", \"EngineerModifications\":\"\", \"Cost\":500  }";
+        public const string NAME = "Module sold from storage";
+        public const string DESCRIPTION = "Triggered when selling a module from storage";
+        public const string SAMPLE = "{ \"timestamp\":\"2017-09-20T02:28:37Z\", \"event\":\"ModuleSellRemote\", \"StorageSlot\":10, \"SellItem\":\"$int_fueltank_size4_class3_name;\", \"SellItem_Localised\":\"Fuel Tank\", \"ServerId\":128064349, \"SellPrice\":24116, \"Ship\":\"diamondbackxl\", \"ShipID\":38 }";
         public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
 
-        static ModuleStoredEvent()
+        static ModuleSoldFromStorage()
         {
-            VARIABLES.Add("slot", "The outfitting slot");
-            VARIABLES.Add("module", "The stored module");
-            VARIABLES.Add("cost", "The cost of storage (if any)");
-            VARIABLES.Add("replacement", "The replacement module (if a core module)");
-            VARIABLES.Add("ship", "The ship from which the module was stored");
-            VARIABLES.Add("shipid", "The ID of the ship from which the module was stored");
+            VARIABLES.Add("ship", "The ship from which the module was sold");
+            VARIABLES.Add("shipid", "The ID of the ship from which the module was sold");
+            // VARIABLES.Add("storageslot", "The storage slot"); // This probably isn't useful to the end user
+            // VARIABLES.Add("serverid", "The frontier ID of the item being sold"); // This probably isn't useful to the end user
+            VARIABLES.Add("module", "The module (object) being sold");
+            VARIABLES.Add("price", "The price of the module being sold");
         }
 
-        public string slot { get; private set; }
-        public Module module { get; private set; }
-        public long? cost { get; private set; }
-        public Module replacement { get; private set; }
         public string ship { get; private set; }
-        public int shipid { get; private set; }
+        public int? shipid { get; private set; }
+        public int storageslot { get; private set; }
+        public long serverid { get; private set; }
+        public Module module { get; private set; }
+        public long price { get; private set; }
 
-        public ModuleStoredEvent(DateTime timestamp, string slot, Module module, long? cost, Module replacement, string ship, int shipid) : base(timestamp, NAME)
+        public ModuleSoldFromStorage(DateTime timestamp, string ship, int? shipid, int storageslot, long serverid, Module module, long price) : base(timestamp, NAME)
         {
-            this.slot = slot;
-            this.module = module;
-            this.cost = cost;
-            this.replacement = replacement;
             this.ship = ship;
             this.shipid = shipid;
+            this.storageslot = storageslot;
+            this.serverid = serverid;
+            this.module = module;
+            this.price = price;
         }
     }
 }
