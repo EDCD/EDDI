f96238395a54f814366008afd98295b5c74db81c
StivlWrith <32070483+StivlWrith@users.noreply.github.com>
2017-09-27 10:30:36 +0200

    Follow PowerPlay and Add Gender

--- DataDefinitions/Commander.cs
+++ DataDefinitions/Commander.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
 
 namespace EddiDataDefinitions
 {
     /// <summary>
     /// Details about a commander
     /// </summary>
-    public class Commander
+    public class Commander : INotifyPropertyChanged
     {
         /// <summary>The commander's name</summary>
         public string name { get; set;  }
@@ -40,5 +47,44 @@ namespace EddiDataDefinitions
 
         /// <summary>The insurance excess percentage the commander has to pay</summary>
         public decimal? insurance { get; set;  }
+		
+
+        private string _PPObedience;
+		/// <summary>The PowerPlay Master choosen by the player</summary>
+		public string powerplay
+        {
+            get
+            {
+                if (_PPObedience != null)
+                {
+                    //Logging.Info("Obedience" + _PPObedience);
+                    return _PPObedience;
+                }
+                else
+                {
+
+                    //Logging.Info("Obedience" + "None");
+                    return "None";
+                }
+            }
+            set
+            {
+                if (_PPObedience != value)
+                {
+                    _PPObedience = value;
+                    NotifyPropertyChanged("powerplay");
+                }
+            }
+        }
+
+        /// <summary>The commander's name</summary>
+        public string gender { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
     }
 }
