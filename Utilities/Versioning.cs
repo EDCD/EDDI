ffb28a9818532b02c14ddace72f1dcfa3f3e6d2b
Richard Buckle <github@sailmaker.co.uk>
2017-10-18 00:41:48 +0100

    Accept any [a-z]+ version modifier, not just [a-b]

--- Utilities/Versioning.cs
+++ Utilities/Versioning.cs
@@ -26,7 +26,7 @@ namespace Utilities
             {
                 return -1;
             }
-            System.Text.RegularExpressions.Regex versionRegex = new System.Text.RegularExpressions.Regex(@"^([\d]+)\.([\d]+)\.([\d]+)-?([a-b])?([\d]+)?");
+            System.Text.RegularExpressions.Regex versionRegex = new System.Text.RegularExpressions.Regex(@"^([\d]+)\.([\d]+)\.([\d]+)-?([a-z]+)?([\d]+)?");
             System.Text.RegularExpressions.MatchCollection m1 = versionRegex.Matches(Version1);
             System.Text.RegularExpressions.MatchCollection m2 = versionRegex.Matches(Version2);
 
