8d2ef6c8acc9de1dae70ff4bcab9ea06151f98cb
Unknown <florian.brossard@edu.univ-fcomte.fr>
2017-10-19 20:54:47 +0200

    Préparation de Tests, implémentation I18N basique
    à corrigée

--- Utilities/I18N.cs
+++ Utilities/I18N.cs
@@ -5,11 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Resources;
 using System.Reflection;
+using System.Globalization;
+using System.IO;
 
 namespace Utilities
 {
-    class I18N
+    public class I18N
     {
         private static ResourceManager i18nManager = new ResourceManager("", Assembly.GetExecutingAssembly());
+        private static string langCode = getSystemLangCode();
+
+        public static string getSystemLangCode()
+        {
+            return CultureInfo.CurrentUICulture.Name.Split('-')[0];
+        }
+
+        public static string getCurrentLangCode()
+        {
+            return langCode;
+        }
+
+        public static string getString(string code)
+        {
+            return getString(code, getCurrentLangCode());
+        }
+
+        public static string getString(string code, string lang)
+        {
+            return i18nManager.GetString(code, CultureInfo.CreateSpecificCulture(lang));
+        }
+
+        public static List<string> getAvailableLang()
+        {
+            //TODO get resources correctly
+            List<string> files = Directory.GetFiles(getLangPath(), "*.resx").ToList<string>();
+            List<string> langs = new List<string>();
+            string suffix = ".resx";
+            foreach(string file in files)
+            {
+                if (file.EndsWith(".resx"))
+                {
+                    string lang = file.Substring(0, file.Length - suffix.Length);
+                    langs.Add(lang);
+                }
+            }
+            return langs;
+        }
+
+        public static string getLangPath()
+        {
+            string path =  Path.Combine(Environment.CurrentDirectory, @"lang\");
+            return path;
+        }
     }
 }
