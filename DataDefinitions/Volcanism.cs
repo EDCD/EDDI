f2c9add5bf23639ef63e5ffa899084fb13ec1d2e
StivlWrith <32070483+StivlWrith@users.noreply.github.com>
2017-11-20 11:33:22 +0100

    compatibilité 2.4.4

--- DataDefinitions/Volcanism.cs
+++ DataDefinitions/Volcanism.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Utilities;
+using Newtonsoft.Json;
 
 namespace EddiDataDefinitions
 {
@@ -18,6 +19,16 @@ namespace EddiDataDefinitions
         public string type { get; set; } // Geysers/Magma
 
         public string composition { get; set; } // Iron, Silicate, etc.
+
+        /// <summary>The Local-ized Volcanism composition</summary>
+        [JsonIgnore]
+        public string LocalComposition
+        {
+            get
+            {
+                return I18N.GetString(composition) ?? composition;
+            }
+        }
 
         public string amount { get; set; } // Minor, Major, null (for normal)
 
