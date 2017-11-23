bc271c8a1d4c173fa3297ae77dc555fcde8af86e
StivlWrith <mordekaii.gallianus@free.fr>
2017-11-23 09:44:36 +0100

    Remove unnecessary `using` declarations

--- Utilities/I18N.cs
+++ Utilities/I18N.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Reflection;
-using System.Resources;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
 
