b62f449460946c2dce18fd64bb8d1ce5be498f77
Richard Buckle <github@sailmaker.co.uk>
2017-12-05 21:10:29 +0000

    Add PromotionTests

---
+++ Tests/PromotionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class PromotionTests
+    {
+        [TestMethod]
+        public void TestCombatPromotion()
+        {
+        }
+    }
+}
