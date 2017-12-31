e1ec9474d56a31ad2fc41fdbc7f5fd9c25c737f2
BullZye <garry_jordan@msn.com>
2017-12-30 18:55:08 -0800

    Initial implementation.

--- EDDI/App.xaml.cs
+++ EDDI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 
 namespace Eddi
 {
@@ -7,8 +9,22 @@ namespace Eddi
     /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
+        private static Mutex eddiMutex = null;
+
+        protected override void OnStartup(StartupEventArgs e)
         {
+            const string appName = "EDCD/EDDI";
+            bool createdNew;
+
+            eddiMutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                // EDDI already running, so exit now.
+                Application.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
         }
     }
 }
