6a2b3519d9f86b4a81ea0ef344b39b4cd4c08a22
T'kael <dragoncharmer84@hotmail.com>
2018-01-13 17:36:46 -0800

    Fix for #337. Prevents similar errors in EDSM inputs and ship names

--- EDDI/MainWindow.xaml.cs
+++ EDDI/MainWindow.xaml.cs
@@ -340,6 +340,7 @@ namespace Eddi
         private void homeSystemChanged(object sender, TextChangedEventArgs e)
         {
             EDDIConfiguration eddiConfiguration = EDDIConfiguration.FromFile();
+            eddiHomeSystemText.Text = Strings.JSONify(eddiHomeSystemText.Text);
             eddiConfiguration.HomeSystem = string.IsNullOrWhiteSpace(eddiHomeSystemText.Text) ? null : eddiHomeSystemText.Text.Trim();
             eddiConfiguration.ToFile();
             EDDI.Instance.updateHomeSystemStation(eddiConfiguration);
@@ -348,6 +349,7 @@ namespace Eddi
         private void homeStationChanged(object sender, TextChangedEventArgs e)
         {
             EDDIConfiguration eddiConfiguration = EDDIConfiguration.FromFile();
+            eddiHomeStationText.Text = Strings.JSONify(eddiHomeStationText.Text);
             eddiConfiguration.HomeStation = string.IsNullOrWhiteSpace(eddiHomeStationText.Text) ? null : eddiHomeStationText.Text.Trim();
             eddiConfiguration.ToFile();
             EDDI.Instance.updateHomeSystemStation(eddiConfiguration);
---
+++ Utilities/Strings.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Utilities
+{
+    public static class Strings
+    {
+        /// <summary> Remove unacceptable characters from strings written to json</summary>
+        public static string JSONify(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str)
+            {
+                if (c != '"' && c != '\\')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
