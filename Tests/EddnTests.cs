4f408196aa0dd2e8a673c2231829519efcc8d8c8
Richard Buckle <github@sailmaker.co.uk>
2018-03-29 00:31:53 +0100

    Remove useless test

--- Tests/EddnTests.cs
+++ Tests/EddnTests.cs
@@ -61,31 +61,6 @@ namespace Tests
             return responder;
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")] // this usage is perfectly correct
-        [TestMethod, TestCategory("Network")]
-        public void TestListen()
-        {
-            using (var subscriber = new SubscriberSocket())
-            {
-                int i = 0;
-                subscriber.Connect("tcp://eddn.edcd.io:9500");
-                subscriber.Subscribe("");
-                while (i < 10)
-                {
-                    string data;
-                    byte[] compressed = subscriber.ReceiveFrameBytes();
-                    using (var stream = new MemoryStream(compressed, 2, compressed.Length - 2))
-                    using (var inflater = new DeflateStream(stream, CompressionMode.Decompress))
-                    using (var streamReader = new StreamReader(inflater))
-                    {
-                        data = streamReader.ReadToEnd();
-                    }
-                    Console.WriteLine(data);
-                    i++;
-                }
-            }
-        }
-
         [TestMethod()]
         public void TestEDDNResponderGoodMatch()
         {
