2c53fd1f65473dcc7f2ef1e6a4513dec7e8af5d4
T'kael <dragoncharmer84@hotmail.com>
2018-04-09 17:53:37 -0700

    Improve data collection and attempt to resolve rollbar item #10 & 11. (Based on the data provided, the status monitor appears to be attempting to parse an empty string)

--- StatusMonitor/StatusMonitor.cs
+++ StatusMonitor/StatusMonitor.cs
@@ -121,7 +121,7 @@ namespace EddiStatusMonitor
                                 {
                                     // file open elsewhere or being written, just wait for the next pass
                                 }
-                                if (lastStatus != thisStatus && thisStatus != string.Empty)
+                                if (lastStatus != thisStatus && !string.IsNullOrWhiteSpace(thisStatus))
                                 {
                                     Status status = ParseStatusEntry(thisStatus);
 
@@ -259,7 +259,7 @@ namespace EddiStatusMonitor
             catch (Exception ex)
             {
                 Logging.Warn("Failed to parse Status.json line: " + ex.ToString());
-                Logging.Error("Exception whilst parsing Status.json line ", line);
+                Logging.Error(ex);
             }
             return status = null;
         }
