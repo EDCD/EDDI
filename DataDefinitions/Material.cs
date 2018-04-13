115e42256d6532938f652ce9f4d9303bc8007762
T'kael <dragoncharmer84@hotmail.com>
2018-04-13 10:05:00 -0700

    Correct `tg_shipsystemdata` to `tg_shipsystemsdata`. Per rollbar issue #74 & & https://github.com/msarilar/EDEngineer/blob/07bb17950420b920236c8bcc94ea4bb2c33a2f43/EDEngineer/Resources/Data/entryData.json#L1659

--- DataDefinitions/Material.cs
+++ DataDefinitions/Material.cs
@@ -131,7 +131,7 @@ namespace EddiDataDefinitions
         public static readonly Material AncientLanguageData = new Material("ancientlanguagedata", "Encoded", "Ancient Language Data (Pattern Delta)", Rarity.Rare);
         public static readonly Material AncientTechnologicalData = new Material("ancienttechnologicaldata", "Encoded", "Ancient Technological Data (Pattern Epsilon)", Rarity.Rare);
         public static readonly Material Tg_ShipFlightData = new Material("tg_shipflightdata", "Encoded", "Ship Flight Data", Rarity.Rare);
-        public static readonly Material Tg_ShipSystemData = new Material("tg_shipsystemdata", "Encoded", "Ship System Data", Rarity.Rare);
+        public static readonly Material Tg_ShipSystemsData = new Material("tg_shipsystemsdata", "Encoded", "Ship System Data", Rarity.Rare);
         public static readonly Material Tg_ResidueData = new Material("tg_residuedata", "Encoded", "Thargoid Residue Data", Rarity.Rare);
         public static readonly Material UnknownWakeScan = new Material("unknownwakedata", "Encoded", "Thargoid Wake Data", Rarity.Rare);
 
