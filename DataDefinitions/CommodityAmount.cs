aca277f639118e246618f253e697e01b29d64b66
T'kael <dragoncharmer84@hotmail.com>
2018-05-06 12:43:04 -0700

    Move CommodityAmount to EddiDataDefinitions and make both commodity and edname properties dependent on a single CommodityDefinition (to prevent independent states that could develop from setting each individually)

---
+++ DataDefinitions/CommodityAmount.cs
@@ -0,0 +1,16 @@
+﻿namespace EddiDataDefinitions
+{
+    public class CommodityAmount
+    {
+        public CommodityDefinition commodityDefinition { get; private set; }
+        public string commodity => commodityDefinition.localizedName;
+        public string edname => commodityDefinition.edname;
+        public int amount { get; private set; }
+
+        public CommodityAmount(CommodityDefinition commodity, int amount)
+        {
+            this.commodityDefinition = commodity;
+            this.amount = amount;
+        }
+    }
+}
