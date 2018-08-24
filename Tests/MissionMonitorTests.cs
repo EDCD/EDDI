27e95b4effe8527175b66ed9e89b8761bf0c265b
Kenneth Quinn <naperken@gmail.com>
2018-08-24 17:43:30 -0500

    Added shell for mission monitor unit tests

---
+++ Tests/MissionMonitorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Eddi;
+using EddiCargoMonitor;
+using EddiMissionMonitor;
+using EddiDataDefinitions;
+using EddiEvents;
+using EddiJournalMonitor;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rollbar;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MissionMonitorTests
+    {
+        MissionMonitor missionMonitor = new MissionMonitor();
+        Mission mission;
+        string line;
+        List<Event> events;
+
+        [TestInitialize]
+        private void StartTestMissionMonitor()
+        {
+            // Prevent telemetry data from being reported based on test results
+            RollbarLocator.RollbarInstance.Config.Enabled = false;
+
+            // Set ourselves as in beta to stop sending data to remote systems
+            EDDI.Instance.eventHandler(new FileHeaderEvent(DateTime.Now, "JournalBeta.txt", "beta", "beta"));
+        }
+
+        [TestCleanup]
+        private void StopTestMissionMonitor()
+        {
+        }
+    }
+}
