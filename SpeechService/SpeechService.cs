d38646a6013bafa5a8ac2c2725e94132012d0722
Tkael <dragoncharmer84@hotmail.com>
2018-08-31 19:46:10 -0700

    Attempt to fix random crashes with VoiceAttack speech. - Select voice using a Task rather than a thread (try-catch statements obtain exception info from a Task better than from another thread) - Null test synth.Voice before attempting to speak.

--- SpeechService/SpeechService.cs
+++ SpeechService/SpeechService.cs
@@ -15,6 +15,7 @@ using System.Speech.Synthesis;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 using Utilities;
 
 namespace EddiSpeechService
@@ -365,43 +366,44 @@ namespace EddiSpeechService
                             try
                             {
                                 Logging.Debug("Selecting voice " + voice);
-                                Thread t = new Thread(() => synth.SelectVoice(voice));
+                                Task t = new Task(() => synth.SelectVoice(voice));
                                 t.Start();
-                                if (!t.Join(TimeSpan.FromSeconds(2)))
+                                if (!t.Wait(TimeSpan.FromSeconds(2)))
                                 {
-                                    t.Abort();
-                                    Logging.Warn("Failed to select voice " + voice + " (1)");
+                                    t.Dispose();
+                                    Logging.Warn("Failed to select voice " + voice + " (timed out)");
                                 }
                             }
                             catch (Exception ex)
                             {
-                                Logging.Warn("Failed to select voice " + voice + " (2)", ex);
+                                Logging.Warn("Failed to select voice " + voice, ex);
                             }
                         }
-                        Logging.Debug("Post-selection");
-
-                        Logging.Debug("Configuration is " + configuration == null ? "<null>" : JsonConvert.SerializeObject(configuration));
-                        synth.Rate = configuration.Rate;
-                        synth.Volume = configuration.Volume;
-
-                        synth.SetOutputToWaveStream(stream);
-
-                        // Keep XML version at 1.0. Version 1.1 is not recommended for general use. https://en.wikipedia.org/wiki/XML#Versions
-                        if (speech.Contains("<"))
+                        if (synth.Voice != null)
                         {
-                            Logging.Debug("Obtaining best guess culture");
-                            string culture = @" xml:lang=""" + bestGuessCulture(synth) + @"""";
-                            Logging.Debug("Best guess culture is " + culture);
-                            speech = @"<?xml version=""1.0"" encoding=""UTF-8""?><speak version=""1.0"" xmlns=""http://www.w3.org/2001/10/synthesis""" + culture + ">" + escapeSsml(speech) + @"</speak>";
-                            Logging.Debug("Feeding SSML to synthesizer: " + escapeSsml(speech));
-                            synth.SpeakSsml(speech);
+                            Logging.Debug("Configuration is " + configuration == null ? "<null>" : JsonConvert.SerializeObject(configuration));
+                            synth.Rate = configuration.Rate;
+                            synth.Volume = configuration.Volume;
+
+                            synth.SetOutputToWaveStream(stream);
+
+                            // Keep XML version at 1.0. Version 1.1 is not recommended for general use. https://en.wikipedia.org/wiki/XML#Versions
+                            if (speech.Contains("<"))
+                            {
+                                Logging.Debug("Obtaining best guess culture");
+                                string culture = @" xml:lang=""" + bestGuessCulture(synth) + @"""";
+                                Logging.Debug("Best guess culture is " + culture);
+                                speech = @"<?xml version=""1.0"" encoding=""UTF-8""?><speak version=""1.0"" xmlns=""http://www.w3.org/2001/10/synthesis""" + culture + ">" + escapeSsml(speech) + @"</speak>";
+                                Logging.Debug("Feeding SSML to synthesizer: " + escapeSsml(speech));
+                                synth.SpeakSsml(speech);
+                            }
+                            else
+                            {
+                                Logging.Debug("Feeding normal text to synthesizer: " + speech);
+                                synth.Speak(speech);
+                            }
+                            stream.ToArray();
                         }
-                        else
-                        {
-                            Logging.Debug("Feeding normal text to synthesizer: " + speech);
-                            synth.Speak(speech);
-                        }
-                        stream.ToArray();
                     }
                 }
                 catch (ThreadAbortException)
