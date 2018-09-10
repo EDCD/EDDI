562f5d7563555d188ece01d39b7a642cbfa07fc1
Tkael <dragoncharmer84@hotmail.com>
2018-09-09 19:57:26 -0700

    Tidy up naming

--- EDDBService/EddbBodyData.cs
+++ EDDBService/EddbBodyData.cs
@@ -112,9 +112,9 @@ namespace EddiEddbService
 
         private static Body ParseEddbBody(object response)
         {
-            JObject body = ((JObject)response);
+            JObject bodyJson = ((JObject)response);
 
-            if ((string)body["group_name"] == "belt")
+            if ((string)bodyJson["group_name"] == "belt")
             {
                 // Not interested in asteroid belts
                 return null;
@@ -123,41 +123,41 @@ namespace EddiEddbService
             Body Body = new Body();
 
             // General items
-            Body.EDDBID = (long)body["id"];
-            Body.updatedat = (long)(Dates.fromDateTimeStringToSeconds((string)body["updated_at"]));
-            Body.name = (string)body["name"];
-            Body.Type = BodyType.FromEDName((string)body["group_name"]);
+            Body.EDDBID = (long)bodyJson["id"];
+            Body.updatedat = (long)(Dates.fromDateTimeStringToSeconds((string)bodyJson["updated_at"]));
+            Body.name = (string)bodyJson["name"];
+            Body.Type = BodyType.FromEDName((string)bodyJson["group_name"]);
 
             // System name is not given directly, but we have an EDDB ID linking to the system 
             // that we will be able to use for lookups once our system lookup is complete.
             // We will also need to use this lookup to find the reserve level associated with this system
-            Body.systemEDDBID = (long)body["system_id"];
+            Body.systemEDDBID = (long)bodyJson["system_id"];
             /*
             Body.systemname = systemName;
             Body.reserves = reserves
             */
 
             // Orbital data
-            Body.distance = (long?)body["distance_to_arrival"];
-            Body.temperature = (decimal?)body["surface_temperature"];
-            Body.tidallylocked = (bool?)body["is_rotational_period_tidally_locked"];
-            Body.rotationalperiod = (decimal?)(double?)body["rotational_period"];
-            Body.tilt = (decimal?)(double?)body["axis_tilt"];
-            Body.semimajoraxis = ConstantConverters.au2km((decimal?)(double?)body["semi_major_axis"]);
-            Body.orbitalperiod = (decimal?)(double?)body["orbital_period"];
-            Body.periapsis = (decimal?)(double?)body["arg_of_periapsis"];
-            Body.eccentricity = (decimal?)(double?)body["orbital_eccentricity"];
-            Body.inclination = (decimal?)(double?)body["orbital_inclination"];
+            Body.distance = (long?)bodyJson["distance_to_arrival"];
+            Body.temperature = (decimal?)bodyJson["surface_temperature"];
+            Body.tidallylocked = (bool?)bodyJson["is_rotational_period_tidally_locked"];
+            Body.rotationalperiod = (decimal?)(double?)bodyJson["rotational_period"];
+            Body.tilt = (decimal?)(double?)bodyJson["axis_tilt"];
+            Body.semimajoraxis = ConstantConverters.au2km((decimal?)(double?)bodyJson["semi_major_axis"]);
+            Body.orbitalperiod = (decimal?)(double?)bodyJson["orbital_period"];
+            Body.periapsis = (decimal?)(double?)bodyJson["arg_of_periapsis"];
+            Body.eccentricity = (decimal?)(double?)bodyJson["orbital_eccentricity"];
+            Body.inclination = (decimal?)(double?)bodyJson["orbital_inclination"];
 
             if (Body.type == "Star")
             {
                 // Star-specific items
-                Body.stellarclass = ((string)body["spectral_class"]).ToUpperInvariant();
-                Body.luminosityclass = ((string)body["luminosity_class"]).ToUpperInvariant();
-                Body.solarmass = (decimal?)(double?)body["solar_masses"];
-                Body.solarradius = (decimal?)(double?)body["solar_radius"];
-                Body.age = (long?)body["age"];
-                Body.mainstar = (bool?)body["is_main_star"];
+                Body.stellarclass = ((string)bodyJson["spectral_class"]).ToUpperInvariant();
+                Body.luminosityclass = ((string)bodyJson["luminosity_class"]).ToUpperInvariant();
+                Body.solarmass = (decimal?)(double?)bodyJson["solar_masses"];
+                Body.solarradius = (decimal?)(double?)bodyJson["solar_radius"];
+                Body.age = (long?)bodyJson["age"];
+                Body.mainstar = (bool?)bodyJson["is_main_star"];
                 Body.landable = false;
                 Body.setStellarExtras();
             }
@@ -165,19 +165,19 @@ namespace EddiEddbService
             if (Body.type == "Planet")
             {
                 // Planet-specific items
-                Body.planetClass = PlanetClass.FromEDName((string)body["type_name"]);
-                Body.landable = (bool?)body["is_landable"];
-                Body.earthmass = (decimal?)(double?)body["earth_masses"];
-                Body.gravity = (decimal?)(double?)body["gravity"];
-                Body.radius = (decimal?)body["radius"];
-                Body.pressure = (decimal?)(double?)body["surface_pressure"] ?? 0;
-                Body.terraformState = TerraformState.FromName((string)body["terraforming_state_name"]);
-                Body.volcanism = Volcanism.FromName((string)body["volcanism_type_name"]);
-                Body.atmosphereclass = AtmosphereClass.FromEDName((string)body["atmosphere_type_name"]);
-                if (body["atmosphere_composition"] != null)
+                Body.planetClass = PlanetClass.FromEDName((string)bodyJson["type_name"]);
+                Body.landable = (bool?)bodyJson["is_landable"];
+                Body.earthmass = (decimal?)(double?)bodyJson["earth_masses"];
+                Body.gravity = (decimal?)(double?)bodyJson["gravity"];
+                Body.radius = (decimal?)bodyJson["radius"];
+                Body.pressure = (decimal?)(double?)bodyJson["surface_pressure"] ?? 0;
+                Body.terraformState = TerraformState.FromName((string)bodyJson["terraforming_state_name"]);
+                Body.volcanism = Volcanism.FromName((string)bodyJson["volcanism_type_name"]);
+                Body.atmosphereclass = AtmosphereClass.FromEDName((string)bodyJson["atmosphere_type_name"]);
+                if (bodyJson["atmosphere_composition"] != null)
                 {
                     List<AtmosphereComposition> atmosphereCompositions = new List<AtmosphereComposition>();
-                    foreach (JObject atmoJson in body["atmosphere_composition"])
+                    foreach (JObject atmoJson in bodyJson["atmosphere_composition"])
                     {
                         string composition = (string)atmoJson["atmosphere_component_name"];
                         decimal? share = (decimal?)atmoJson["share"];
@@ -192,10 +192,10 @@ namespace EddiEddbService
                         Body.atmosphereCompositions = atmosphereCompositions;
                     }
                 }
-                if (body["solid_composition"] != null)
+                if (bodyJson["solid_composition"] != null)
                 {
                     List<BodySolidComposition> bodyCompositions = new List<BodySolidComposition>();
-                    foreach (JObject bodyCompJson in body["solid_composition"])
+                    foreach (JObject bodyCompJson in bodyJson["solid_composition"])
                     {
                         string composition = (string)bodyCompJson["solid_component_name"];
                         decimal? share = (decimal?)bodyCompJson["share"];
@@ -210,10 +210,10 @@ namespace EddiEddbService
                         Body.solidComposition = bodyCompositions;
                     }
                 }
-                if (body["materials"] != null)
+                if (bodyJson["materials"] != null)
                 {
                     List<MaterialPresence> Materials = new List<MaterialPresence>();
-                    foreach (JObject materialJson in body["materials"])
+                    foreach (JObject materialJson in bodyJson["materials"])
                     {
                         Material material = Material.FromEDName((string)materialJson["material_name"]);
                         decimal? amount = (decimal?)(double?)materialJson["share"];
@@ -232,9 +232,9 @@ namespace EddiEddbService
 
             // Rings may be an object or may be a singular
             List<Ring> rings = new List<Ring>();
-            if (body["rings"] != null)
+            if (bodyJson["rings"] != null)
             {
-                foreach (JObject ringJson in body["rings"])
+                foreach (JObject ringJson in bodyJson["rings"])
                 {
                     string name = (string)ringJson["name"];
                     string composition = (string)ringJson["ring_type_name"];
@@ -245,13 +245,13 @@ namespace EddiEddbService
                     rings.Add(ring);
                 }
             }
-            else if (body["ring_type_name"] != null)
+            else if (bodyJson["ring_type_name"] != null)
             {
-                string name = (string)body["name"];
-                string composition = (string)body["ring_type_name"];
-                decimal mass = (decimal)body["ring_mass"];
-                decimal innerRadius = (decimal)body["ring_inner_radius"];
-                decimal outerRadius = (decimal)body["ring_outer_radius"];
+                string name = (string)bodyJson["name"];
+                string composition = (string)bodyJson["ring_type_name"];
+                decimal mass = (decimal)bodyJson["ring_mass"];
+                decimal innerRadius = (decimal)bodyJson["ring_inner_radius"];
+                decimal outerRadius = (decimal)bodyJson["ring_outer_radius"];
                 Ring ring = new Ring(name, composition, mass, innerRadius, outerRadius);
                 rings.Add(ring);
             }
