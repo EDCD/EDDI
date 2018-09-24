35680f90797b88b0f20b6f9d58679b2afc5fff28
Tkael <dragoncharmer84@hotmail.com>
2018-09-23 23:52:10 -0700

    Convert EDDB station models to match journal

--- DataDefinitions/StationModels.cs
+++ DataDefinitions/StationModels.cs
@@ -28,7 +28,46 @@ namespace EddiDataDefinitions
         public StationModel() : this("")
         {}
 
-        private StationModel(string edname) : base(edname, edname)
+        private StationModel(string edname) : base(edname, edname.Replace(" Starport", ""))
         {}
+
+        new public static StationModel FromName(string from)
+        {
+            return ResourceBasedLocalizedEDName<StationModel>.FromName(eddbModel2journalModel(from));
+        }
+
+        private static string eddbModel2journalModel(string eddbModelName)
+        {
+            if (eddbModelName == null)
+            {
+                return null;
+            }
+
+            eddbModelName = eddbModelName.ToLowerInvariant();
+
+            if (eddbModelName.StartsWith("planetary") || eddbModelName.EndsWith("planetary"))
+            {
+                return "surface station";
+            }
+
+            if (eddbModelName.EndsWith("starport"))
+            {
+                eddbModelName.Replace(" starport", "");
+
+                // Ocellus starports are described by the journal as "Bernal" so we make the replacement here.
+                eddbModelName.Replace("ocellus", "bernal");
+
+                return eddbModelName;
+            }
+
+            if (eddbModelName.EndsWith("outpost"))
+            {
+                // Though EDDB provides details on the type of outpost (military, civilian, scientific, etc.), 
+                // that info is not present in the journal. Strip it for consistent output between journal and EDDB data.
+                return "outpost";
+            }
+
+            return eddbModelName;
+        }
     }
 }
