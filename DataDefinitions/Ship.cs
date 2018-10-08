105eef47a2d328b1bf3e5cf9f5ed3729a3a41101
Hoodathunk <naperken@gmail.com>
2018-10-08 07:20:11 -0500

    Added 'maxfuel' & 'maxjump' properties to ship object and 'Jumped' event handler.

--- DataDefinitions/Ship.cs
+++ DataDefinitions/Ship.cs
@@ -263,13 +263,15 @@ namespace EddiDataDefinitions
         public Module sensors { get; set; }
         public Module fueltank { get; set; }
         public Module datalinkscanner { get; set; }
-        public decimal? fueltankcapacity { get; set; } // Core capacity
-        public decimal? fueltanktotalcapacity { get; set; } // Capacity including additional tanks
         public List<Hardpoint> hardpoints { get; set; }
         public List<Compartment> compartments { get; set; }
         public List<LaunchBay> launchbays { get; set; }
 
         public string paintjob { get; set; }
+        public decimal? fueltankcapacity { get; set; } // Core capacity
+        public decimal? fueltanktotalcapacity { get; set; } // Capacity including additional tanks
+        public decimal maxfuel { get; set; }
+        public decimal maxjump { get; set; }
 
         // Admin
         // The ID in Elite: Dangerous' database
