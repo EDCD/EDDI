0a5e047676cd4a9e5abb61fd87cebcbcad48c1ff
Tkael <dragoncharmer84@hotmail.com>
2018-10-08 18:38:51 -0700

    unit test tidy ups temporary latency testing

--- Tests/DataProviderTests.cs
+++ Tests/DataProviderTests.cs
@@ -31,92 +31,6 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void TestDataProviderBodies()
-        {
-            // Force obtain the data from remote source
-            StarSystem starSystem = DataProviderService.GetFullSystemData("Sol");
-            Assert.IsNotNull(starSystem);
-
-            Assert.IsNotNull(starSystem.bodies);
-            Assert.IsFalse(starSystem.bodies.Count == 0);
-
-            Body sol = starSystem.bodies.Find(b => b.name == "Sol");
-            Assert.AreEqual(4792, sol.age);
-            Assert.AreEqual("No atmosphere", sol.atmosphereclass.invariantName);
-            Assert.AreEqual(0, sol.distance);
-            Assert.IsNull(sol.earthmass);
-            Assert.IsNull(sol.eccentricity);
-            Assert.IsNull(sol.gravity);
-            Assert.IsNull(sol.inclination);
-            Assert.IsNotNull(sol.landable);
-            Assert.IsFalse((bool)sol.landable);
-            Assert.IsNotNull(sol.mainstar);
-            Assert.IsTrue((bool)sol.mainstar);
-            Assert.AreEqual(0, sol.materials.Count);
-            Assert.AreEqual("Sol", sol.name);
-            Assert.IsNull(sol.orbitalperiod);
-            Assert.AreEqual("Star", sol.Type.invariantName);
-            Assert.IsNull(sol.pressure);
-            Assert.IsNull(sol.radius);
-            Assert.AreEqual((double)2.5832745587384M, (double)sol.rotationalperiod, 0.01);
-            Assert.IsNull(sol.semimajoraxis);
-            Assert.AreEqual(1.0, (double)sol.solarmass, 0.001);
-            Assert.AreEqual(1.0, (double)sol.solarradius, 0.001);
-            Assert.AreEqual("G", sol.stellarclass);
-            Assert.AreEqual("Sol", sol.systemname);
-            Assert.AreEqual(5778, sol.temperature);
-            Assert.AreEqual("Not terraformable", sol.terraformState.invariantName);
-            Assert.IsNotNull(sol.tidallylocked);
-            Assert.IsFalse((bool)sol.tidallylocked);
-            Assert.AreEqual((double)-0.219615M, (double)sol.tilt, 0.01);
-            Assert.AreEqual("Star", sol.Type.invariantName);
-            Assert.IsNull(sol.volcanism);
-            // Stellar extras
-            Assert.AreEqual("yellow-white", sol.chromaticity);
-            Assert.AreEqual(68, sol.massprobability);
-            Assert.AreEqual(49, sol.radiusprobability);
-            Assert.AreEqual(51, sol.tempprobability);
-            Assert.AreEqual(51, sol.ageprobability);
-
-            Body mercury = starSystem.bodies.Find(n => n.name.Equals("Mercury"));
-            Assert.IsNull(mercury.age);
-            Assert.AreEqual("Planet", mercury.Type.invariantName);
-            Assert.AreEqual("No atmosphere", mercury.atmosphereclass.invariantName);
-            Assert.IsNotNull(mercury.distance);
-            Assert.AreEqual(0.055M, mercury.earthmass);
-            Assert.AreEqual(0.2056M, mercury.eccentricity);
-            Assert.AreEqual((double)0.38M, (double)mercury.gravity, 0.1);
-            Assert.AreEqual(7, mercury.inclination);
-            Assert.IsNotNull(mercury.landable);
-            Assert.IsTrue((bool)mercury.landable);
-            Assert.IsNull(mercury.mainstar);
-            Assert.IsNotNull(mercury.materials);
-            Assert.AreEqual(11, mercury.materials.Count);
-            Assert.AreEqual("Iron", mercury.materials[0].material);
-            Assert.AreEqual(23.5, (double)mercury.materials[0].percentage, 0.1);
-            Assert.AreEqual("Mercury", mercury.materials[10].material);
-            Assert.AreEqual(1.0, (double)mercury.materials[10].percentage, 0.1);
-            Assert.AreEqual("Mercury", mercury.name);
-            Assert.AreEqual(88.0, (double)mercury.orbitalperiod, 0.1);
-            Assert.AreEqual("Metal-rich body", mercury.planetClass.invariantName);
-            Assert.IsNotNull(mercury.pressure);
-            Assert.IsNotNull(mercury.radius);
-            Assert.AreEqual(2440, (double)mercury.radius, 10);
-            Assert.AreEqual(58.6, (double)mercury.rotationalperiod, 0.1);
-            Assert.AreEqual(0.39, (double)mercury.semimajoraxis, 0.01);
-            Assert.IsNull(mercury.solarmass);
-            Assert.IsNull(mercury.solarradius);
-            Assert.IsNull(mercury.stellarclass);
-            Assert.AreEqual("Sol", mercury.systemname);
-            Assert.AreEqual(402, (double)mercury.temperature, 0.1);
-            Assert.AreEqual("Not terraformable", mercury.terraformState.invariantName);
-            Assert.IsNotNull(mercury.tidallylocked);
-            Assert.IsFalse((bool)mercury.tidallylocked);
-            Assert.AreEqual((double)0.036826M, (double)mercury.tilt, .001);
-            Assert.IsNull(mercury.volcanism);
-        }
-
-        [TestMethod]
         public void TestDataProviderUnknown()
         {
             StarSystem starSystem = DataProviderService.GetSystemData("Not appearing in this galaxy");
@@ -187,7 +101,7 @@ namespace UnitTests
         [TestMethod]
         public void TestStarSystemData()
         {
-            // Test system & body data
+            // Test system & body data in a complete star system
             StarSystem starSystem = DataProviderService.GetFullSystemData("Sol");
 
             Assert.AreEqual("Sol", starSystem.name);
@@ -217,6 +131,17 @@ namespace UnitTests
 
             Assert.AreEqual("Ugrasin", starSystem.name);
             Assert.AreEqual(1, starSystem.stations.Count);
+            Assert.AreEqual(starSystem.z, (decimal)11.28125);
+        }
+
+        [TestMethod]
+        public void TestFullSystemLatency()
+        {
+            System.DateTime startTime = System.DateTime.UtcNow;
+            StarSystem system = DataProviderService.GetFullSystemData("Shinrarta Dezhra");
+            System.TimeSpan latencyTimeSpan = System.DateTime.UtcNow - startTime;
+            Assert.IsNotNull(system);
+            Assert.IsTrue(latencyTimeSpan.Milliseconds < 50); //8.48 seconds
         }
     }
 }
