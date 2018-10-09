85ae0048d417ce72b8e58aab99e50600ece75fc2
Tkael <dragoncharmer84@hotmail.com>
2018-10-08 18:38:51 -0700

    unit test tidy ups temporary latency testing

--- Tests/EddbDataTests.cs
+++ Tests/EddbDataTests.cs
@@ -19,10 +19,11 @@ namespace UnitTests
             RollbarLocator.RollbarInstance.Config.Enabled = false;
         }
 
-        /*
         [TestMethod]
         public void TestEddbDataMultiParameters()
         {
+            PrivateType eddbService = new PrivateType(typeof(EddbService));
+
             List<KeyValuePair<string, object>> queryList = new List<KeyValuePair<string, object>>()
             {
                 new KeyValuePair<string, object>(SystemQuery.government, "Anarchy"),
@@ -31,138 +32,109 @@ namespace UnitTests
             };
 
             string data = JsonConvert.SerializeObject(EddbService.GetData(Endpoint.systems, queryList));
-            List<StarSystem> response = JsonConvert.DeserializeObject<List<StarSystem>>(data);
-
+            List<object> response = JsonConvert.DeserializeObject<List<object>>(data);
             Assert.IsNotNull(response);
-            Assert.IsTrue((response[0]).government.ToLowerInvariant() == "anarchy");
-            Assert.IsTrue((response[0]).security.ToLowerInvariant() == "anarchy");
-            Assert.IsTrue((response[0]).allegiance.ToLowerInvariant() == "empire");
-        }
-        */
 
-        [TestMethod]
-        public void TestBodyDataSol()
-        {
-            Body body = EddbService.Body("Sol");
-            Assert.AreEqual(1, body.EDDBID);
-            Assert.AreEqual(4792, body.age);
-            Assert.AreEqual("No atmosphere", body.atmosphereclass.invariantName);
-            Assert.AreEqual(0, body.distance);
-            Assert.IsNull(body.earthmass);
-            Assert.IsNull(body.eccentricity);
-            Assert.IsNull(body.gravity);
-            Assert.IsNull(body.inclination);
-            Assert.IsNotNull(body.landable);
-            Assert.IsFalse((bool)body.landable);
-            Assert.IsNotNull(body.mainstar);
-            Assert.IsTrue((bool)body.mainstar);
-            Assert.AreEqual(0, body.materials.Count);
-            Assert.AreEqual("Sol", body.name);
-            Assert.IsNull(body.orbitalperiod);
-            Assert.AreEqual("None", body.planetClass.invariantName);
-            Assert.IsNull(body.pressure);
-            Assert.IsNull(body.radius);
-            Assert.AreEqual(2.58327455873843, (double)body.rotationalperiod, .001);
-            Assert.IsNull(body.semimajoraxis);
-            Assert.AreEqual(1.0, (double)body.solarmass, 0.001);
-            Assert.AreEqual(1.0, (double)body.solarradius, 0.001);
-            Assert.AreEqual("G", body.stellarclass);
-            Assert.AreEqual(5778, body.temperature);
-            Assert.AreEqual("Not terraformable", body.terraformState.invariantName);
-            Assert.AreEqual(-0.219615, (double)body.tilt, .001);
-            Assert.AreEqual("Star", body.Type.invariantName);
-            Assert.IsNull(body.volcanism);
+            StarSystem system = (StarSystem)eddbService.InvokeStatic("ParseEddbSystem", response[0]);
+            Assert.IsNotNull(system);
+            if (response.Count > 0)
+            {
+                Assert.IsTrue(system.Faction?.Government?.invariantName == "Anarchy");
+                Assert.IsTrue(system.securityLevel?.invariantName == "Anarchy");
+                Assert.IsTrue(system.Faction?.Allegiance.invariantName == "Empire");
+            }
+            else
+            {
+                Assert.Fail();
+            }
         }
 
         [TestMethod]
-        public void TestBodyDataAlphaCentauriB()
+        public void TestSolBodies()
         {
-            Body body = EddbService.Body("Alpha Centauri B");
-            Assert.AreEqual(16309, body.EDDBID);
-            Assert.AreEqual("Alpha Centauri B", body.name);
-            Assert.AreEqual(764, body.systemEDDBID);
-            Assert.AreEqual("Star", body.Type.invariantName);
-            Assert.AreEqual("K", body.stellarclass);
-            Assert.AreEqual(5398, (double)body.distance, 50);
-            Assert.AreEqual(5240, body.temperature);
-            Assert.IsNotNull(body.mainstar);
-            Assert.IsFalse((bool)body.mainstar);
-            Assert.AreEqual(9212, body.age);
-            Assert.AreEqual(0.855469, (double)body.solarmass, 0.001);
-            Assert.AreEqual(0.90351818312491, (double)body.solarradius, 0.001);
-            Assert.IsNull(body.volcanism);
-            Assert.AreEqual("No atmosphere", body.atmosphereclass.invariantName);
-            Assert.AreEqual("Not terraformable", body.terraformState.invariantName);
-            Assert.IsNull(body.earthmass);
-            Assert.IsNull(body.radius);
-            Assert.IsNull(body.gravity);
-            Assert.IsNull(body.pressure);
-            Assert.AreEqual(9348.9066666667, (double)body.orbitalperiod, .001);
-            Assert.AreEqual(6.3673077331842, (double)body.semimajoraxis, .001);
-            Assert.AreEqual(0.5179, (double)body.eccentricity, .001);
-            Assert.AreEqual(79.205002, (double)body.inclination, .001);
-            Assert.AreEqual(296.656494, (double)body.periapsis, .001);
-            Assert.AreEqual(4.0409704137731, (double)body.rotationalperiod, .001);
-            Assert.AreEqual(0.203023, (double)body.tilt, .001);
-            Assert.IsNotNull(body.tidallylocked);
-            Assert.IsFalse((bool)body.tidallylocked);
-            Assert.IsNotNull(body.landable);
-            Assert.IsFalse((bool)body.landable);
-            Assert.AreEqual(0, body.materials.Count);
-            Assert.AreEqual("None", body.planetClass.invariantName);
-        }
+            // Force obtain the data from remote source
+            List<Body> bodies = EddbService.Bodies("Sol");
+            Assert.IsNotNull(bodies);
+            Assert.AreEqual(40, bodies.Count);
 
-        [TestMethod]
-        public void TestBodyDataMercury()
-        {
-            Body body = EddbService.Body("Mercury");
+            Body sol = bodies.Find(b => b.name == "Sol");
+            Assert.AreEqual(4792, sol.age);
+            Assert.AreEqual("No atmosphere", sol.atmosphereclass.invariantName);
+            Assert.AreEqual(0, sol.distance);
+            Assert.IsNull(sol.earthmass);
+            Assert.IsNull(sol.eccentricity);
+            Assert.IsNull(sol.gravity);
+            Assert.IsNull(sol.inclination);
+            Assert.IsNotNull(sol.landable);
+            Assert.IsFalse((bool)sol.landable);
+            Assert.IsNotNull(sol.mainstar);
+            Assert.IsTrue((bool)sol.mainstar);
+            Assert.AreEqual(0, sol.materials.Count);
+            Assert.AreEqual("Sol", sol.name);
+            Assert.IsNull(sol.orbitalperiod);
+            Assert.AreEqual("Star", sol.Type.invariantName);
+            Assert.IsNull(sol.pressure);
+            Assert.IsNull(sol.radius);
+            Assert.AreEqual((double)2.5832745587384M, (double)sol.rotationalperiod, 0.01);
+            Assert.IsNull(sol.semimajoraxis);
+            Assert.AreEqual(1.0, (double)sol.solarmass, 0.001);
+            Assert.AreEqual(1.0, (double)sol.solarradius, 0.001);
+            Assert.AreEqual("G", sol.stellarclass);
+            Assert.AreEqual("Sol", sol.systemname);
+            Assert.AreEqual(5778, sol.temperature);
+            Assert.AreEqual("Not terraformable", sol.terraformState.invariantName);
+            Assert.IsNotNull(sol.tidallylocked);
+            Assert.IsFalse((bool)sol.tidallylocked);
+            Assert.AreEqual((double)-0.219615M, (double)sol.tilt, 0.01);
+            Assert.AreEqual("Star", sol.Type.invariantName);
+            Assert.IsNull(sol.volcanism);
+            // Stellar extras
+            Assert.AreEqual("yellow-white", sol.chromaticity);
+            Assert.AreEqual(68, sol.massprobability);
+            Assert.AreEqual(49, sol.radiusprobability);
+            Assert.AreEqual(51, sol.tempprobability);
+            Assert.AreEqual(51, sol.ageprobability);
+
+            Body mercury = bodies.Find(n => n.name.Equals("Mercury"));
+            Assert.IsNull(mercury.age);
+            Assert.AreEqual("Planet", mercury.Type.invariantName);
+            Assert.AreEqual("No atmosphere", mercury.atmosphereclass.invariantName);
+            Assert.IsNotNull(mercury.distance);
+            Assert.AreEqual(0.055M, mercury.earthmass);
+            Assert.AreEqual(0.2056M, mercury.eccentricity);
+            Assert.AreEqual((double)0.38M, (double)mercury.gravity, 0.1);
+            Assert.AreEqual(7, mercury.inclination);
+            Assert.IsNotNull(mercury.landable);
+            Assert.IsTrue((bool)mercury.landable);
+            Assert.IsNull(mercury.mainstar);
+            Assert.IsNotNull(mercury.materials);
+            Assert.AreEqual(11, mercury.materials.Count);
+            Assert.AreEqual("Iron", mercury.materials[0].material);
+            Assert.AreEqual(23.5, (double)mercury.materials[0].percentage, 0.1);
+            Assert.AreEqual("Mercury", mercury.materials[10].material);
+            Assert.AreEqual(1.0, (double)mercury.materials[10].percentage, 0.1);
+            Assert.AreEqual("Mercury", mercury.name);
+            Assert.AreEqual(88.0, (double)mercury.orbitalperiod, 0.1);
+            Assert.AreEqual("Metal-rich body", mercury.planetClass.invariantName);
+            Assert.IsNotNull(mercury.pressure);
+            Assert.IsNotNull(mercury.radius);
+            Assert.AreEqual(2440, (double)mercury.radius, 10);
+            Assert.AreEqual(58.6, (double)mercury.rotationalperiod, 0.1);
+            Assert.AreEqual(0.39, (double)mercury.semimajoraxis, 0.01);
+            Assert.IsNull(mercury.solarmass);
+            Assert.IsNull(mercury.solarradius);
+            Assert.IsNull(mercury.stellarclass);
+            Assert.AreEqual("Sol", mercury.systemname);
+            Assert.AreEqual(402, (double)mercury.temperature, 0.1);
+            Assert.AreEqual("Not terraformable", mercury.terraformState.invariantName);
+            Assert.IsNotNull(mercury.tidallylocked);
+            Assert.IsFalse((bool)mercury.tidallylocked);
+            Assert.AreEqual((double)0.036826M, (double)mercury.tilt, .001);
+            Assert.IsNull(mercury.volcanism);
+
+            Body body = bodies.Find(n => n.name.Equals("Earth"));
             Assert.IsNull(body.age);
-            Assert.AreEqual("No atmosphere", body.atmosphereclass.invariantName);
-            Assert.IsNotNull(body.distance);
-            Assert.AreEqual(0.055M, body.earthmass);
-            Assert.AreEqual(0.2056M, body.eccentricity);
-            Assert.AreEqual(0.37588835103399, (double)body.gravity, .001);
-            Assert.AreEqual(7, body.inclination);
-            Assert.IsNotNull(body.landable);
-            Assert.IsTrue((bool)body.landable);
-            Assert.IsNull(body.mainstar);
-            Assert.IsNotNull(body.materials);
-            Assert.AreEqual(11, body.materials.Count);
-            Assert.AreEqual("Iron", body.materials[0].material);
-            Assert.AreEqual(23.5, (double)body.materials[0].percentage, 0.1);
-            Assert.AreEqual("Mercury", body.materials[10].material);
-            Assert.AreEqual(1.0, (double)body.materials[10].percentage, 0.1);
-            Assert.AreEqual("Mercury", body.name);
-            Assert.AreEqual(88.0, (double)body.orbitalperiod, 0.1);
-            Assert.AreEqual("Metal-rich body", body.planetClass.invariantName);
-            Assert.AreEqual(0, body.pressure);
-            Assert.IsNotNull(body.radius);
-            Assert.AreEqual(2439.7M, body.radius);
-            Assert.AreEqual(58.6, (double)body.rotationalperiod, 0.1);
-            Assert.AreEqual((double)0.38709837299843, (double)body.semimajoraxis, 0.01);
-            Assert.IsNull(body.solarmass);
-            Assert.IsNull(body.solarradius);
-            Assert.IsNull(body.stellarclass);
-            Assert.AreEqual(401.965271M, body.temperature);
-            Assert.AreEqual("Not terraformable", body.terraformState.invariantName);
-            Assert.IsNotNull(body.tidallylocked);
-            Assert.IsFalse((bool)body.tidallylocked);
-            Assert.AreEqual((double)0.036826M, (double)body.tilt, .001);
-            Assert.AreEqual("Planet", body.Type.invariantName);
-            Assert.IsNull(body.volcanism);
-            Assert.AreEqual("Metal", body.solidComposition[0].invariantName);
-            Assert.AreEqual(60M, body.solidComposition[0].percent);
-            Assert.AreEqual("Rock", body.solidComposition[1].invariantName);
-            Assert.AreEqual(40M, body.solidComposition[1].percent);
-            Assert.AreEqual(2, body.solidComposition.Count);
-        }
-
-        [TestMethod]
-        public void TestBodyDataEarth()
-        {
-            Body body = EddbService.Body("Earth", "Sol");
-            Assert.IsNull(body.age);
-            Assert.AreEqual("Suitable for water-based life", body.atmosphereclass.invariantName); 
+            Assert.AreEqual("Suitable for water based life", body.atmosphereclass.invariantName);
             // Data source reports this as "Suitable for water-based life", matching System Map, but the journal raw is "Earthlike". 
             // Apparently intentional: 
             // https://github.com/EDSM-NET/Alias/blob/3a904f799a4b7b4dd28f12af80eba38307789c99/Body/Planet/Atmosphere.php#L83
@@ -207,6 +179,45 @@ namespace UnitTests
         }
 
         [TestMethod]
+        public void TestBodyDataAlphaCentauriB()
+        {
+            // Test secondary star (including orbital parameters relative to the primary)
+            Body body = EddbService.Body("Alpha Centauri B");
+            Assert.AreEqual(16309, body.EDDBID);
+            Assert.AreEqual("Alpha Centauri B", body.name);
+            Assert.AreEqual(764, body.systemEDDBID);
+            Assert.AreEqual("Star", body.Type.invariantName);
+            Assert.AreEqual("K", body.stellarclass);
+            Assert.AreEqual(5398, (double)body.distance, 100);
+            Assert.AreEqual(5240, body.temperature);
+            Assert.IsNotNull(body.mainstar);
+            Assert.IsFalse((bool)body.mainstar);
+            Assert.AreEqual(9212, body.age);
+            Assert.AreEqual(0.855469, (double)body.solarmass, 0.001);
+            Assert.AreEqual(0.90351818312491, (double)body.solarradius, 0.001);
+            Assert.IsNull(body.volcanism);
+            Assert.AreEqual("No atmosphere", body.atmosphereclass.invariantName);
+            Assert.AreEqual("Not terraformable", body.terraformState.invariantName);
+            Assert.IsNull(body.earthmass);
+            Assert.IsNull(body.radius);
+            Assert.IsNull(body.gravity);
+            Assert.IsNull(body.pressure);
+            Assert.AreEqual(9348.9066666667, (double)body.orbitalperiod, .001);
+            Assert.AreEqual(6.3673077331842, (double)body.semimajoraxis, .001);
+            Assert.AreEqual(0.5179, (double)body.eccentricity, .001);
+            Assert.AreEqual(79.205002, (double)body.inclination, .001);
+            Assert.AreEqual(296.656494, (double)body.periapsis, .001);
+            Assert.AreEqual(4.0409704137731, (double)body.rotationalperiod, .001);
+            Assert.AreEqual(0.203023, (double)body.tilt, .001);
+            Assert.IsNotNull(body.tidallylocked);
+            Assert.IsFalse((bool)body.tidallylocked);
+            Assert.IsNotNull(body.landable);
+            Assert.IsFalse((bool)body.landable);
+            Assert.AreEqual(0, body.materials.Count);
+            Assert.AreEqual("None", body.planetClass.invariantName);
+        }
+
+        [TestMethod]
         public void TestBodyDataRingedGasGiant()
         {
             Body body = EddbService.Body("HD 43193 CD 7 a");
@@ -234,13 +245,6 @@ namespace UnitTests
             Assert.AreEqual(355470000000M, body.rings[0].mass);
             Assert.AreEqual("HD 43193 CD 7 a A Ring", body.rings[0].name);
             Assert.AreEqual(169800M, body.rings[0].outerradius);
-        }
-
-        [TestMethod]
-        public void TestBodiesSol()
-        {
-            List<Body> bodies = EddbService.Bodies("Sol");
-            Assert.AreEqual(40, bodies.Count);
         }
 
         [TestMethod]
@@ -393,13 +397,43 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void TestUgrasin()
+        public void TestBodyLatency()
         {
-            // Test randomly
-            StarSystem starSystem = EddbService.System("Ugrasin");
+            System.DateTime startTime = System.DateTime.UtcNow; 
+            Body body = EddbService.Body("Jupiter");
+            System.TimeSpan latencyTimeSpan = System.DateTime.UtcNow - startTime;
+            Assert.IsNotNull(body);
+            Assert.IsTrue(latencyTimeSpan.Milliseconds < 50); //1.91 seconds
+        }
 
-            Assert.AreEqual("Ugrasin", starSystem.name);
-            Assert.AreEqual(starSystem.z, (decimal)11.28125);
+        [TestMethod]
+        public void TestStationLatency()
+        {
+            System.DateTime startTime = System.DateTime.UtcNow;
+            Station station = EddbService.Station("Jameson Memorial");
+            System.TimeSpan latencyTimeSpan = System.DateTime.UtcNow - startTime;
+            Assert.IsNotNull(station);
+            Assert.IsTrue(latencyTimeSpan.Milliseconds < 50); //1.90 seconds
+        }
+
+        [TestMethod]
+        public void TestFactionLatency()
+        {
+            System.DateTime startTime = System.DateTime.UtcNow;
+            Faction faction = EddbService.Faction("Coalition of Achali");
+            System.TimeSpan latencyTimeSpan = System.DateTime.UtcNow - startTime;
+            Assert.IsNotNull(faction);
+            Assert.IsTrue(latencyTimeSpan.Milliseconds < 50); //0.86 seconds
+        }
+
+        [TestMethod]
+        public void TestSystemLatency()
+        {
+            System.DateTime startTime = System.DateTime.UtcNow;
+            StarSystem system = EddbService.System("Shinrarta Dezhra");
+            System.TimeSpan latencyTimeSpan = System.DateTime.UtcNow - startTime;
+            Assert.IsNotNull(system);
+            Assert.IsTrue(latencyTimeSpan.Milliseconds < 50); //1.33 seconds
         }
     }
 }
