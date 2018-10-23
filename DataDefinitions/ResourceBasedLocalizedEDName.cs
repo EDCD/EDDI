10d7e5a70760369e3fbccce54e8c6f5c41096405
Richard Buckle <github@sailmaker.co.uk>
2018-10-23 19:46:04 +0100

    Implement JsonConverterFromEDName

--- DataDefinitions/ResourceBasedLocalizedEDName.cs
+++ DataDefinitions/ResourceBasedLocalizedEDName.cs
@@ -1,15 +1,68 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Resources;
-using System.Runtime.Serialization;
 using Utilities;
 
 namespace EddiDataDefinitions
 {
-    [JsonObject(MemberSerialization.OptIn)]
+    // A JsonConverter that correctly initialises ResourceBasedLocalizedEDName<T> instances using their static FromEDName() method.
+    // Unfortunately we cannot make this a generic type as they are not allowed as parameters for JsonConverterAttribute (or any attribute for that matter),
+    // so instead we have to access FromEDName() via the reflection API.
+    public class JsonConverterFromEDName : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => typeof(ResourceBasedLocalizedEDName<>).IsAssignableFrom(objectType);
+        public override bool CanRead => true;
+        public override bool CanWrite => false;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            try
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonToken.None:
+                    case JsonToken.Null:
+                    case JsonToken.Undefined:
+                    case JsonToken.EndObject:
+                    case JsonToken.EndArray:
+                        return null;
+                    default:
+                        break;
+                }
+                if (reader.TokenType == JsonToken.Null)
+                {
+                    return null;
+                }
+                JObject jsonObject = JObject.Load(reader);
+                bool success = jsonObject.TryGetValue("edname", out JToken token);
+                if (!success)
+                {
+                    return null;
+                }
+                string edname = token.Value<string>();
+                MethodInfo method = objectType.GetMethod("FromEDName", BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy, null, new Type[] { typeof(string) }, null);
+                object result = method?.Invoke(null, new object[] { edname });
+                return result;
+            }
+            catch (Exception)
+            {
+                // let the Json.Net machinery handle the exceptoion
+                throw;
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [JsonObject(MemberSerialization.OptIn), JsonConverter(typeof(JsonConverterFromEDName))]
     public class ResourceBasedLocalizedEDName<T> where T : ResourceBasedLocalizedEDName<T>, new()
     {
         static ResourceBasedLocalizedEDName()
@@ -20,21 +73,14 @@ namespace EddiDataDefinitions
         protected static ResourceManager resourceManager;
         protected static Func<string, T> missingEDNameHandler;
 
-        [JsonProperty] // Required by the constructor
-        public readonly string edname;
-
-        [JsonProperty] // Required by the constructor
-        public readonly string basename;
+        [JsonIgnore]
+        public readonly string edname; // must not be JsonProperty as that lets Json.NET modify it, which causes chaos
 
         [JsonIgnore]
-        public string invariantName
-        {
-            get
-            {
-                string invariantName = resourceManager.GetString(basename, CultureInfo.InvariantCulture) ?? basename;
-                return invariantName;
-            }
-        }
+        public readonly string basename; // must not be JsonProperty as that lets Json.NET modify it, which causes chaos
+
+        [JsonIgnore]
+        public string invariantName => resourceManager.GetString(basename, CultureInfo.InvariantCulture) ?? basename;
 
         [JsonIgnore]
         public string fallbackLocalizedName { get; set; } = null;
