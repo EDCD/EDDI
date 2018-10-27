c157afcf4d1910d3908b7092256eb9134848def4
Tkael <dragoncharmer84@hotmail.com>
2018-10-26 23:34:53 -0700

    Add EDSM to DataProviderService.cs

--- DataProviderService/DataProviderService.cs
+++ DataProviderService/DataProviderService.cs
@@ -14,39 +14,110 @@ using System.Threading.Tasks;
 
 namespace EddiDataProviderService
 {
-    /// <summary>Access to EDDP data<summary>
+    /// <summary>Access data services<summary>
     public class DataProviderService
     {
-        public static StarSystem GetFullSystemData(string system)
+        // EDDB data service
+        public static StarSystem GetEddbFullSystemData (string system)
         {
             if (system == null) { return null; }
 
-            // Retrieve starSystem and station details concurrently.
+            StarSystem starSystem = EddbService.System(system);
 
-            Task<StarSystem> getSystem = Task.Run(() => EddbService.System(system));
-            StarSystem starSystem = getSystem.Result;
+            List<Body> bodies = EddbService.Bodies(system);
+            SetBodyReserves(starSystem, bodies);
+            starSystem.bodies = bodies;
 
-            Task<List<Station>> getStations = Task.Run(() => setStationSystemData(system));
-
-            // Wait until the starSystem has been retrieved, then set reserve levels from starSystem data
-
-            Task<List<Body>> getBodies = getSystem.ContinueWith(s => setBodySystemData(system, s.Result.Reserve));
-
-            starSystem.stations = getStations.Result;
-            starSystem.bodies = getBodies.Result;
+            if (starSystem?.population > 0)
+            {
+                List<Station> stations = EddbService.Stations(system);
+                SetStationSystemName(starSystem);
+                SetCommodityData(system, stations);
+                starSystem.stations = stations;
+            }
 
             return starSystem;
         }
 
-        public static StarSystem GetSystemData(string system)
+        public static StarSystem GetEddbSystemData(string system)
         {
             if (system == null) { return null; }
             return EddbService.System(system);
         }
 
-        private static List<Station> setStationSystemData(string system)
+        private static void SetStationSystemName(StarSystem starSystem)
         {
-            List<Station> stations = EddbService.Stations(system);
+            // Assign the system name to each stations. Only needed for EDDB data. 
+            foreach (Station station in starSystem.stations)
+            {
+                station.systemname = starSystem.name;
+            }
+        }
+
+        private static void SetBodyReserves(StarSystem starSystem, List<Body> bodies)
+        {
+            // Sets body reserve levels. Only needed for EDDB data.
+            foreach (Body body in bodies)
+            {
+                if (body.rings?.Count() > 0)
+                {
+                    body.reserveLevel = starSystem.Reserve;
+                }
+                body.systemname = starSystem.name;
+            }
+        }
+
+        // EDSM data service
+        public static StarSystem GetEdsmFullSystemData(string system)
+        {
+            if (system == null) { return null; }
+
+            StarSystem starSystem = StarMapService.GetStarMapSystem(system);
+            if (starSystem != null)
+            {
+                List<Body> bodies = StarMapService.GetStarMapBodies(system);
+                starSystem.bodies = bodies;
+
+                if (starSystem?.population > 0)
+                {
+                    List<Faction> factions = StarMapService.GetStarMapFactions(system);
+                    List<Station> stations = StarMapService.GetStarMapStations(system);
+
+                    SetStationFactionData(stations, factions);
+                    SetCommodityData(system, stations);
+
+                    starSystem.factions = factions;
+                    starSystem.stations = stations;
+                }
+            }
+
+            return starSystem ?? new StarSystem() { name = system };
+        }
+
+        public static StarSystem GetEdsmSystemData(string system)
+        {
+            if (system == null) { return null; }
+            return StarMapService.GetStarMapSystem(system);
+        }
+
+        private static void SetStationFactionData(List<Station> stations, List<Faction> factions)
+        {
+            // EDSM doesn't provide FactionState information from the stations endpoint data, we need to add it from the factions endpoint data
+            foreach (Station station in stations)
+            {
+                foreach (Faction faction in factions)
+                {
+                    if (station.Faction.name == faction.name)
+                    {
+                        station.Faction = faction;
+                    }
+                }
+            }
+        }
+
+        // Legacy EDDP data service
+        private static void SetCommodityData (string system, List<Station> stations)
+        {
             foreach (Station station in stations)
             {
                 station.systemname = system;
@@ -54,25 +125,10 @@ namespace EddiDataProviderService
                 station.commodities = commodities;
                 station.commoditiesupdatedat = commoditiesupdatedat;
             }
-            return stations;
         }
 
-        private static List<Body> setBodySystemData(string system, SystemReserveLevel reserve)
-        {
-            List<Body> bodies = EddbService.Bodies(system);
-            foreach (Body body in bodies)
-            {
-                if (body.rings?.Count() > 0)
-                {
-                    body.reserveLevel = reserve;
-                }
-                body.systemname = system;
-            }
-            return bodies;
-        }
-
-
-        public void syncFromStarMapService(StarMapService starMapService, StarMapConfiguration starMapCredentials)
+        // EDSM flight log synchronization
+        public void syncFromStarMapService (StarMapService starMapService, StarMapConfiguration starMapCredentials)
         {
             Logging.Info("Syncing from EDSM");
             try
@@ -113,7 +169,7 @@ namespace EddiDataProviderService
             }
         }
 
-        public static void saveFromStarMapService(List<StarSystem> syncSystems)
+        public static void saveFromStarMapService (List<StarSystem> syncSystems)
         {
             StarSystemSqLiteRepository.Instance.SaveStarSystems(syncSystems);
             StarMapConfiguration starMapConfiguration = StarMapConfiguration.FromFile();
