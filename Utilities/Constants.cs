f9d84dc6b6fddeefc01214083d676ab2d65838c7
Richard Buckle <github@sailmaker.co.uk>
2018-12-18 00:15:57 +0000

    3.3.0-rc1

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -8,7 +8,7 @@ namespace Utilities
     public class Constants
     {
         public const string EDDI_NAME = "EDDI";
-        public static Version EDDI_VERSION = new Version(3, 3, 0, Version.TestPhase.b, 2);
+        public static Version EDDI_VERSION = new Version(3, 3, 0, Version.TestPhase.rc, 1);
         public const string EDDI_SERVER_URL = "http://edcd.github.io/EDDP/";
         public static readonly string EDDI_SYSTEM_MUTEX_NAME = $"{EDDI_SERVER_URL}/{EDDI_NAME}/{Environment.GetEnvironmentVariable("UserName")}";
 
