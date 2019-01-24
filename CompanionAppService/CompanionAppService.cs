0ff171652e135ac1b2a9a873ac97c7db9c90cc9a
Richard Buckle <github@sailmaker.co.uk>
2019-01-24 21:18:49 +0000

    Suppress CA1009
    If we were talking about UI event handlers it would be valid, but here it is just unnecessary ceremony.

--- CompanionAppService/CompanionAppService.cs
+++ CompanionAppService/CompanionAppService.cs
@@ -58,6 +58,9 @@ namespace EddiCompanionAppService
             }
         }
         public delegate void StateChangeHandler(State oldState, State newState);
+
+        // This is not a UI event handler so I consider that CA1009 is just unnecessary ceremony for no benfit.
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1009:DeclareEventHandlersCorrectly")]
         public event StateChangeHandler StateChanged;
 
         public CompanionAppCredentials Credentials;
