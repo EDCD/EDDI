b02a7b5321b69835d37950c956e6f05a1c938fd5
Hoodathunk <naperken@gmail.com>
2019-01-25 09:46:30 -0600

    Hotfix for 'EconomiesFromProfile' exception

--- CompanionAppService/CompanionAppService.cs
+++ CompanionAppService/CompanionAppService.cs
@@ -690,7 +690,7 @@ namespace EddiCompanionAppService
                 foreach (dynamic economyJson in json["lastStarport"]["economies"])
                 {
                     dynamic economy = economyJson.Value;
-                    string name = (string)economy["name"].Replace("Agri", "Agriculture");
+                    string name = ((string)economy["name"]).Replace("Agri", "Agriculture");
                     decimal proportion = (decimal)economy["proportion"];
                     EconomyShare Economy = new EconomyShare(name, proportion);
                     Economies.Add(Economy);
