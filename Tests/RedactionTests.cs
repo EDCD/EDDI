fd1bd8b6fe3bdebaa8a45e7cc5f6d5e7c9654d98
Richard Buckle <github@sailmaker.co.uk>
2019-03-03 20:32:44 +0000

    Add Redaction class with tests

---
+++ Tests/RedactionTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RedactionTests : TestBase
+    {
+        [TestInitialize]
+        public void start()
+        {
+            MakeSafe();
+        }
+
+    }
+}
