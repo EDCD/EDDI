0676264878d0feeec825ee8f5693167f37f6d06b
Richard Buckle <github@sailmaker.co.uk>
2019-03-25 23:24:31 +0000

    Exit early for no change

--- EDDI/StarSystemComboBox.cs
+++ EDDI/StarSystemComboBox.cs
@@ -13,46 +13,44 @@ namespace Eddi
 
         internal void TextDidChange(object sender, TextChangedEventArgs e, string oldValue, Action changeHandler)
         {
-            if (Text != oldValue)
+            if (Text == oldValue) { return; }
+
+            string systemName = Text;
+            if (systemName.Length > 1)
             {
-                string systemName = Text;
-                if (systemName.Length > 1)
+                systemList = systemList.Where(s => s.StartsWith(systemName, StringComparison.InvariantCultureIgnoreCase)).ToList();
+                if (systemList.Count < 5)
                 {
-                    systemList = systemList.Where(s => s.StartsWith(systemName, StringComparison.InvariantCultureIgnoreCase)).ToList();
-                    if (systemList.Count < 5)
-                    {
-                        systemList = StarMapService.GetStarMapSystemsPartial(systemName, false, false)
-                            .Select(s => s.name).ToList();
+                    systemList = StarMapService.GetStarMapSystemsPartial(systemName, false, false)
+                        .Select(s => s.name).ToList();
 
-                        if (systemList.Count == 1 && systemName.Equals(systemList[0], StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            ItemsSource = systemList.Take(1);
-                            SelectedItem = systemList[0];
-                            IsDropDownOpen = false;
-                            return;
-                        }
-                    }
-                    ItemsSource = systemList.Take(5);
-
-                    if (IsDropDownOpen == false)
+                    if (systemList.Count == 1 && systemName.Equals(systemList[0], StringComparison.InvariantCultureIgnoreCase))
                     {
-                        IsDropDownOpen = true;
-                        var cmbTextBox = (TextBox)Template
-                            .FindName("PART_EditableTextBox", this);
-                        cmbTextBox.CaretIndex = Text.Length;
-                    }
-                }
-                else
-                {
-                    if (ItemsSource != null)
-                    {
+                        ItemsSource = systemList.Take(1);
+                        SelectedItem = systemList[0];
                         IsDropDownOpen = false;
-                        ItemsSource = null;
+                        return;
                     }
                 }
+                ItemsSource = systemList.Take(5);
 
-                changeHandler?.Invoke();
+                if (IsDropDownOpen == false)
+                {
+                    IsDropDownOpen = true;
+                    var cmbTextBox = (TextBox)Template.FindName("PART_EditableTextBox", this);
+                    cmbTextBox.CaretIndex = Text.Length;
+                }
             }
+            else
+            {
+                if (ItemsSource != null)
+                {
+                    IsDropDownOpen = false;
+                    ItemsSource = null;
+                }
+            }
+
+            changeHandler?.Invoke();
         }
 
         internal void SelectionDidChange(Action<string> changeHandler)
