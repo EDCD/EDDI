6440d3b3b22653641efefab0e8c7843cf22db449
Tkael <dragoncharmer84@hotmail.com>
2019-04-28 20:44:04 -0700

    Fix failing unit test.

--- Tests/JournalMonitorTests.cs
+++ Tests/JournalMonitorTests.cs
@@ -849,11 +849,12 @@ namespace UnitTests
         [TestMethod]
         public void TestSettlementApproachedEvent()
         {
-            string line = @"{ ""timestamp"":""2018-11-04T03:11:56Z"", ""event"":""ApproachSettlement"", ""Name"":""Bulmer Enterprise"", ""MarketID"":3510380288, ""Latitude"":-23.121552, ""Longitude"":-98.177559 }";
+            string line = @"{ ""timestamp"":""2018-11-04T03:11:56Z"", ""event"":""ApproachSettlement"", ""Name"":""Bulmer Enterprise"", ""MarketID"":3510380288, ""SystemAddress"": 670417429889, ""Latitude"":-23.121552, ""Longitude"":-98.177559 }";
             List<Event> events = JournalMonitor.ParseJournalEntry(line);
             SettlementApproachedEvent @event = (SettlementApproachedEvent)events[0];
 
             Assert.AreEqual(3510380288, @event.marketId);
+            Assert.AreEqual(670417429889, @event.systemAddress);
             Assert.AreEqual("Bulmer Enterprise", @event.name);
             Assert.AreEqual(-23.121552M, @event.latitude);
             Assert.AreEqual(-98.177559M, @event.longitude);
