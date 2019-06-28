548e2751c0e75d12ef4790c060d146b97975406e
Richard Buckle <github@sailmaker.co.uk>
2019-06-28 20:43:19 +0100

    Add test for non-null OAuth client ID

--- CompanionAppService/Properties/AssemblyInfo.cs
+++ CompanionAppService/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -43,3 +44,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Tests")]
---
+++ Tests/OAuthClientIDTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class OAuthClientIDTests
+    {
+        [TestMethod, TestCategory("Credentials")]
+        public void TestClientIDNotNull()
+        {
+            string ID = EddiCompanionAppService.ClientId.ID;
+            Assert.IsNotNull(ID);
+        }
+    }
+}
