218dd752c0dab97112e63d1162b5f88c98187778
Tkael <dragoncharmer84@hotmail.com>
2019-07-01 00:38:14 -0700

    Conflict definition tidy ups

--- DataDefinitions/Conflict.cs
+++ DataDefinitions/Conflict.cs
@@ -1,8 +1,10 @@
-﻿namespace EddiDataDefinitions
+﻿using System;
+
+namespace EddiDataDefinitions
 {
     public class Conflict
     {
-        public string conflict => conflictType?.localizedName;
+        public string state => factionState?.localizedName;
 
         public string status { get; set; }
 
@@ -10,9 +12,7 @@
 
         public int conflictdays => faction1dayswon + faction2dayswon;
 
-        public string winningfaction => winningFaction?.name;
-
-        public int winningdays { get; private set; }
+        public int margin => Math.Abs(faction1dayswon - faction2dayswon);
 
         // Faction 1
         public string faction1 => Faction1?.name;
@@ -25,7 +25,7 @@
         public int faction2dayswon { get; private set; }
 
         // Not intended to be user facing
-        public FactionState conflictType { get; private set; }
+        public FactionState factionState { get; private set; }
         public Faction Faction1 { get; private set; }
         public Faction Faction2 { get; private set; }
         private string faction1Stake { get; set; }
@@ -34,7 +34,7 @@
 
         public Conflict(FactionState conflictType, string status, Faction Faction1, string faction1Stake, int faction1WonDays, Faction Faction2, string faction2Stake, int faction2WonDays)
         {
-            this.conflictType = conflictType;
+            this.factionState = conflictType;
             this.status = status;
             this.Faction1 = Faction1;
             this.faction1Stake = faction1Stake;
@@ -42,17 +42,6 @@
             this.Faction2 = Faction2;
             this.faction2Stake = faction2Stake;
             this.faction2dayswon = faction2WonDays;
-
-            if (faction1dayswon > faction2dayswon)
-            {
-                winningFaction = Faction1;
-                winningdays = faction1dayswon;
-            }
-            else if (faction2dayswon > faction1dayswon)
-            {
-                winningFaction = Faction2;
-                winningdays = faction2dayswon;
-            }
         }
     }
 }
