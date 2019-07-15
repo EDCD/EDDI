efbf40b4d95708f2bf868041fabee6a97ac5fb56
Hoodathunk <naperken@gmail.com>
2019-07-14 21:31:13 -0500

    Added localized loadout descriptions. Fixed bugs. Tweaked unit test. Updated 'List launchbays' script.

--- DataDefinitions/Vehicle.cs
+++ DataDefinitions/Vehicle.cs
@@ -14,48 +14,42 @@ namespace EddiDataDefinitions
         public int rebuilds { get; private set; }
 
         [JsonIgnore]
-        public string localizedName => definition.localizedName;
+        private VehicleDefinition definition;
 
         [JsonIgnore]
-        public string invariantName => definition.invariantName;
+        public string localizedName => definition?.localizedName;
+
+        [JsonIgnore]
+        public string invariantName => definition?.invariantName;
 
         [JsonIgnore]
         [Obsolete("Please be explicit and use localizedName or invariantName")]
         public string name => localizedName;
         
         [JsonIgnore]
-        private VehicleDefinition definition;
+        private LoadoutDescription loadoutDescription;
 
-        public static Vehicle FromLoadoutName(string loadout)
-        {
-            if (loadout == null) { return null; }
+        [JsonIgnore]
+        public string localizedDescription => loadoutDescription?.localizedName;
 
-            string tidiedLoadout = loadout.ToLowerInvariant()
-                .Replace("_fighter_loadout", "")
-                .Replace("_name", "");
-            List<string> parts = tidiedLoadout.Split('_').ToList();
-
-            Vehicle vehicle = new Vehicle();
-            if (parts.Count == 2)
-            {
-                vehicle.definition = VehicleDefinition.FromEDName(parts[0]);
-                vehicle.loadout = parts[1];
-            }
-
-            return vehicle;
-        }
+        [JsonIgnore]
+        [Obsolete("Please be explicit and use localizedDescription")]
+        public string description => localizedDescription;
 
         public static Vehicle FromJson(int subslot, dynamic json)
         {
             if (json is null) { return null; }
 
             string edName = (string)json["name"];
+            string loadoutName = (string)json["loadoutName"];
+
             Vehicle vehicle = new Vehicle()
             {
                 loadout = (string)json["loadout"],
                 rebuilds = (int)json["rebuilds"],
                 subslot = subslot,
                 definition = VehicleDefinition.FromEDName(edName),
+                loadoutDescription = LoadoutDescription.FromLoadoutName(loadoutName)
             };
 
             return vehicle;
--- Tests/DataDefinitionTests.cs
+++ Tests/DataDefinitionTests.cs
@@ -198,6 +198,8 @@ namespace UnitTests
             JObject json = JsonConvert.DeserializeObject<JObject>(jsonString);
             Vehicle v0 = Vehicle.FromJson(0, json);
             Assert.AreEqual(0, v0.subslot, "testing v0 subslot from JSON");
+            Assert.AreEqual(v0.localizedName, "SRV Scarab");
+            Assert.AreEqual(v0.localizedDescription, "dual plasma repeaters");
 
             Vehicle v1 = Vehicle.FromJson(1, json);
             Assert.AreEqual(1, v1.subslot, "testing v1 subslot from JSON");
