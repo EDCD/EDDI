8830ecb4a179d9edfc78f7fafde497188d8fe81b
Tkael <dragoncharmer84@hotmail.com>
2019-07-28 22:56:11 -0700

    Create a definition for Ocellus... trying to conform everything to one edname just complicates things unnecessarily.

--- DataDefinitions/Properties/StationModels.Designer.cs
+++ DataDefinitions/Properties/StationModels.Designer.cs
@@ -19,7 +19,7 @@ namespace EddiDataDefinitions.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class StationModels {
@@ -129,6 +129,15 @@ namespace EddiDataDefinitions.Properties {
         public static string None {
             get {
                 return ResourceManager.GetString("None", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Ocellus Starport.
+        /// </summary>
+        public static string Ocellus {
+            get {
+                return ResourceManager.GetString("Ocellus", resourceCulture);
             }
         }
         
