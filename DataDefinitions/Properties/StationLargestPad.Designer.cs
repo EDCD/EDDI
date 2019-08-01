7fe0c8dee05ddbbde6da40e6d349bc2ca29d7ab5
Tkael <dragoncharmer84@hotmail.com>
2019-07-31 23:37:02 -0700

    Regenerate StationLargestPad.Designer.cs

--- DataDefinitions/Properties/StationLargestPad.Designer.cs
+++ DataDefinitions/Properties/StationLargestPad.Designer.cs
@@ -10,8 +10,8 @@
 
 namespace EddiDataDefinitions.Properties {
     using System;
-    
-    
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -39,7 +39,7 @@ namespace EddiDataDefinitions.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EddiDataDefinitions.Properties.LandingPadSize", typeof(StationLargestPad).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EddiDataDefinitions.Properties.StationLargestPad", typeof(StationLargestPad).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
