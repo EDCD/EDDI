e2f5939e840cdb41d469c63fe5794bbec7f2dd56
Tkael <dragoncharmer84@hotmail.com>
2019-08-02 16:02:46 -0700

    Create the basic structures for the Inara Service and the Inara Responder.

---
+++ EddiInaraService/InaraConfiguration.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using Utilities;
+
+namespace EddiInaraService
+{
+    /// <summary>Storage of credentials for a single Elite: Dangerous user to access EDSM</summary>
+    public class InaraConfiguration
+    {
+        [JsonProperty("apiKey")]
+        public string apiKey { get; set; }
+        [JsonProperty("commanderName")]
+        public string commanderName { get; set; }
+        [JsonProperty("lastSync")]
+
+        [JsonIgnore]
+        private string dataPath;
+
+        [JsonIgnore]
+        static readonly object fileLock = new object();
+
+        /// <summary>
+        /// Obtain credentials from a file.  If the file name is not supplied the the default
+        /// path of Constants.Data_DIR\inara.json is used
+        /// </summary>
+        public static InaraConfiguration FromFile(string filename=null)
+        {
+            if (filename == null)
+            {
+                filename = Constants.DATA_DIR + @"\inara.json";
+            }
+
+            InaraConfiguration configuration = new InaraConfiguration();
+            if (File.Exists(filename))
+            {
+                try
+                {
+                    string data = Files.Read(filename);
+                    if (data != null)
+                    {
+                        configuration = JsonConvert.DeserializeObject<InaraConfiguration>(data);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logging.Debug("Failed to read Inara configuration", ex);
+                }
+            }
+            if (configuration == null)
+            {
+                configuration = new InaraConfiguration();
+            }
+
+            configuration.dataPath = filename;
+            return configuration;
+        }
+
+        /// <summary>
+        /// Clear the information held by credentials.
+        /// </summary>
+        public void Clear()
+        {
+            apiKey = null;
+            commanderName = null;
+        }
+
+        /// <summary>
+        /// Obtain credentials to a file.  If the filename is not supplied then the path used
+        /// when reading in the credentials will be used, or the default path of 
+        /// Constants.Data_DIR\inara.json will be used
+        /// </summary>
+        public void ToFile(string filename=null)
+        {
+            if (filename == null)
+            {
+                filename = dataPath;
+            }
+            if (filename == null)
+            {
+                filename = Constants.DATA_DIR + @"\inara.json";
+            }
+
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            lock (fileLock)
+            {
+                Files.Write(filename, json);
+            }
+        }
+    }
+}
---
+++ InaraResponder/InaraResponder.cs
@@ -0,0 +1,91 @@
+﻿using Eddi;
+using EddiDataProviderService;
+using EddiEvents;
+using EddiInaraService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Utilities;
+
+namespace EddiInaraResponder
+{
+    public class InaraResponder : EDDIResponder
+    {
+        private Thread updateThread;
+
+        public string ResponderName()
+        {
+            return "Inara responder";
+        }
+
+        public string LocalizedResponderName()
+        {
+            return Properties.InaraResources.name;
+        }
+
+        public string ResponderVersion()
+        {
+            return "1.0.0";
+        }
+
+        public string ResponderDescription()
+        {
+            return Properties.InaraResources.desc;
+        }
+
+        public InaraResponder()
+        {
+            Logging.Info("Initialised " + ResponderName() + " " + ResponderVersion());
+        }
+
+        public bool Start()
+        {
+            Reload();
+            return InaraService.Instance != null;
+        }
+
+        public void Reload()
+        { }
+
+        public UserControl ConfigurationTabItem()
+        {
+            return new ConfigurationWindow();
+        }
+
+        public void Stop()
+        {
+            updateThread?.Abort();
+            updateThread = null;
+        }
+
+        public void Handle(Event theEvent)
+        {
+            if (EDDI.Instance.inCQC)
+            {
+                // We don't do anything whilst in CQC
+                return;
+            }
+
+            if (EDDI.Instance.inCrew)
+            {
+                // We don't do anything whilst in multicrew
+                return;
+            }
+
+            if (EDDI.Instance.inBeta)
+            {
+                // We don't send data whilst in beta
+                return;
+            }
+
+            if (InaraService.Instance != null)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
