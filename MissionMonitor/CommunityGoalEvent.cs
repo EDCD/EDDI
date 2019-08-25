c2c9e9667237e38ce044b664be58ea839a9ad59c
Tkael <dragoncharmer84@hotmail.com>
2019-08-24 22:43:00 -0700

    Add community goal events to the Inara responder.

--- MissionMonitor/CommunityGoalEvent.cs
+++ MissionMonitor/CommunityGoalEvent.cs
@@ -12,20 +12,22 @@ namespace EddiEvents
 
         static CommunityGoalEvent()
         {
-            VARIABLES.Add("cgid", "the unique id of the goal (this is a list if participating in multiple goals)");
-            VARIABLES.Add("name", "The description of the goal (this is a list if participating in multiple goals)");
-            VARIABLES.Add("system", "The system where the goal is located (this is a list if participating in multiple goals)");
-            VARIABLES.Add("station", "The station where the goal is located (this is a list if participating in multiple goals)");
-            VARIABLES.Add("expiry", "The expiration time for the goal in seconds (this is a list if participating in multiple goals)");
-            VARIABLES.Add("iscomplete", "The completion status of the goal (true/false) (this is a list if participating in multiple goals)");
-            VARIABLES.Add("total", "The community's current total contributions (this is a list if participating in multiple goals)");
-            VARIABLES.Add("contribution", "The commander's contribution (this is a list if participating in multiple goals)");
-            VARIABLES.Add("contributors", "The number of commanders participating (this is a list if participating in multiple goals)");
-            VARIABLES.Add("percentileband", "the commander's current rewards percentile (this is a list if participating in multiple goals)");
-            VARIABLES.Add("topranksize", "The number of commanders in the top rank (only for goals with a fixed size top rank) (this is a list if participating in multiple goals)");
-            VARIABLES.Add("toprank", "Whether the commander is currently in the top rank (true/false) (only for goals with a fixed size top rank) (this is a list if participating in multiple goals)");
-            VARIABLES.Add("tier", "The current tier of the goal (only once the 1st tier is reached) (this is a list if participating in multiple goals)");
-            VARIABLES.Add("tierreward", "The reward on offer for the current tier");
+            VARIABLES.Add("cgid", "the unique id of the goal (this is a list containing values for all active goals)");
+            VARIABLES.Add("name", "The description of the goal (this is a list containing values for all active goals)");
+            VARIABLES.Add("system", "The system where the goal is located (this is a list containing values for all active goals)");
+            VARIABLES.Add("station", "The station where the goal is located (this is a list containing values for all active goals)");
+            VARIABLES.Add("expiry", "The expiration time for the goal in seconds (this is a list containing values for all active goals)");
+            VARIABLES.Add("iscomplete", "The completion status of the goal (true/false) (this is a list containing values for all active goals)");
+            VARIABLES.Add("total", "The community's current total contributions (this is a list containing values for all active goals)");
+            VARIABLES.Add("contribution", "The commander's contribution (this is a list containing values for all active goals)");
+            VARIABLES.Add("contributors", "The number of commanders participating (this is a list containing values for all active goals)");
+            VARIABLES.Add("percentileband", "the commander's current rewards percentile (this is a list containing values for all active goals)");
+            VARIABLES.Add("topranksize", "The number of commanders in the top rank (only for goals with a fixed size top rank) (this is a list containing values for all active goals)");
+            VARIABLES.Add("toprank", "Whether the commander is currently in the top rank (true/false) (only for goals with a fixed size top rank) (this is a list containing values for all active goals)");
+            VARIABLES.Add("tier", "The current tier of the goal (only once the 1st tier is reached) (this is a list containing values for all active goals)");
+            VARIABLES.Add("tierreward", "The reward on offer for the current tier (this is a list containing values for all active goals)");
+            VARIABLES.Add("maxtier", "The maximum tier of the goal (this is a list containing values for all active goals)");
+            VARIABLES.Add("maxtierreward", "The reward on offer for the current tier (this is a list containing values for all active goals)");
         }
 
         public List<long> cgid { get; private set; }
@@ -56,14 +58,21 @@ namespace EddiEvents
 
         public List<long?> tierreward { get; private set; }
 
-        public CommunityGoalEvent(DateTime timestamp, List<long> cgid, List<string> name, List<string> system, List<string> station, List<long> expiry, List<bool> iscomplete, List<int> total, List<int> contribution, List<int> contributors, List<decimal> percentileband, List<int?> topranksize, List<bool?> toprank, List<string> tier, List<long?> tierreward) : base(timestamp, NAME)
+        public List<string> maxtier { get; private set; }
 
+        public List<long?> maxtierreward { get; private set; }
+
+        // Not intended to be user facing
+        public List<DateTime> expiryDateTime { get; private set; }
+
+        public CommunityGoalEvent(DateTime timestamp, List<long> cgid, List<string> name, List<string> system, List<string> station, List<long> expiry, List<DateTime> expiryDateTime, List<bool> iscomplete, List<int> total, List<int> contribution, List<int> contributors, List<decimal> percentileband, List<int?> topranksize, List<bool?> toprank, List<string> tier, List<long?> tierreward, List<string> maxtier, List<long?> maxtierreward) : base(timestamp, NAME)
         {
             this.cgid = cgid;
             this.name = name;
             this.system = system;
             this.station = station;
             this.expiry = expiry;
+            this.expiryDateTime = expiryDateTime;
             this.iscomplete = iscomplete;
             this.total = total;
             this.contribution = contribution;
@@ -73,6 +82,8 @@ namespace EddiEvents
             this.toprank = toprank;
             this.tier = tier;
             this.tierreward = tierreward;
+            this.maxtier = maxtier;
+            this.maxtierreward = maxtierreward;
         }
     }
 }
