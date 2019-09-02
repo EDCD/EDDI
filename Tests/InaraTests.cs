9684eba4962efc2b7938cf9f1624e31cdfae47a8
Tkael <dragoncharmer84@hotmail.com>
2019-09-01 23:29:48 -0700

    Move tests to the `IntegrationTests` namespace.

--- Tests/InaraTests.cs
+++ Tests/InaraTests.cs
@@ -1,14 +1,10 @@
-﻿using EddiInaraResponder;
-using EddiInaraService;
+﻿using EddiInaraService;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnitTests;
 
-namespace Apitests
+namespace IntegrationTests
 {
     [TestClass]
     public class InaraTests : TestBase
