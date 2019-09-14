02cd24ae38d097255aea38d97ce53ef0808eb9cb
Richard Buckle <github@sailmaker.co.uk>
2019-09-15 00:53:19 +0100

    Remove dead code

--- Tests/JournalMonitorTests.cs
+++ Tests/JournalMonitorTests.cs
@@ -474,10 +474,8 @@ namespace UnitTests
         public void TestFriends()
         {
             string line = "{ \"timestamp\":\"2017-08-24T17:22:03Z\", \"event\":\"Friends\", \"Status\":\"Online\", \"Name\":\"_Testy_McTest_\" }";
-            string line2 = "{ \"timestamp\":\"2017-08-24T17:22:03Z\", \"event\":\"Friends\", \"Status\":\"Offline\", \"Name\":\"_Testy_McTest_\" }";
 
             List<Event> events = JournalMonitor.ParseJournalEntry(line);
-            events = JournalMonitor.ParseJournalEntry(line2);
 
             /// Since this friend is unknown to us, the first time we see this friend no event should trigger. 
             /// Only the second line, registering the status as offline, should be registered as an event.
