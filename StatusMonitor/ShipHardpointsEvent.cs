a81e9e3101eaf618feb619383e1ce1958ecb871b
Daniel Ricardo <daniel.k.ricardo@gmail.com>
2019-12-31 17:09:58 +0200

    Create a ShipHardpointsEvent

---
+++ StatusMonitor/ShipHardpointsEvent.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace EddiEvents
+{
+    public class ShipHardpointsEvent : Event
+    {
+        public const string NAME = "Hardpoints";
+        public const string DESCRIPTION = "Triggered when you deploy or retract your hardpoints";
+        public const string SAMPLE = "null";
+
+        public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
+
+        static ShipHardpointsEvent()
+        {
+            VARIABLES.Add("deployed", "A boolean value. True if you hardpoints are deployed.");
+        }
+
+        [JsonProperty("deployed")]
+        public bool deployed { get; private set; }
+
+        public ShipHardpointsEvent(DateTime timestamp, bool deployed) : base(timestamp, NAME)
+        {
+            this.deployed = deployed;
+        }
+    }
+}
