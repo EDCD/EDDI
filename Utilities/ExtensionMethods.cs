560b15579b058dd36b20a4b6cd3dfd41be963a56
Tkael <dragoncharmer84@hotmail.com>
2020-01-03 22:35:04 -0800

    Add extension method for deep equality comparison between objects. Note: I also tested equality comparison via reflection but found that method to be both slower and more error prone than comparison via json strings.

---
+++ Utilities/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Utilities
+{
+    public static class CustomExtensionMethods
+    {
+        public static bool DeepEquals(this object obj, object another)
+        {
+            if (ReferenceEquals(obj, another)) { return true; }
+            if ((obj == null) || (another == null)) { return false; }
+            if (obj.GetType() != another.GetType()) { return false; }
+
+            var objJson = JsonConvert.SerializeObject(obj);
+            var anotherJson = JsonConvert.SerializeObject(another);
+
+            return objJson == anotherJson;
+        }
+    }
+}
