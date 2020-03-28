99d49d3125f8aa988d1134cf85f91b6172361066
Tkael <dragoncharmer84@hotmail.com>
2020-03-28 01:46:16 -0700

    Inara code-behind re-written. Implement binding, validation, a short delay to allow for typing, a `OnConfigurationUpdated` method, INotifyPropertyChanged, INotifyDataErrorInfo, and refactor.

--- InaraResponder/ConfigurationWindow.xaml.cs
+++ InaraResponder/ConfigurationWindow.xaml.cs
@@ -1,36 +1,130 @@
-﻿using Eddi;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using EddiInaraService;
+using System.Timers;
 using System.Windows.Controls;
 
 namespace EddiInaraResponder
 {
-    /// <summary>
-    /// Interaction logic for ConfigurationWindow.xaml
-    /// </summary>
-    public partial class ConfigurationWindow : UserControl
+    /// <summary> Interaction logic for ConfigurationWindow.xaml </summary>
+    public partial class ConfigurationWindow : UserControl, INotifyPropertyChanged, INotifyDataErrorInfo
     {
+        // Set up a timer... wait 3 seconds before reconfiguring the InaraService for any change in the API key
+        private const int delayMilliseconds = 3000;
+        private readonly Timer inputTimer = new Timer(delayMilliseconds);
+
+        public string apiKey
+        {
+            get => _apiKey;
+            set
+            {
+                OnPropertyChanged();
+                _apiKey = value;
+            }
+        }
+        private string _apiKey;
+
         public ConfigurationWindow()
         {
+            // Subscribe to events that require our attention
+            InaraConfiguration.ConfigurationUpdated += (s, e) => { OnConfigurationUpdated((InaraConfiguration)s); };
+            inputTimer.Elapsed += InputTimer_Elapsed;
+
+            DataContext = this;
             InitializeComponent();
 
             InaraConfiguration inaraConfiguration = InaraConfiguration.FromFile();
             inaraApiKeyTextBox.Text = inaraConfiguration.apiKey;
+            SetAPIKeyValidity(inaraConfiguration.isAPIkeyValid);
         }
 
-        private void inaraApiKeyChanged(object sender, TextChangedEventArgs e)
+        private void InaraApiKeyChanged(object sender, TextChangedEventArgs e)
         {
-            updateInaraConfiguration();
+            if (sender is TextBox textBox)
+            {
+                if (textBox.IsLoaded && textBox.Name == "inaraApiKeyTextBox")
+                {
+                    SetAPIKeyValidity(true);
+                    inputTimer.Stop();
+                    inputTimer.Start();
+                }
+            }
         }
 
-        private void updateInaraConfiguration()
+        private void InputTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            inputTimer.Stop();
+            UpdateConfiguration();
+        }
+
+        private void UpdateConfiguration()
         {
             InaraConfiguration inaraConfiguration = InaraConfiguration.FromFile();
-            if (!string.IsNullOrWhiteSpace(inaraApiKeyTextBox.Text))
-            {
-                inaraConfiguration.apiKey = inaraApiKeyTextBox.Text.Trim();
-            }
+
+            // Reset API key validity when it is edited.
+            inaraConfiguration.isAPIkeyValid = true;
+
+            // Update the changed API key in our configuration
+            inaraConfiguration.apiKey = apiKey;
+
+            // Save and reload
             inaraConfiguration.ToFile();
-            EDDI.Instance.Reload("Inara responder");
+        }
+
+        private void OnConfigurationUpdated(InaraConfiguration inaraConfiguration)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                SetAPIKeyValidity(inaraConfiguration.isAPIkeyValid);
+            });
+
+        }
+
+        private void SetAPIKeyValidity(bool isAPIkeyValid)
+        {
+            if (isAPIkeyValid)
+            {
+                ClearErrors(nameof(apiKey));
+            }
+            else
+            {
+                ReportError(nameof(apiKey), Properties.InaraResources.invalidKeyErr);
+            }
+            inaraApiKeyTextBox.Text = apiKey; // Forces validation to update
+        }
+
+        // Implement INotifyDataErrorInfo for validation
+        public void ReportError(string propertyName, string errorMessage)
+        {
+            if (string.IsNullOrEmpty(propertyName)) { return; }
+            if (!Errors.ContainsKey(propertyName)) { Errors.Add(propertyName, new List<string>()); }
+            Errors[propertyName].Add(errorMessage);
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName) || (!HasErrors)) { return null; }
+            if (!Errors.ContainsKey(propertyName)) { Errors.Add(propertyName, new List<string>()); }
+            return Errors[propertyName];
+        }
+        public void ClearErrors(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName) || (!HasErrors)) { return; }
+            if (Errors.ContainsKey(propertyName)) { Errors.Remove(propertyName); }
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+        private readonly Dictionary<string, List<string>> Errors = new Dictionary<string, List<string>>();
+        public bool HasErrors => Errors.Count > 0;
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        // Implement INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) 
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
         }
     }
 }
