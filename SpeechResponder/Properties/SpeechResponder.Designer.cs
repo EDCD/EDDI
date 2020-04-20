1dc9b7281a3106ba159c7a49b25f84fa7aeb08a6
Jean-Pierre Bachmann <Jean-Pierre_Bachmann@live.de>
2020-04-20 02:32:23 +0200

    #1742: Done

--- SpeechResponder/Properties/SpeechResponder.Designer.cs
+++ SpeechResponder/Properties/SpeechResponder.Designer.cs
@@ -277,6 +277,24 @@ namespace EddiSpeechResponder.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to EDDI has recovered a script after the application has Crashed. Do you want to open it?.
+        /// </summary>
+        public static string messagebox_recoveredScript {
+            get {
+                return ResourceManager.GetString("messagebox_recoveredScript", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Recovered Script.
+        /// </summary>
+        public static string messagebox_recoveredScript_title {
+            get {
+                return ResourceManager.GetString("messagebox_recoveredScript_title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Speech Responder.
         /// </summary>
         public static string name {
