33727dd40f7a5010f6661e4f972f7d0554171dac
Jean-Pierre Bachmann <Jean-Pierre_Bachmann@live.de>
2020-04-20 02:32:23 +0200

    #1742: Done

--- SpeechResponder/EditScriptWindow.xaml.cs
+++ SpeechResponder/EditScriptWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Eddi;
+﻿using System;
+using Eddi;
 using EddiEvents;
 using EddiJournalMonitor;
 using EddiShipMonitor;
@@ -6,199 +7,217 @@ using ICSharpCode.AvalonEdit.Search;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows;
+using EddiSpeechResponder.Service;
 using Utilities;
 
 namespace EddiSpeechResponder
 {
-    /// <summary>
-    /// Interaction logic for EditScriptWindow.xaml
-    /// </summary>
-    public partial class EditScriptWindow : Window, INotifyPropertyChanged
-    {
-        private readonly Dictionary<string, Script> scripts;
-        private Script script;
-        private readonly string originalName;
+	/// <summary>
+	/// Interaction logic for EditScriptWindow.xaml
+	/// </summary>
+	public partial class EditScriptWindow : Window, INotifyPropertyChanged
+	{
+		private readonly Dictionary<string, Script> scripts;
+		private Script script;
+		private readonly string originalName;
 
-        private string scriptName;
-        public string ScriptName
-        {
-            get { return scriptName; }
-            set { scriptName = value; OnPropertyChanged("ScriptName"); }
-        }
-        private string scriptDescription;
-        public string ScriptDescription
-        {
-            get { return scriptDescription; }
-            set { scriptDescription = value; OnPropertyChanged("ScriptDescription"); }
-        }
-        private string scriptValue;
-        public string ScriptValue
-        {
-            get { return scriptValue; }
-            set
-            {
-                scriptValue = string.IsNullOrWhiteSpace(value) ? null : value;
-                OnPropertyChanged("ScriptValue");
-            }
-        }
+		private string scriptName;
+		public string ScriptName
+		{
+			get { return scriptName; }
+			set { scriptName = value; OnPropertyChanged("ScriptName"); }
+		}
+		private string scriptDescription;
+		public string ScriptDescription
+		{
+			get { return scriptDescription; }
+			set { scriptDescription = value; OnPropertyChanged("ScriptDescription"); }
+		}
+		private string scriptValue;
+		public string ScriptValue
+		{
+			get { return scriptValue; }
+			set
+			{
+				scriptValue = string.IsNullOrWhiteSpace(value) ? null : value;
+				OnPropertyChanged("ScriptValue");
+			}
+		}
 
-        private bool responder;
-        public bool Responder
-        {
-            get { return responder; }
-            set { responder = value; OnPropertyChanged("Responder"); }
-        }
+		private bool responder;
+		public bool Responder
+		{
+			get { return responder; }
+			set { responder = value; OnPropertyChanged("Responder"); }
+		}
 
-        public string ScriptDefaultValue;
+		public string ScriptDefaultValue;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged(string name)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
 
-        public EditScriptWindow(Dictionary<string, Script> scripts, string name)
-        {
-            InitializeComponent();
-            DataContext = this;
-            SearchPanel.Install(scriptView);
+		public ScriptRecoveryService ScriptRecoveryService { get; set; }
 
-            this.scripts = scripts;
-            this.originalName = name;
+		public EditScriptWindow(Dictionary<string, Script> scripts, string name)
+		{
+			InitializeComponent();
+			DataContext = this;
+			SearchPanel.Install(scriptView);
 
-            scripts.TryGetValue(name, out script);
-            if (script == null)
-            {
-                // This is a new script
-                ScriptName = "New script";
-                ScriptDescription = null;
-                ScriptValue = null;
-                Responder = false;
-            }
-            else
-            {
-                // This is an existing script
-                ScriptName = script.Name;
-                ScriptDescription = script.Description;
-                ScriptValue = script.Value;
-                ScriptDefaultValue = script.defaultValue;
-                Responder = script.Responder;
-            }
+			this.scripts = scripts;
+			this.originalName = name;
 
-            // See if there is the default value for this script is empty
-            if (string.IsNullOrWhiteSpace(ScriptDefaultValue))
-            {
-                // No default; disable reset and show
-                showDiffButton.IsEnabled = false;
-                resetToDefaultButton.IsEnabled = false;
-            }
+			scripts.TryGetValue(name, out script);
+			if (script == null)
+			{
+				// This is a new script
+				ScriptName = "New script";
+				ScriptDescription = null;
+				ScriptValue = null;
+				Responder = false;
+			}
+			else
+			{
+				// This is an existing script
+				ScriptName = script.Name;
+				ScriptDescription = script.Description;
+				ScriptValue = script.Value;
+				ScriptDefaultValue = script.defaultValue;
+				Responder = script.Responder;
+			}
 
-            scriptView.Text = scriptValue;
-        }
+			// See if there is the default value for this script is empty
+			if (string.IsNullOrWhiteSpace(ScriptDefaultValue))
+			{
+				// No default; disable reset and show
+				showDiffButton.IsEnabled = false;
+				resetToDefaultButton.IsEnabled = false;
+			}
 
-        private void acceptButtonClick(object sender, RoutedEventArgs e)
-        {
-            // Update the script
-            string newScriptText = scriptView.Text;
-            if (script != null)
-            {
-                Script newScript = new Script(scriptName, scriptDescription, script?.Responder ?? false, newScriptText, script.Priority, script.defaultValue);
-                script = newScript;
-            }
+			scriptView.Text = scriptValue;
+			ScriptRecoveryService = new ScriptRecoveryService(this);
+			ScriptRecoveryService.BeginScriptRecovery(ScriptName);
+			scriptView.TextChanged += ScriptView_TextChanged;
+		}
 
-            Script defaultScript = null;
-            if (Personality.Default().Scripts?.TryGetValue(script.Name, out defaultScript) ?? false)
-            {
-                script = Personality.UpgradeScript(script, defaultScript);
-            }
+		private void ScriptView_TextChanged(object sender, System.EventArgs e)
+		{
+			ScriptValue = scriptView.Text;
+		}
 
-            // Might be updating an existing script so remove it from the list before adding
-            scripts.Remove(originalName);
+		protected override void OnClosed(EventArgs e)
+		{
+			base.OnClosed(e);
+			ScriptRecoveryService.StopScriptRecovery();
+		}
 
-            scripts.Add(script.Name, script);
+		private void acceptButtonClick(object sender, RoutedEventArgs e)
+		{
+			// Update the script
+			string newScriptText = scriptView.Text;
+			if (script != null)
+			{
+				Script newScript = new Script(scriptName, scriptDescription, script?.Responder ?? false, newScriptText, script.Priority, script.defaultValue);
+				script = newScript;
+			}
 
-            DialogResult = true;
-            this.Close();
-        }
+			Script defaultScript = null;
+			if (Personality.Default().Scripts?.TryGetValue(script.Name, out defaultScript) ?? false)
+			{
+				script = Personality.UpgradeScript(script, defaultScript);
+			}
 
-        private void cancelButtonClick(object sender, RoutedEventArgs e)
-        {
-            DialogResult = false;
-            this.Close();
-        }
+			// Might be updating an existing script so remove it from the list before adding
+			scripts.Remove(originalName);
 
-        private void helpButtonClick(object sender, RoutedEventArgs e)
-        {
-            MarkdownWindow helpWindow = new MarkdownWindow("Help.md");
-            helpWindow.Show();
-        }
+			scripts.Add(script.Name, script);
 
-        private void variablesButtonClick(object sender, RoutedEventArgs e)
-        {
-            VariablesWindow variablesWindow = new VariablesWindow(ScriptName);
-            variablesWindow.Show();
-        }
+			DialogResult = true;
+			this.Close();
+		}
 
-        private void resetButtonClick(object sender, RoutedEventArgs e)
-        {
-            // Resetting the script resets it to its value in the default personality
-            ScriptValue = ScriptDefaultValue;
-            scriptView.Text = ScriptValue;
-        }
+		private void cancelButtonClick(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+			this.Close();
+		}
 
-        private void testButtonClick(object sender, RoutedEventArgs e)
-        {
-            // Splice the new script in to the existing scripts
-            ScriptValue = scriptView.Text;
-            Dictionary<string, Script> newScripts = new Dictionary<string, Script>(scripts);
-            Script testScript = new Script(ScriptName, ScriptDescription, false, ScriptValue);
-            newScripts.Remove(ScriptName);
-            newScripts.Add(ScriptName, testScript);
+		private void helpButtonClick(object sender, RoutedEventArgs e)
+		{
+			MarkdownWindow helpWindow = new MarkdownWindow("Help.md");
+			helpWindow.Show();
+		}
 
-            SpeechResponder speechResponder = new SpeechResponder();
-            speechResponder.Start();
+		private void variablesButtonClick(object sender, RoutedEventArgs e)
+		{
+			VariablesWindow variablesWindow = new VariablesWindow(ScriptName);
+			variablesWindow.Show();
+		}
 
-            // See if we have a sample
-            List<Event> sampleEvents;
-            object sample = Events.SampleByName(script.Name);
-            if (sample == null)
-            {
-                sampleEvents = new List<Event>();
-            }
-            else if (sample is string)
-            {
-                // It's a string so a journal entry.  Parse it
-                sampleEvents = JournalMonitor.ParseJournalEntry((string)sample);
-            }
-            else if (sample is Event)
-            {
-                // It's a direct event
-                sampleEvents = new List<Event>() { (Event)sample };
-            }
-            else
-            {
-                Logging.Warn("Unknown sample type " + sample.GetType());
-                sampleEvents = new List<Event>();
-            }
+		private void resetButtonClick(object sender, RoutedEventArgs e)
+		{
+			// Resetting the script resets it to its value in the default personality
+			ScriptValue = ScriptDefaultValue;
+			scriptView.Text = ScriptValue;
+		}
 
-            ScriptResolver scriptResolver = new ScriptResolver(newScripts);
-            if (sampleEvents.Count == 0)
-            {
-                sampleEvents.Add(null);
-            }
-            foreach (Event sampleEvent in sampleEvents)
-            {
-                speechResponder.Say(scriptResolver, ((ShipMonitor)EDDI.Instance.ObtainMonitor("Ship monitor"))?.GetCurrentShip(), ScriptName, sampleEvent, scriptResolver.priority(script.Name));
-            }
-        }
+		private void testButtonClick(object sender, RoutedEventArgs e)
+		{
+			ScriptRecoveryService.SaveRecoveryScript(ScriptValue, ScriptName, ScriptDescription, Responder);
+			// Splice the new script in to the existing scripts
+			ScriptValue = scriptView.Text;
+			Dictionary<string, Script> newScripts = new Dictionary<string, Script>(scripts);
+			Script testScript = new Script(ScriptName, ScriptDescription, false, ScriptValue);
+			newScripts.Remove(ScriptName);
+			newScripts.Add(ScriptName, testScript);
 
-        private void showDiffButtonClick(object sender, RoutedEventArgs e)
-        {
-            ScriptValue = scriptView.Text;
-            if (!string.IsNullOrWhiteSpace(ScriptDefaultValue))
-            {
-                new ShowDiffWindow(ScriptDefaultValue, ScriptValue).Show();
-            }
-        }
-    }
+			SpeechResponder speechResponder = new SpeechResponder();
+			speechResponder.Start();
+
+			// See if we have a sample
+			List<Event> sampleEvents;
+			object sample = Events.SampleByName(script.Name);
+			if (sample == null)
+			{
+				sampleEvents = new List<Event>();
+			}
+			else if (sample is string)
+			{
+				// It's a string so a journal entry.  Parse it
+				sampleEvents = JournalMonitor.ParseJournalEntry((string)sample);
+			}
+			else if (sample is Event)
+			{
+				// It's a direct event
+				sampleEvents = new List<Event>() { (Event)sample };
+			}
+			else
+			{
+				Logging.Warn("Unknown sample type " + sample.GetType());
+				sampleEvents = new List<Event>();
+			}
+
+			ScriptResolver scriptResolver = new ScriptResolver(newScripts);
+			if (sampleEvents.Count == 0)
+			{
+				sampleEvents.Add(null);
+			}
+			foreach (Event sampleEvent in sampleEvents)
+			{
+				speechResponder.Say(scriptResolver, ((ShipMonitor)EDDI.Instance.ObtainMonitor("Ship monitor"))?.GetCurrentShip(), ScriptName, sampleEvent, scriptResolver.priority(script.Name));
+			}
+		}
+
+		private void showDiffButtonClick(object sender, RoutedEventArgs e)
+		{
+			ScriptValue = scriptView.Text;
+			if (!string.IsNullOrWhiteSpace(ScriptDefaultValue))
+			{
+				new ShowDiffWindow(ScriptDefaultValue, ScriptValue).Show();
+			}
+		}
+	}
 }
---
+++ SpeechResponder/Service/ScriptRecoveryService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace EddiSpeechResponder.Service
+{
+	public class ScriptRecoveryService
+	{
+		static ScriptRecoveryService()
+		{
+			WorkingDirectory = Directory.GetCurrentDirectory();
+		}
+
+		public ScriptRecoveryService(EditScriptWindow scriptWindow)
+		{
+			_scriptWindow = scriptWindow;
+			_lockRoot = new object();
+		}
+
+		private static readonly string WorkingDirectory;
+		private string _tempFileName;
+		private EditScriptWindow _scriptWindow;
+		private bool _scriptSaveCallGuard;
+		private readonly object _lockRoot;
+
+		public static Script GetRecoveredScript()
+		{
+			var recoveringScript = Directory.EnumerateFiles(WorkingDirectory, "*.temp").FirstOrDefault();
+			if (recoveringScript == null)
+			{
+				return null;
+			}
+
+			return JsonConvert.DeserializeObject<Script>(File.ReadAllText(recoveringScript));
+		}
+
+		/// <summary>
+		///		Will be called when ether the name of the script has changed or the script edit window was opened
+		/// </summary>
+		/// <param name="scriptName"></param>
+		public void BeginScriptRecovery(string scriptName)
+		{
+			_tempFileName = Path.Combine(WorkingDirectory, scriptName + ".temp");
+
+			if (File.Exists(_tempFileName))
+			{
+				File.Delete(_tempFileName);
+			}
+
+			_scriptWindow.PropertyChanged += _scriptWindow_PropertyChanged;
+		}
+
+		private void _scriptWindow_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName == nameof(EditScriptWindow.ScriptValue))
+			{
+				//the script value has changed. Begin the callguard and save the script value
+				BeginScriptSave(_scriptWindow);
+			}
+		}
+
+		private void BeginScriptSave(EditScriptWindow window)
+		{
+			//this is guaranteed to run in the dispatcher so no worry about non locked accessing
+			if (_scriptSaveCallGuard)
+			{
+				return;
+			}
+
+			_scriptSaveCallGuard = true;
+
+			Task.Run(async () =>
+			{
+				try
+				{
+					await Task.Delay(TimeSpan.FromSeconds(3));
+					SaveRecoveryScript(window.ScriptValue, window.ScriptName, window.ScriptDescription, window.Responder);
+				}
+				finally
+				{
+					_scriptSaveCallGuard = false;
+				}
+			});
+		}
+
+		/// <summary>
+		///		Should be called periodically and saves the script into the temp file
+		/// </summary>
+		public void SaveRecoveryScript(string scriptValue,
+			string scriptName,
+			string scriptDescription,
+			bool isResponder)
+		{
+			lock (_lockRoot)
+			{
+				var script = new Script(scriptName, scriptDescription, isResponder, scriptValue);
+				var serializeObject = JsonConvert.SerializeObject(script);
+				File.WriteAllText(_tempFileName, serializeObject);
+			}
+		}
+
+		/// <summary>
+		///		The script editor was closed and the temp file is no long needed
+		/// </summary>
+		public void StopScriptRecovery()
+		{
+			if (File.Exists(_tempFileName))
+			{
+				File.Delete(_tempFileName);
+			}
+			_scriptWindow.PropertyChanged -= _scriptWindow_PropertyChanged;
+		}
+	}
+}
