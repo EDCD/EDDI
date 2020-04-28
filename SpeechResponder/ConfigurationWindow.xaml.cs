55c6f0c6e1f212aa09126ab0b267e13323d19819
Richard Buckle <github@sailmaker.co.uk>
2020-04-28 23:38:18 +0100

    Prune and sort headers

--- SpeechResponder/ConfigurationWindow.xaml.cs
+++ SpeechResponder/ConfigurationWindow.xaml.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using Eddi;
+﻿using Eddi;
 using EddiEvents;
 using EddiJournalMonitor;
 using EddiShipMonitor;
+using EddiSpeechResponder.Service;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Security.AccessControl;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Threading;
-using EddiSpeechResponder.Service;
 using Utilities;
 
 namespace EddiSpeechResponder
