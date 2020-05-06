85c9d13c5aefed0112c1d3525acfc13bad73b23c
Tkael <dragoncharmer84@hotmail.com>
2020-05-06 00:01:29 -0700

    Fix invalid IPA for the `Electra` star system Corrects an exception like ``` SpeechService:speak Speech failed:  System.FormatException: 'phoneme' attribute in 'item' not valid.    at System.Speech.Internal.Synthesis.VoiceSynthesis.Speak(Prompt prompt)    at System.Speech.Synthesis.SpeechSynthesizer.Speak(Prompt prompt)    at System.Speech.Synthesis.SpeechSynthesizer.SpeakSsml(String textToSpeak)    at EddiSpeechService.SpeechService.<>c__DisplayClass40_0.<speak>b__0() in %USERPROFILE%\source\repos\EDDI\SpeechService\SpeechService.cs:line 380 ```

--- SpeechService/Properties/Phonetics.Designer.cs
+++ SpeechService/Properties/Phonetics.Designer.cs
@@ -19,7 +19,7 @@ namespace EddiSpeechService.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Phonetics {
@@ -547,7 +547,7 @@ namespace EddiSpeechService.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ᵻˈlɛktrə.
+        ///   Looks up a localized string similar to iˈlɛktrə.
         /// </summary>
         public static string electra {
             get {
