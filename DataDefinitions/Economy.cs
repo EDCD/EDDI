c77d3ecd6b7c44060100e094557389b38f7566e1
Tkael <dragoncharmer84@hotmail.com>
2020-05-12 22:32:58 -0700

    Handle null values passed to the Economy.FromEDName() method

--- DataDefinitions/Economy.cs
+++ DataDefinitions/Economy.cs
@@ -42,6 +42,7 @@ namespace EddiDataDefinitions
         public new static Economy FromEDName(string edname)
         {
             // Economy names from the journal are prefixed with "$economy_" and sufficed with ";" while economy names from the Frontier API are not.
+            if (string.IsNullOrEmpty(edname)) { return None; }
             string tidiedName = edname.Replace("$economy_", "").Replace(";", "");
             return ResourceBasedLocalizedEDName<Economy>.FromEDName(tidiedName);
         }
