adf54956fb036e2db9a801aa915659738a98e02a
Richard Buckle <github@sailmaker.co.uk>
2020-06-08 23:36:43 +0100

    Add EddiCore project

--- EddiCore/Properties/AssemblyInfo.cs
+++ EddiCore/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
