593ea79e03b6e43890bdcf9f82449526d6c47e88
Hoodathunk <naperken@gmail.com>
2020-06-12 14:43:01 -0500

    Added 'NavRoute' event handler

--- CompanionAppService/ClientId.cs
+++ CompanionAppService/ClientId.cs
@@ -4,6 +4,6 @@ namespace EddiCompanionAppService
     class ClientId
     {
         // replace with official FDev Client ID. We don't need the Client Secret for PKCE authentication
-        internal static string ID = null;
+        internal static string ID = "6436e255-c28b-44c3-b3c3-9d183518cd34";
     }
 }
