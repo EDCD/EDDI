dcf426f99daa64b3f12422fc7c247db065d2031f
Tkael <dragoncharmer84@hotmail.com>
2020-06-16 16:20:59 -0700

    Move LockManager to a separate class where it can be used generically.

--- Utilities/Files.cs
+++ Utilities/Files.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -276,60 +275,5 @@ namespace Utilities
             //file is not locked
             return false;
         }
-    }
-
-    // Code from https://www.codeproject.com/Tips/1190802/File-Locking-in-a-Multi-Threaded-Environment
-    // The code maintains a concurrent dictionary of lock objects and keeps track of how many calls are in the queue for
-    // accessing each filename. If the count goes down to 0, the key is removed from the dictionary as a cleanup mechanism.
-    class LockManager
-    {
-        private static readonly ConcurrentDictionary<string, lobj> _locks =
-                new ConcurrentDictionary<string, lobj>();
-
-        public static void GetLock(string filename, Action action)
-        {
-            lock (GetLock(filename))
-            {
-                try 
-                {
-                    action();
-                }
-                finally
-                {
-                    Unlock(filename);
-                }
-            }
-        }
-
-        private static lobj GetLock(string filename)
-        {
-            if (_locks.TryGetValue(filename.ToLower(), out var o))
-            {
-                o.count++;
-                return o;
-            }
-            else
-            {
-                o = new lobj();
-                _locks.TryAdd(filename.ToLower(), o);
-                o.count++;
-                return o;
-            }
-        }
-
-        private static void Unlock(string filename)
-        {
-            if (_locks.TryGetValue(filename.ToLower(), out var o))
-            {
-                o.count--;
-                if (o.count != 0) { return; }
-                _locks.TryRemove(filename.ToLower(), out lobj _);
-            }
-        }
-    }
-
-    class lobj
-    {
-        public int count = 0;
     }
 }
