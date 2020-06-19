7c18f5a55532b0f4a3ab7d052090092a944c8916
Tkael <dragoncharmer84@hotmail.com>
2020-06-19 14:27:41 -0700

    To Cottle version 1.6.2

--- Tests/ScriptResolverTest.cs
+++ Tests/ScriptResolverTest.cs
@@ -1,28 +1,33 @@
-﻿using Cottle.Documents;
-using Cottle.Functions;
+﻿using Cottle;
+using Cottle.Values;
 using EddiSpeechResponder;
 using EddiSpeechService;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using Cottle;
 
 namespace UnitTests
 {
     [TestClass]
     public class ScriptResolverTest : TestBase
     {
+        DocumentConfiguration setting;
+
         [TestInitialize]
         public void start()
         {
             MakeSafe();
+            setting = new DocumentConfiguration
+            {
+                Trimmer = DocumentConfiguration.TrimRepeatedWhitespaces
+            };
         }
 
         [TestMethod]
         public void TestTemplateSimple()
         {
-            var document = new SimpleDocument(@"Hello {name}!");
+            var document = Document.CreateDefault(@"Hello {name}!", setting).DocumentOrThrow;
             var vars = new Dictionary<Value, Value>();
             vars["name"] = "world";
             var result = document.Render(Context.CreateBuiltin(vars));
@@ -32,9 +37,9 @@ namespace UnitTests
         [TestMethod]
         public void TestTemplateFunctional()
         {
-            var document = new SimpleDocument(@"You are entering the {P(system)} system.");
+            var document = Document.CreateDefault(@"You are entering the {P(system)} system.", setting).DocumentOrThrow;
             var vars = new Dictionary<Value, Value>();
-            vars["P"] = new NativeFunction((values) =>
+            vars["P"] = new FunctionValue(Function.Create((state, values, output) =>
             {
                 return Translations.GetTranslation(values[0].AsString);
             }, 1);
@@ -46,7 +51,7 @@ namespace UnitTests
         [TestMethod]
         public void TestTemplateConditional()
         {
-            var document = new SimpleDocument("{if value = 1:foo|else:{if value = 2:bar|else:baz}}");
+            var document = Document.CreateDefault("{if value = 1:foo|else:{if value = 2:bar|else:baz}}", setting).DocumentOrThrow;
             var vars = new Dictionary<Value, Value>();
             vars["value"] = 1;
             var result = document.Render(Context.CreateBuiltin(vars));
@@ -63,12 +68,12 @@ namespace UnitTests
         public void TestTemplateOneOf()
         {
             Random random = new Random();
-            var document = new SimpleDocument("The letter is {OneOf(\"a\", \"b\", \"c\", \"d\", null)}.");
+            var document = Document.CreateDefault("The letter is {OneOf(\"a\", \"b\", \"c\", \"d\", null)}.", setting).DocumentOrThrow;
             var vars = new Dictionary<Value, Value>();
-            vars["OneOf"] = new NativeFunction((values) =>
+            vars["OneOf"] = new FunctionValue(Function.Create((state, values, output) =>
             {
                 return values[random.Next(values.Count)];
-            });
+            }));
             vars["system"] = "Alrai";
             List<string> results = new List<string>();
             for (int i = 0; i < 1000; i++)
