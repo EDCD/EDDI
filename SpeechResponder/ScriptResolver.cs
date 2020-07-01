58f3f2a4f9b9173afae81b587b9a5cb965b20640
Tkael <dragoncharmer84@hotmail.com>
2020-06-30 17:57:19 -0700

    Identify the faulting script if we can.

--- SpeechResponder/ScriptResolver.cs
+++ SpeechResponder/ScriptResolver.cs
@@ -25,6 +25,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using Utilities;
 
@@ -158,7 +159,7 @@ namespace EddiSpeechResponder
             catch (Exception e)
             {
                 Logging.Warn(e.Message, e);
-                return $"{e.Message}";
+                return $"Error with {scriptObject?.Name ?? "this"} script: {e.Message}";
             }
         }
 
