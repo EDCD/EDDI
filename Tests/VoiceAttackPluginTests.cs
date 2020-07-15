c523fdeb74788d43c3c7dcdad77cd0f97fcd6c81
Tkael <dragoncharmer84@hotmail.com>
2020-07-14 20:33:05 -0700

    Code cleanup of usings

--- Tests/VoiceAttackPluginTests.cs
+++ Tests/VoiceAttackPluginTests.cs
@@ -2,12 +2,12 @@
 using EddiDataProviderService;
 using EddiEvents;
 using EddiJournalMonitor;
+using EddiVoiceAttackResponder;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using EddiVoiceAttackResponder;
 
 namespace UnitTests
 {
