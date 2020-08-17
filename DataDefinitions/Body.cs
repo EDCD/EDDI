ed94ca3ab0805e9e1396e2dd9760008bda24049f
Tkael <dragoncharmer84@hotmail.com>
2020-08-17 00:04:15 -0700

    Add calculations for `solardays` and `solarsurfacevelocity` to the body definition.

--- DataDefinitions/Body.cs
+++ DataDefinitions/Body.cs
@@ -418,6 +418,10 @@ namespace EddiDataDefinitions
         public decimal? gravityprobability => Probability.CumulativeP(starClass == null ? planetClass.gravitydistribution : null, gravity);
         [JsonIgnore]
         public decimal? pressureprobability => Probability.CumulativeP(starClass == null ? planetClass.pressuredistribution : null, pressure);
+        [JsonIgnore] // The duration of a solar day on the body, in Earth days
+        public decimal? solardays => (orbitalperiod * rotationalperiod) / (orbitalperiod - rotationalperiod);
+        [JsonIgnore] // The ground speed of the parent body's shadow on the surface of the body in meters per second
+        public decimal? solarsurfacevelocity => (2 * (decimal)Math.PI * radius * 1000) / (solardays * 86400);
 
         private long estimateBodyValue()
         {
@@ -547,8 +551,7 @@ namespace EddiDataDefinitions
             else { return null; }
         }
 
-        // Orbital Velocity
-
+        // Orbital Velocity required to maintain orbit at a given altitude
         public decimal? GetOrbitalVelocityMetersPerSecond(decimal? altitudeMeters)
         {
             if (earthmass != null && radius != null && altitudeMeters != null)
