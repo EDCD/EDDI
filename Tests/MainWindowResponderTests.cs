07ece513a9804625196c0dc2e22d2f8a2eca47d2
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2020-09-02 23:28:56 +0200

    Load MainWindowResponder in EDDI

--- Tests/MainWindowResponderTests.cs
+++ Tests/MainWindowResponderTests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Eddi;
+﻿using Eddi;
 using EddiCore;
-using EddiEvents;
-using EddiJournalMonitor;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -14,15 +9,26 @@ namespace Tests
     public class MainWindowResponderTests
     {
         [TestMethod]
-        public void WhenFindingResponders_MainWindowResponderSHOULDBePresent()
+        public void WhenEDDIIsInitialized_MainWindowResponderSHOULDBePresent()
         {
-            var responders = EDDI.Instance.findResponders();
+            var responders = EDDI.Instance.responders;
 
             responders.Should()
                 .ContainSingle(r => r.GetType() == typeof(MainWindowResponder));
         }
 
-        //string line = @"{ ""timestamp"":""2018-10-17T16:17:55Z"", ""event"":""SquadronDemotion"", ""SquadronName"":""TestSquadron"", ""OldRank"":3, ""NewRank"":2 }";
-        //List<Event> events = JournalMonitor.ParseJournalEntry(line);
+        [TestMethod]
+        public void WhenAppIsLaunched_SquadronNameSHOULDBeEmpty()
+        {
+            //var line = @"{ ""timestamp"":""2018-10-17T16:17:55Z"", ""event"":""SquadronDemotion"", ""SquadronName"":""TestSquadron"", ""OldRank"":3, ""NewRank"":2 }";
+            //var events = JournalMonitor.ParseJournalEntry(line);
+            //EDDI.Instance.enqueueEvent(events.First());
+
+            //if (Application.Current?.MainWindow != null)
+            //{
+            //    ((MainWindow)Application.Current.MainWindow).squadronPowerDropDown.SelectedItem = power.localizedName;
+
+            //}
+        }
     }
 }
