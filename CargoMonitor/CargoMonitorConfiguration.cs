c25897a6eb09fc0f085145a04e4a65d106777c2e
Richard Buckle <github@sailmaker.co.uk>
2020-11-12 21:50:31 +0000

    Stop non-filebacked CargoMonitorConfigurations from writing to default location

--- CargoMonitor/CargoMonitorConfiguration.cs
+++ CargoMonitor/CargoMonitorConfiguration.cs
@@ -87,22 +87,16 @@ namespace EddiCargoMonitor
 
         /// <summary>
         /// Write configuration to a file.  If the filename is not supplied then the path used
-        /// when reading in the configuration will be used, or the default path of 
-        /// Constants.Data_DIR\cargomonitor.json will be used
+        /// when reading in the configuration will be used, unless it too is null, for example
+        /// a test config purely based on JSON, in which case nothing will be written.
         /// </summary>
         public void ToFile(string filename = null)
         {
             // Remove any items that are all NULL
             //limits = limits.Where(x => x.Value.minimum.HasValue || x.Value.desired.HasValue || x.Value.maximum.HasValue).ToDictionary(x => x.Key, x => x.Value);
 
-            if (filename == null)
-            {
-                filename = dataPath;
-            }
-            if (filename == null)
-            {
-                filename = Constants.DATA_DIR + @"\cargomonitor.json";
-            }
+            filename = filename ?? dataPath;
+            if (filename == null) { return; }
 
             string json = JsonConvert.SerializeObject(this, Formatting.Indented);
             Logging.Debug("Configuration to file: " + json);
