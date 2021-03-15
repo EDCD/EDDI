26350fd1b14eddd5d795d06b14de421e852f7f2b
Richard Buckle <github@sailmaker.co.uk>
2021-03-15 21:46:48 +0000

    Implement `forceIntegerMantissa`
    For languages like Russian where phrases like "2.4 million" case complications with case etc.

--- Tests/HumanizeTests.cs
+++ Tests/HumanizeTests.cs
@@ -28,6 +28,30 @@ namespace UnitTests
         }
 
         [DataTestMethod]
+        [DataRow(0, "zero")]
+        [DataRow(456, "456")]
+        [DataRow(-1000, "minus 1000")]
+        [DataRow(100000, "100000")]
+        [DataRow(51000001, "51000000")]
+        [DataRow(-51000000, "minus 51000000")]
+        [DataRow(-12345, "well over minus 12 thousand")]
+        [DataRow(1800001, "1800000")]
+        [DataRow(-1999001, "nearly minus 2 million")]
+        [DataRow(-1100001, "minus 1100000")]
+        [DataRow(-1110001, "just over minus 1 million")]
+        [DataRow(-1210001, "over minus 1 million")]
+        [DataRow(-1310001, "well over minus 1 million")]
+        [DataRow(-1410001, "nearly minus 1 and a half million")]
+        [DataRow(-1510001, "around minus 1 and a half million")]
+        [DataRow(-1610001, "over minus 1 and a half million")]
+        [DataRow(-1810001, "well over minus 1 and a half million")]
+        [DataRow(-1999001, "nearly minus 2 million")]
+        public void TestIntToIntegerMantissa(int number, string expected)
+        {
+            Assert.AreEqual(expected, Translations.Humanize(number, true));
+        }
+
+        [DataTestMethod]
         [DataRow(-12345.0, "well over minus 12 thousand")]
         [DataRow(0.15555555, "0.16")]
         [DataRow(0.015555555, "0.016")]
@@ -45,6 +69,26 @@ namespace UnitTests
         public void TestDoubleToFloatingMantissa(double number, string expected)
         {
             Assert.AreEqual(expected, Translations.Humanize((decimal)number));
+        }
+
+        [DataTestMethod]
+        [DataRow(-12345.0, "well over minus 12 thousand")]
+        [DataRow(0.15555555, "0.16")]
+        [DataRow(0.015555555, "0.016")]
+        [DataRow(0.0015555555, "0.0016")]
+        [DataRow(-0.15555555, "minus 0.16")]
+        [DataRow(-0.015555555, "minus 0.016")]
+        [DataRow(-0.0015555555, "minus 0.0016")]
+        [DataRow(-12.1, "minus 12.1")]
+        [DataRow(-12.01, "minus 12")]
+        [DataRow(6.459E5, "over 640 thousand")]
+        [DataRow(1.8E6, "1800000")]
+        [DataRow(9.4571E11, "over 940 billion")]
+        [DataRow(4.36156E14, "over 430 trillion")]
+        [DataRow(9.1235E17, "over 912 quadrillion")]
+        public void TestDoubleToIntegerMantissa(double number, string expected)
+        {
+            Assert.AreEqual(expected, Translations.Humanize((decimal)number, true));
         }
 
         [DataTestMethod]
