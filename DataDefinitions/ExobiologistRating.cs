4026b00a7de72ca3b12dd76bcb5d5b74e0d792ba
Tkael <dragoncharmer84@hotmail.com>
2021-05-21 11:05:10 -0700

    Add new exobiologist and mercenary rank definitions.

---
+++ DataDefinitions/ExobiologistRating.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Utilities;
+
+namespace EddiDataDefinitions
+{
+    /// <summary>
+    /// Exobiologist ratings
+    /// </summary>
+    public class ExobiologistRating : ResourceBasedLocalizedEDName<ExobiologistRating>
+    {
+        static ExobiologistRating()
+        {
+            resourceManager = Properties.ExobiologistRatings.ResourceManager;
+            resourceManager.IgnoreCase = false;
+        }
+
+        public static readonly ExobiologistRating Directionless = new ExobiologistRating("Directionless", 0);
+        public static readonly ExobiologistRating MostlyDirectionless = new ExobiologistRating("MostlyDirectionless", 1);
+        public static readonly ExobiologistRating Compiler = new ExobiologistRating("Compiler", 2);
+        public static readonly ExobiologistRating Collector = new ExobiologistRating("Collector", 3);
+        public static readonly ExobiologistRating Cataloguer = new ExobiologistRating("Cataloguer", 4);
+        public static readonly ExobiologistRating Taxonomist = new ExobiologistRating("Taxonomist", 5);
+        public static readonly ExobiologistRating Ecologist = new ExobiologistRating("Ecologist", 6);
+        public static readonly ExobiologistRating Geneticist = new ExobiologistRating("Geneticist", 7);
+        public static readonly ExobiologistRating Elite = new ExobiologistRating("Elite", 8);
+
+        public int rank { get; private set; }
+
+        // dummy used to ensure that the static constructor has run
+        public ExobiologistRating() : this("", 0)
+        { }
+
+        private ExobiologistRating(string edname, int rank) : base(edname, edname)
+        {
+            this.rank = rank;
+        }
+
+        public static ExobiologistRating FromRank(int from)
+        {
+            ExobiologistRating result = AllOfThem.FirstOrDefault(v => v.rank == from);
+            if (result == null)
+            {
+                Logging.Info("Unknown Exobiologist Rating rank " + from);
+            }
+            return result;
+        }
+    }
+}
---
+++ DataDefinitions/MercenaryRating.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using Utilities;
+
+namespace EddiDataDefinitions
+{
+    /// <summary>
+    /// Mercenary ratings
+    /// </summary>
+    public class MercenaryRating : ResourceBasedLocalizedEDName<MercenaryRating>
+    {
+        static MercenaryRating()
+        {
+            resourceManager = Properties.MercenaryRatings.ResourceManager;
+            resourceManager.IgnoreCase = false;
+        }
+
+        public static readonly MercenaryRating Defenceless = new MercenaryRating("Defenceless", 0);
+        public static readonly MercenaryRating MostlyDefenceless = new MercenaryRating("MostlyDefenceless", 1);
+        public static readonly MercenaryRating Rookie = new MercenaryRating("Rookie", 2);
+        public static readonly MercenaryRating Mercenary = new MercenaryRating("Soldier", 3);
+        public static readonly MercenaryRating Gunslinger = new MercenaryRating("Gunslinger", 4);
+        public static readonly MercenaryRating Warrior = new MercenaryRating("Warrior", 5);
+        public static readonly MercenaryRating Gladiator = new MercenaryRating("Gladiator", 6);
+        public static readonly MercenaryRating Deadeye = new MercenaryRating("Deadeye", 7);
+        public static readonly MercenaryRating Elite = new MercenaryRating("Elite", 8);
+
+        public int rank { get; private set; }
+
+        // dummy used to ensure that the static constructor has run
+        public MercenaryRating() : this("", 0)
+        { }
+
+        private MercenaryRating(string edname, int rank) : base(edname, edname)
+        {
+            this.rank = rank;
+        }
+
+        public static MercenaryRating FromRank(int from)
+        {
+            MercenaryRating result = AllOfThem.FirstOrDefault(v => v.rank == from);
+            if (result == null)
+            {
+                Logging.Info("Unknown Mercenary Rating rank " + from);
+            }
+            return result;
+        }
+    }
+}
