20b2f259848ba55c62f4b4137289fa602dd31021
Tkael <dragoncharmer84@hotmail.com>
2021-05-30 15:34:56 -0700

    Reallign unit test criteria for updated micro-resource category names.

--- Tests/JournalMonitorTests.cs
+++ Tests/JournalMonitorTests.cs
@@ -1736,22 +1736,22 @@ namespace UnitTests
 
             var circuitSwitch = @event.backpack.FirstOrDefault(m => m.edname == "CircuitSwitch");
             Assert.AreEqual("Circuit Switch", circuitSwitch.microResource?.invariantName);
-            Assert.AreEqual("Component", circuitSwitch.microResource?.Category?.invariantName);
+            Assert.AreEqual("Assets", circuitSwitch.microResource?.Category?.invariantName);
             Assert.AreEqual(0, circuitSwitch.ownerId);
             Assert.AreEqual(18446744073709551615M, circuitSwitch.missionId);
             Assert.AreEqual(1, circuitSwitch.amount);
 
             var energyCell = @event.backpack.FirstOrDefault(m => m.edname == "EnergyCell");
             Assert.AreEqual("Energy Cell", energyCell.microResource?.invariantName);
-            Assert.AreEqual("Consumable", energyCell.microResource?.Category?.invariantName);
+            Assert.AreEqual("Consumables", energyCell.microResource?.Category?.invariantName);
             Assert.AreEqual(0, energyCell.ownerId);
             Assert.AreEqual(18446744073709551615M, energyCell.missionId);
             Assert.AreEqual(3, energyCell.amount);
         }
 
         [DataTestMethod]
-        [DataRow("{ \"timestamp\":\"2021-05-29T21:14:30Z\", \"event\":\"BackpackChange\", \"Added\":[ { \"Name\":\"viscoelasticpolymer\", \"Name_Localised\":\"Viscoelastic Polymer\", \"OwnerID\":0, \"Count\":1, \"Type\":\"Component\" } ] }", true, "Viscoelastic Polymer", "Component", 0, -1, 1)]
-        [DataRow("{ \"timestamp\":\"2021-05-29T20:40:44Z\", \"event\":\"BackpackChange\", \"Removed\":[ { \"Name\":\"largecapacitypowerregulator\", \"Name_Localised\":\"Power Regulator\", \"OwnerID\":1642695, \"MissionID\":775573921, \"Count\":1, \"Type\":\"Item\" } ] }", false, "Power Regulator", "Item", 1642695, 775573921, 1)]
+        [DataRow("{ \"timestamp\":\"2021-05-29T21:14:30Z\", \"event\":\"BackpackChange\", \"Added\":[ { \"Name\":\"viscoelasticpolymer\", \"Name_Localised\":\"Viscoelastic Polymer\", \"OwnerID\":0, \"Count\":1, \"Type\":\"Component\" } ] }", true, "Viscoelastic Polymer", "Assets", 0, -1, 1)]
+        [DataRow("{ \"timestamp\":\"2021-05-29T20:40:44Z\", \"event\":\"BackpackChange\", \"Removed\":[ { \"Name\":\"largecapacitypowerregulator\", \"Name_Localised\":\"Power Regulator\", \"OwnerID\":1642695, \"MissionID\":775573921, \"Count\":1, \"Type\":\"Item\" } ] }", false, "Power Regulator", "Goods", 1642695, 775573921, 1)]
         public void TestBackpackChanged(string line, bool isAdded, string expectedInvariantName, string expectedInvariantCategory, int expectedOwnerId, long expectedMissionId, int expectedAmount)
         {
             // DataRow attributes don't work well with nullable types so we work around that here.
@@ -1825,7 +1825,7 @@ namespace UnitTests
             var @event = (MicroResourcesPurchasedEvent)events[0];
 
             Assert.AreEqual("Medkit", @event.microResource?.invariantName);
-            Assert.AreEqual("Consumable", @event.microResource?.Category?.invariantName);
+            Assert.AreEqual("Consumables", @event.microResource?.Category?.invariantName);
             Assert.AreEqual(2, @event.amount);
             Assert.AreEqual(2000, @event.price);
             Assert.AreEqual(3221524992, @event.marketid);
