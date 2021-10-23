18a8a2e3ef5885ce913be1ab31d332085c79ae53
T'kael <dragoncharmer84@hotmail.com>
2021-10-23 01:43:19 -0700

    Generate `Bookmark details` event data from the `bookmark` object

--- Events/BookmarkDetailsEvent.cs
+++ Events/BookmarkDetailsEvent.cs
@@ -1,6 +1,5 @@
 ﻿using EddiDataDefinitions;
 using System;
-using System.Collections.Generic;
 using Utilities;
 
 namespace EddiEvents
@@ -11,40 +10,43 @@ namespace EddiEvents
         public const string DESCRIPTION = "Triggered when a bookmark has been added or updated";
         public const string SAMPLE = null;
 
-        [PublicAPI("The bookmark request (location, query, update, set)")]
+        [PublicAPI("The bookmark request type (location, query, update, set)")]
         public string request { get; private set; }
 
-        [PublicAPI("The bookmarked system")]
-        public string system { get; private set; }
+        [PublicAPI("The bookmarked system name")] 
+        public string systemname => bookmark?.systemname;
 
-        [PublicAPI("The bookmarked body, if applicable")]
-        public string body { get; private set; }
+        [PublicAPI("The bookmarked body name, if applicable")]
+        public string bodyname => bookmark?.bodyname;
+
+        [PublicAPI("The bookmarked body short name, if applicable")]
+        public string bodyshortname => bookmark?.bodyshortname;
 
         [PublicAPI("The bookmarked 'point of interest', if applicable")]
-        public string poi { get; private set; }
+        public string poi => bookmark?.poi;
+
+        [PublicAPI("The bookmarked comment, if applicable")]
+        public string comment => bookmark?.comment;
 
         [PublicAPI("True if the point of interest is a station")]
-        public bool isstation { get; private set; }
+        public bool isstation => bookmark?.isstation ?? false;
 
         [PublicAPI("The latitude on the surface of a landable body")]
-        public decimal? latitude { get; private set; }
+        public decimal? latitude => bookmark?.latitude;
 
         [PublicAPI("The longitude on the surface of a landable body")]
-        public decimal? longitude { get; private set; }
+        public decimal? longitude => bookmark?.longitude;
 
         [PublicAPI("True if the body is 'landable'")]
-        public bool landable { get; private set; }
+        public bool landable => bookmark?.landable ?? false;
+
+        // Variables below are not intended to be user facing
+        private NavBookmark bookmark;
 
         public BookmarkDetailsEvent(DateTime timestamp, string request, NavBookmark bookmark) : base(timestamp, NAME)
         {
             this.request = request;
-            this.system = bookmark.systemname;
-            this.body = bookmark.bodyname;
-            this.poi = bookmark.poi;
-            this.isstation = bookmark.isstation;
-            this.latitude = bookmark.latitude;
-            this.longitude = bookmark.longitude;
-            this.landable = bookmark.landable;
+            this.bookmark = bookmark;
         }
     }
 }
