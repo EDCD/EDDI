afe0ee9a74ad7fcfe0a37e160f435c7ef971fa44
T'kael <dragoncharmer84@hotmail.com>
2022-02-26 15:12:42 -0800

    Add constructors and additional properties to the `NavWaypoint` class.

--- DataDefinitions/NavWaypoint.cs
+++ DataDefinitions/NavWaypoint.cs
@@ -1,4 +1,6 @@
-﻿namespace EddiDataDefinitions
+﻿using System.Linq;
+
+namespace EddiDataDefinitions
 {
     public class NavWaypoint
     {
@@ -8,14 +10,22 @@
         public decimal? y { get; set; }
         public decimal? z { get; set; }
 
-        public decimal distanceTravelled { get; set; }
+        public decimal distance { get; set; }
+        public decimal distanceTraveled { get; set; }
         public decimal distanceRemaining { get; set; }
 
-        // Spansh Neutron and Galaxy plotters only
+        public int index { get; set; }
+
+        // NavRoute only
+        public string stellarclass { get; set; }
+
+        // NavRoute and Spansh Neutron and Galaxy plotters only
         public bool hasNeutronStar { get; set; }
 
-        // Spansh Galaxy plotter only
+        // NavRoute and Spansh Galaxy plotter only
         public bool? isScoopable { get; set; }
+
+        // Spansh Galaxy plotter only
         public bool? refuelRecommended { get; set; }
 
         // Spansh Carrier plotter only
@@ -23,5 +33,45 @@
         public bool? hasPristineMining { get; set; }
         public bool? isDesiredDestination { get; set; } // If this is one of the destinations prescribed by the commander for the route
         public int? fuelUsed { get; set; } // The amount of tritium used to jump to this location
+
+        // Info seeded from other monitors
+        public bool isMissionSystem { get; set; }
+
+        // Whether we've already visited the system during our current route
+        public bool visited;
+
+        // Default constructor
+        public NavWaypoint(string systemName, decimal x, decimal y, decimal z)
+        {
+            this.systemName = systemName;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public NavWaypoint(StarSystem starSystem)
+        {
+            this.systemName = starSystem.systemname;
+            this.x = starSystem.x;
+            this.y = starSystem.y;
+            this.z = starSystem.z;
+            var closeStars = starSystem.bodies.Where(b => b.bodyType == BodyType.FromEDName("Star") && b.distance < 100).ToList();
+            stellarclass = closeStars.FirstOrDefault(b => b.mainstar ?? false)?.stellarclass;
+            isScoopable = closeStars.Any(b => !string.IsNullOrEmpty(b.stellarclass) && "KGBFOAM".Contains(b.stellarclass));
+            hasNeutronStar = closeStars.Any(b => !string.IsNullOrEmpty(b.stellarclass) && "N".Contains(b.stellarclass));
+        }
+
+        // From `NavRouteInfoItem`
+        public NavWaypoint(NavRouteInfoItem navRouteItem)
+        {
+            systemName = navRouteItem.systemname;
+            systemAddress = navRouteItem.systemAddress;
+            x = navRouteItem.x;
+            y = navRouteItem.y;
+            z = navRouteItem.z;
+            stellarclass = navRouteItem.stellarclass;
+            isScoopable = !string.IsNullOrEmpty(navRouteItem.stellarclass) && "KGBFOAM".Contains(navRouteItem.stellarclass);
+            hasNeutronStar = !string.IsNullOrEmpty(navRouteItem.stellarclass) && "N".Contains(navRouteItem.stellarclass);
+        }
     }
 }
