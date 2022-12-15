07e5a1de1482221d23a4f0428b0c1cda7ca764f2
Tkael <dragoncharmer84@hotmail.com>
2022-12-15 01:05:19 -0800

    Add new AX modules, re-sort definitions by EDID

--- DataDefinitions/Properties/Modules.Designer.cs
+++ DataDefinitions/Properties/Modules.Designer.cs
@@ -19,7 +19,7 @@ namespace EddiDataDefinitions.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Modules {
@@ -363,6 +363,24 @@ namespace EddiDataDefinitions.Properties {
         public static string Enforcer {
             get {
                 return ResourceManager.GetString("Enforcer", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enhanced AX Missile Rack.
+        /// </summary>
+        public static string Enhanced_AXMissileRack {
+            get {
+                return ResourceManager.GetString("Enhanced_AXMissileRack", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enhanced AX Multi-Cannon.
+        /// </summary>
+        public static string Enhanced_AXMulti_Cannon {
+            get {
+                return ResourceManager.GetString("Enhanced_AXMulti_Cannon", resourceCulture);
             }
         }
         
