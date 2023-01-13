c4d23375949776f7f8823fa0863f8549ab5100e4
Tkael <dragoncharmer84@hotmail.com>
2023-01-12 22:27:44 -0800

    Make FSSSignalDiscoveredSchema game version agnostic.

--- EDDNResponder/Schemas/FSSSignalDiscoveredSchema.cs
+++ EDDNResponder/Schemas/FSSSignalDiscoveredSchema.cs
@@ -12,80 +12,81 @@ namespace EddiEddnResponder.Schemas
     {
         public List<string> edTypes => new List<string> { "FSSSignalDiscovered" };
 
-        private EDDNState latestSignalState;
-
         private readonly List<IDictionary<string, object>> signals =
             new List<IDictionary<string, object>>();
 
+        private readonly List<EDDNState> eddnStates = new List<EDDNState>();
+
+        private static readonly object signalsLock = new object();
+        private static readonly object stateLock = new object();
+
         public bool Handle(string edType, ref IDictionary<string, object> data, EDDNState eddnState)
         {
-            if (!(eddnState?.Location is null) && !(eddnState.GameVersion is null))
-            {
-                if (!edTypes.Contains(edType))
-                {
-                    // This marks the end of a batch of signals.
-                    if (eddnState.GameVersion.inOdyssey ?? false)
-                    {
-                        // In Odyssey, Location/FSDJump/CarrierJump events are written prior to `FSSSignalDiscovered` events
-                        latestSignalState = eddnState;
-                    }
-                    else
-                    {
-                        // In Horizons, Location/FSDJump/CarrierJump events are written after `FSSSignalDiscovered` events
-                        // so we use the prior signal state
-                    }
-                    if (signals.Any())
-                    {
-                        try
-                        {
-                            LockManager.GetLock(nameof(latestSignalState), () =>
-                            {
-                                if (latestSignalState?.Location.StarSystemLocationIsSet() ?? false)
-                                {
-                                    var retrievedSignals = signals
-                                        .Where(s => JsonParsing.getULong(s, "SystemAddress") == latestSignalState.Location.systemAddress)
-                                        .ToList();
-                                    if (retrievedSignals.Any())
-                                    {
-                                        var handledData = PrepareSignalsData(retrievedSignals, latestSignalState);
-                                        handledData = latestSignalState.GameVersion.AugmentVersion(handledData);
-                                        EDDNSender.SendToEDDN("https://eddn.edcd.io/schemas/fsssignaldiscovered/1", handledData, latestSignalState);
-                                        latestSignalState = null;
-                                        signals.RemoveAll(s => retrievedSignals.Contains(s));
-                                    }
-                                }
-                            });
-                        }
-                        catch (NullReferenceException nre)
-                        {
-                            Logging.Error("Failed to prepare signals for sending to EDDN", nre);
-                        }
-                    }
-                }
+            if (eddnState?.Location is null || eddnState.GameVersion is null) { return false; }
 
-                if (edTypes.Contains(edType) && eddnState.Location.StarSystemLocationIsSet())
+            // Capture any changes to eddnState systemAddress after confirming that the star system location data (name, system address, and star pos) is fully set.
+            lock (stateLock)
+            {
+                if (eddnStates.All(s => s.Location?.systemAddress != eddnState.Location?.systemAddress) &&
+                    eddnState.Location.StarSystemLocationIsSet())
                 {
-                    try
-                    {
-                        // Remove redundant, personal, or time sensitive data
-                        var ussSignalType = data?.ContainsKey("USSType") ?? false 
-                            ? data["USSType"]?.ToString() 
-                            : string.Empty;
-                        if (string.IsNullOrEmpty(ussSignalType) || ussSignalType != "$USS_Type_MissionTarget;")
-                        {
-                            // This is a signal that we need to add to our signal batch
-                            latestSignalState = eddnState;
-                            signals.Add(data);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Logging.Error($"{GetType().Name} failed to handle {edType} journal data.", e);
-                    }
-                    return true;
+                    eddnStates.Add(eddnState);
                 }
             }
-            return false;
+
+            if (!edTypes.Contains(edType))
+            {
+                // Send any signals with a systemAddress matching a saved good eddnState
+                lock (signalsLock)
+                {
+                    if (signals.Any())
+                    {
+                        lock (stateLock)
+                        {
+                            foreach (var state in eddnStates)
+                            {
+                                try
+                                {
+                                    var retrievedSignals = signals
+                                        .Where(s => JsonParsing.getULong(s, "SystemAddress") == state.Location.systemAddress)
+                                        .ToList();
+                                    if (!retrievedSignals.Any()) { continue; }
+                                    var handledData = PrepareSignalsData(retrievedSignals, state);
+                                    handledData = state.GameVersion.AugmentVersion(handledData);
+                                    EDDNSender.SendToEDDN("https://eddn.edcd.io/schemas/fsssignaldiscovered/1", handledData, state);
+                                    signals.RemoveAll(s => retrievedSignals.Contains(s));
+                                }
+                                catch (NullReferenceException nre)
+                                {
+                                    Logging.Error("Failed to prepare signals for sending to EDDN", nre);
+                                }
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+
+            try
+            {
+                // Remove redundant, personal, or time sensitive data
+                var ussSignalType = data?.ContainsKey("USSType") ?? false
+                    ? data["USSType"]?.ToString()
+                    : string.Empty;
+                if (string.IsNullOrEmpty(ussSignalType) || ussSignalType != "$USS_Type_MissionTarget;")
+                {
+                    // This is a signal that we need to add to our signal batch
+                    lock (signalsLock)
+                    {
+                        signals.Add(data);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Logging.Error($"{GetType().Name} failed to handle {edType} journal data.", e);
+            }
+            return true;
         }
 
         private IDictionary<string, object> PrepareSignalsData(List<IDictionary<string, object>> retrievedSignals, EDDNState eddnState)
