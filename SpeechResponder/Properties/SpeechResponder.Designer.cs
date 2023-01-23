75058755bee8c9afb7647c57241bc32395b73664
Tkael <dragoncharmer84@hotmail.com>
2023-01-22 18:39:01 -0800

    Add a tooltip to the Speech Responder search filter textbox Resolves #2487

--- SpeechResponder/Properties/SpeechResponder.Designer.cs
+++ SpeechResponder/Properties/SpeechResponder.Designer.cs
@@ -19,7 +19,7 @@ namespace EddiSpeechResponder.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class SpeechResponder {
@@ -363,6 +363,15 @@ namespace EddiSpeechResponder.Properties {
         public static string search_filter {
             get {
                 return ResourceManager.GetString("search_filter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Filter results by script name, description, or full text..
+        /// </summary>
+        public static string search_filter_tooltip {
+            get {
+                return ResourceManager.GetString("search_filter_tooltip", resourceCulture);
             }
         }
         
