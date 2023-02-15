347fa1b3182d45d3fe931e430ddcd449b13112f5
Tkael <dragoncharmer84@hotmail.com>
2023-02-15 00:31:57 -0800

    Fix accidentally dropped event

---
+++ Events/LowFuelEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Utilities;
+
+namespace EddiEvents
+{
+    [PublicAPI]
+    public class LowFuelEvent : Event
+    {
+        public const string NAME = "Low fuel";
+        public const string DESCRIPTION = "Triggered when your fuel level falls below 25% and in 5% increments thereafter";
+        public const string SAMPLE = null;
+
+        public LowFuelEvent(DateTime timestamp) : base(timestamp, NAME)
+        { }
+    }
+}
