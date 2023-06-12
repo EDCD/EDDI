6d15a2d5ceb30ac0d1e23e4509b4b7672672f09c
Tkael <dragoncharmer84@hotmail.com>
2023-06-12 00:00:07 -0700

    Remove the `GitInfo` dependency so the information is now available via the `SourceRevisionId` MetadataAttribute.

--- Utilities/Logging.cs
+++ Utilities/Logging.cs
@@ -8,6 +8,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -387,11 +388,14 @@ namespace Utilities
                     PersonDataCollectionPolicies.None,
                     IpAddressCollectionPolicy.DoNotCollect,
                     new[] { "Commander", "apiKey", "commanderName", "access_token", "refresh_token", "uploaderID" } );
+                var sourceRevision = Assembly.GetExecutingAssembly()?
+                    .GetCustomAttributes<AssemblyMetadataAttribute>()
+                    .SingleOrDefault( a => a.Key == "SourceRevisionId" )?.Value;
                 var loggerPayloadOptions = new RollbarPayloadAdditionOptions()
                 {
                     Person = new Person( uniqueId + ( fromVA ? " VA" : "" ) ),
                     Server = new Server { Root = "/" },
-                    CodeVersion = ThisAssembly.Git.Sha
+                    CodeVersion = sourceRevision
                 };
                 loggerOptions.RollbarDataSecurityOptions.Reconfigure( loggerDataSecurityOptions );
                 loggerOptions.RollbarPayloadAdditionOptions.Reconfigure( loggerPayloadOptions );
