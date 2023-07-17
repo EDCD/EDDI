4ee71ec738c53700b24fa457fadb3783073f0098
Tkael <dragoncharmer84@hotmail.com>
2023-07-16 17:26:33 -0700

    Update `Heat damage` and `Heat warning` descriptions.

--- Events/HeatDamageEvent.cs
+++ Events/HeatDamageEvent.cs
@@ -7,7 +7,7 @@ namespace EddiEvents
     public class HeatDamageEvent : Event
     {
         public const string NAME = "Heat damage";
-        public const string DESCRIPTION = "Triggered when your ship is taking damage from excessive heat";
+        public const string DESCRIPTION = "Triggered when you begin to take hull damage due to excessive heat.";
         public const string SAMPLE = "{\"timestamp\":\"2016-09-25T12:00:23Z\",\"event\":\"HeatDamage\"}";
 
         public HeatDamageEvent(DateTime timestamp) : base(timestamp, NAME)
--- Events/HeatWarningEvent.cs
+++ Events/HeatWarningEvent.cs
@@ -7,7 +7,7 @@ namespace EddiEvents
     public class HeatWarningEvent : Event
     {
         public const string NAME = "Heat warning";
-        public const string DESCRIPTION = "Triggered when your ship's heat exceeds 100%";
+        public const string DESCRIPTION = "Triggered when your ship's heat exceeds 100% and you begin to take module damage.";
         public const string SAMPLE = "{\"timestamp\":\"2016-09-25T12:00:23Z\",\"event\":\"HeatWarning\"}";
 
         public HeatWarningEvent(DateTime timestamp) : base(timestamp, NAME)
