2632225c2f003e08f200e4f1c2aaf322f0f2fb83
bcthund <bthundereagle@gmail.com>
2023-07-29 08:32:54 -0400

    Bio scan tracking and codex entries Added a basic and functioning system for scanning biologicals and keeping track of how many samples and total are left to scan on the planet. Also keeps track of distance from previous samples to assist in not violating previous sample range. Codex information added for biologicals and so the CodexEntry event was also implemented to make use of this data. Descriptions are all currently hardcoded in english.

---
+++ Events/CodexEntryEvent.cs
@@ -0,0 +1,97 @@
+﻿using EddiDataDefinitions;
+using MathNet.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Utilities;
+
+namespace EddiEvents
+{
+    [PublicAPI]
+    public class CodexEntryEvent : Event
+    {
+        public const string NAME = "Codex entry obtained";
+        public const string DESCRIPTION = "Triggered when a codex entry is obtained";
+        public const string SAMPLE = @"{ ""timestamp"":""2023-07-22T04:10:26Z"", ""event"":""CodexEntry"", ""EntryID"":2440503, ""Name"":""$Codex_Ent_Shrubs_05_F_Name;"", ""Name_Localised"":""Frutexa Fera - Green"", ""SubCategory"":""$Codex_SubCategory_Organic_Structures;"", ""SubCategory_Localised"":""Organic structures"", ""Category"":""$Codex_Category_Biology;"", ""Category_Localised"":""Biological and Geological"", ""Region"":""$Codex_RegionName_5;"", ""Region_Localised"":""Norma Arm"", ""System"":""Greae Phio FO-G d11-1005"", ""SystemAddress"":34542299533283, ""BodyID"":42, ""Latitude"":-45.382187, ""Longitude"":173.182938 }";
+    //public const string SAMPLE = @"{ ""timestamp"":""2023-07-22T05:30:39Z"", ""event"":""CodexEntry"", ""EntryID"":1100201, ""Name"":""$Codex_Ent_B_Type_Name;"", ""Name_Localised"":""B Types"", ""SubCategory"":""$Codex_SubCategory_Stars;"", ""SubCategory_Localised"":""Stars"", ""Category"":""$Codex_Category_StellarBodies;"", ""Category_Localised"":""Astronomical Bodies"", ""Region"":""$Codex_RegionName_5;"", ""Region_Localised"":""Norma Arm"", ""System"":""Greae Phio QE-Q e5-6040"", ""SystemAddress"":25942946711540, ""BodyID"":0, ""IsNewEntry"":true }";
+    //public const string SAMPLE = @"{ ""timestamp"":""2023-07-23T05:43:52Z"", ""event"":""CodexEntry"", ""EntryID"":2100804, ""Name"":""$Codex_Ent_L_Cry_MetCry_Yw_Name;"", ""Name_Localised"":""Flavum Metallic Crystals"", ""SubCategory"":""$Codex_SubCategory_Organic_Structures;"", ""SubCategory_Localised"":""Organic structures"", ""Category"":""$Codex_Category_Biology;"", ""Category_Localised"":""Biological and Geological"", ""Region"":""$Codex_RegionName_5;"", ""Region_Localised"":""Norma Arm"", ""System"":""Greae Phio JU-E d12-299"", ""SystemAddress"":10284324245483, ""BodyID"":2, ""NearestDestination"":""$Fixed_Event_Life_Cloud;"", ""NearestDestination_Localised"":""Notable stellar phenomena"", ""VoucherAmount"":2500 }
+
+    [PublicAPI( "The system the entry was discovered in." )]
+        public string systemName { get; private set; }
+
+        [PublicAPI( "The category of the entry." )]
+        public string categoryName { get; private set; }
+
+        [PublicAPI( "The subcategory of the entry." )]
+        public string subCategoryName { get; private set; }
+
+        [PublicAPI( "The name of the entry." )]
+        public string entryName { get; private set; }
+
+        [PublicAPI( "The name of the entry." )]
+        public string codexName { get; private set; }
+
+        [PublicAPI( "The region of the discovery." )]
+        public string regionName { get; private set; }
+
+        [PublicAPI( "Is this a new discovery?" )]
+        public bool newEntry { get; private set; }
+
+        [PublicAPI( "Is this a new trait?" )]
+        public bool newTrait { get; private set; }
+
+        [PublicAPI( "What is the codex entry worth?" )]
+        public int voucherAmount { get; private set; }
+
+        [PublicAPI( "The detailed data for the biological object" )]
+        public OrganicData organicData { get; private set; }
+
+        [PublicAPI( "The detailed data for the astrological object" )]
+        public AstrometricData astrometricData { get; private set; }
+
+        [PublicAPI( "The detailed data for the geology object" )]
+        public GeologyData geologyData { get; private set; }
+
+        // Not intended to be user facing
+
+        public CodexEntryEvent ( DateTime timestamp, string systemName, string categoryName, string subCategoryName, string codexName, string localisedName, string regionName, bool newEntry, bool newTrait, int voucherAmount ) : base(timestamp, NAME)
+        {
+            this.systemName = systemName;
+            this.codexName = codexName;
+            this.categoryName = categoryName;
+            this.subCategoryName = subCategoryName;
+            this.entryName = localisedName;
+            this.regionName = regionName;
+            this.newEntry = newEntry;
+            this.newTrait = newTrait;
+            this.voucherAmount = voucherAmount;
+
+            switch ( subCategoryName )
+            {
+                case "Organic structures":
+                    this.organicData = OrganicInfo.LookupByVariant( localisedName );
+                    if (this.organicData == null )
+                    {
+                        this.organicData = new OrganicData();
+                    }
+                    break;
+                case "Stars":
+                case "Gas giant planets":
+                case "Terrestrial planets":
+                    this.astrometricData = AstrometricInfo.GetData( subCategoryName, codexName );
+                    if ( this.astrometricData == null )
+                    {
+                        this.astrometricData = new AstrometricData();
+                    }
+                    break;
+                case "Geology and anomalies":
+                    this.geologyData = GeologyInfo.LookupByVariant( localisedName );
+                    if ( this.geologyData == null )
+                    {
+                        this.geologyData = new GeologyData();
+                    }
+                    break;
+            }
+        }
+    }
+}
