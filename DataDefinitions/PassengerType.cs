9e07a6a1375414f58abe58625f373e28d1ce1645
T'kael <DragonCharmer84@hotmail.com>
2024-03-10 20:11:32 -0700

    PassengerType tidy ups.
    `Celebrity` S/B `MinorCelebrity`
    `PrisonerOfWar` S/B `POW`
    Fixed edname and preserved current translations.

--- DataDefinitions/PassengerType.cs
+++ DataDefinitions/PassengerType.cs
@@ -10,34 +10,37 @@
             resourceManager = Properties.PassengerType.ResourceManager;
             resourceManager.IgnoreCase = false;
             missingEDNameHandler = (edname) => new PassengerType(edname);
-
-            var AidWorker = new PassengerType("AidWorker");
-            var Business = new PassengerType("Business");
-            var Celebrity = new PassengerType("Celebrity");
-            var CEO = new PassengerType("CEO");
-            var Criminal = new PassengerType("Criminal");
-            var Doctor = new PassengerType("Doctor");
-            var Explorer = new PassengerType("Explorer");
-            var General = new PassengerType("General");
-            var HeadOfState = new PassengerType("HeadOfState");
-            var Medical = new PassengerType("Medical");
-            var Politician = new PassengerType("Politician");
-            var PrisonerOfWar = new PassengerType("PrisonerOfWar");
-            var Protestor = new PassengerType("Protestor");
-            var Rebel = new PassengerType("Rebel");
-            var Refugee = new PassengerType("Refugee");
-            var Scientist = new PassengerType("Scientist");
-            var Security = new PassengerType("Security");
-            var Terrorist = new PassengerType("Terrorist");
-            var Tourist = new PassengerType("Tourist");
-            var Whistleblower = new PassengerType("Whistleblower");
         }
+
+        public static readonly PassengerType AidWorker = new PassengerType("AidWorker");
+        public static readonly PassengerType Business = new PassengerType("Business");
+        public static readonly PassengerType Celebrity = new PassengerType("MinorCelebrity");
+        public static readonly PassengerType CEO = new PassengerType("CEO");
+        public static readonly PassengerType Criminal = new PassengerType("Criminal");
+        public static readonly PassengerType Doctor = new PassengerType("Doctor");
+        public static readonly PassengerType Explorer = new PassengerType("Explorer");
+        public static readonly PassengerType General = new PassengerType("General");
+        public static readonly PassengerType HeadOfState = new PassengerType("HeadOfState");
+        public static readonly PassengerType Medical = new PassengerType("Medical");
+        public static readonly PassengerType Politician = new PassengerType("Politician");
+        public static readonly PassengerType PrisonerOfWar = new PassengerType("POW");
+        public static readonly PassengerType Protestor = new PassengerType("Protestor");
+        public static readonly PassengerType Rebel = new PassengerType("Rebel");
+        public static readonly PassengerType Refugee = new PassengerType("Refugee");
+        public static readonly PassengerType Scientist = new PassengerType("Scientist");
+        public static readonly PassengerType Security = new PassengerType("Security");
+        public static readonly PassengerType Terrorist = new PassengerType("Terrorist");
+        public static readonly PassengerType Tourist = new PassengerType("Tourist");
+        public static readonly PassengerType Whistleblower = new PassengerType("Whistleblower");
 
         // dummy used to ensure that the static constructor has run
         public PassengerType() : this("")
         { }
 
-        private PassengerType(string edname) : base(edname, edname)
+        private PassengerType(string edname) : base(edname, edname
+            .Replace( "POW", "PrisonerOfWar" )
+            .Replace("MinorCelebrity", "Celebrity" ) 
+        )
         { }
     }
 }
