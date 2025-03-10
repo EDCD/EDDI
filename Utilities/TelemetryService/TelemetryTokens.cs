d2cf17b07234c9add83c9e6dfad70b7b7eeb8f2e
T'kael <DragonCharmer84@hotmail.com>
2025-03-09 21:16:33 -0700

    Update Rollbar token

--- Utilities/TelemetryService/TelemetryTokens.cs
+++ Utilities/TelemetryService/TelemetryTokens.cs
@@ -3,6 +3,6 @@
     internal static class TelemetryTokens
     {
         // replace with official token for writing to the telemetry service
-        public static readonly string rollbarToken = "6c6786a24dd64995b7062639ebcc2bf8";
+        public static readonly string rollbarToken = null;
     }
 }
