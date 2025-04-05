c703098633ef6503c3311f034cbc59ac93560437
T'kael <DragonCharmer84@hotmail.com>
2025-04-05 13:32:41 -0700

    Reset commodity schema message suppression after we've transacted with the market

--- EDDNResponder/Schemas/CommoditySchema.cs
+++ EDDNResponder/Schemas/CommoditySchema.cs
@@ -13,7 +13,7 @@ namespace EddiEddnResponder.Schemas
     [UsedImplicitly]
     public class CommoditySchema : ISchema, ICapiSchema
     {
-        public List<string> edTypes => new List<string> { "Market" };
+        public List<string> edTypes => new List<string> { "Market", "MarketBuy", "MarketSell" };
 
         // Track this so that we do not send duplicate data from the journal and from CAPI.
         private long? lastSentMarketID;
@@ -28,6 +28,15 @@ namespace EddiEddnResponder.Schemas
 
                 var marketID = JsonParsing.getLong(data, "MarketID");
                 var timestamp = JsonParsing.getDateTime( "timestamp", data );
+
+                // Reset message suppression if we've transacted with the market
+                if ( marketID == lastSentMarketID &&
+                     ( edType.Equals( "MarketBuy" ) ||
+                       edType.Equals( "MarketSell" ) ) )
+                {
+                    lastSentDateTime = null;
+                    return true;
+                }
 
                 // Suppress repetitious messages less than 2 minutes apart.
                 if ( lastSentMarketID == marketID && timestamp < ( lastSentDateTime + TimeSpan.FromMinutes( 2 ) ) )
