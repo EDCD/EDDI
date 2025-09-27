6d13fe1de1a297c57453119bffe825a18aee3209
T'kael <DragonCharmer84@hotmail.com>
2025-09-26 21:41:01 -0700

    Fixed failure to parse "Terraformable" values from Spansh dumps. Resolved #2756.

--- DataDefinitions/TerraformState.cs
+++ DataDefinitions/TerraformState.cs
@@ -1,4 +1,7 @@
-﻿namespace EddiDataDefinitions
+﻿using System;
+using System.Collections.Generic;
+
+namespace EddiDataDefinitions
 {
     /// <summary>
     /// Terraform States
@@ -12,18 +15,46 @@
             missingEDNameHandler = (edname) => new TerraformState(edname);
 
             NotTerraformable = new TerraformState("NotTerraformable");
-            var Terraformable = new TerraformState("Terraformable");
-            var Terraforming = new TerraformState("Terraforming");
-            var Terraformed = new TerraformState("Terraformed");
+            Terraformable = new TerraformState("Terraformable");
+            Terraforming = new TerraformState("Terraforming");
+            Terraformed = new TerraformState("Terraformed");
         }
 
         public static readonly TerraformState NotTerraformable;
+        public static readonly TerraformState Terraformable;
+        public static readonly TerraformState Terraforming;
+        public static readonly TerraformState Terraformed;
 
         // dummy used to ensure that the static constructor has run
-        public TerraformState() : this("")
+        public TerraformState () : this( "" )
         { }
 
         private TerraformState(string edname) : base(edname, edname)
         { }
+
+        public static new TerraformState FromName ( string from )
+        {
+            if ( !string.IsNullOrEmpty(from) && equivalencyMap.TryGetValue(from.ToLowerInvariant(), out var terraformState) )
+            {
+                return terraformState;
+            }
+
+            return ResourceBasedLocalizedEDName<TerraformState>.FromName( from );
+        }
+        public static new TerraformState FromEDName ( string from )
+        {
+            if ( !string.IsNullOrEmpty( from ) && equivalencyMap.TryGetValue( from.ToLowerInvariant(), out var terraformState ) )
+            {
+                return terraformState;
+            }
+
+            return ResourceBasedLocalizedEDName<TerraformState>.FromEDName( from );
+        }  
+        
+        private static Dictionary<string, TerraformState> equivalencyMap => new Dictionary<string, TerraformState>( StringComparer.OrdinalIgnoreCase )
+        {
+            { "not terraformable", NotTerraformable },
+            { "terraformable", Terraformable }
+        };
     }
 }
