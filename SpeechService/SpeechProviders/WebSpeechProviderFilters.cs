2fdc086277bdc6b50ef0ee175848e406746be2cf
T'kael <DragonCharmer84@hotmail.com>
2026-07-22 01:28:24 -0700

    If no supported locales or culture code is declared for a web provider voice, assume it is allowed.

--- SpeechService/SpeechProviders/WebSpeechProviderFilters.cs
+++ SpeechService/SpeechProviders/WebSpeechProviderFilters.cs
@@ -7,6 +7,12 @@ namespace EddiSpeechService.SpeechProviders
 {
     internal static class WebSpeechProviderFilters
     {
+        /// <summary>
+        /// The voice is allowed if any of its supported locales match any of the locale filters, if no locale filters are specified, or if the voice has no supported locales.
+        /// </summary>
+        /// <param name="voice"></param>
+        /// <param name="localeFilters"></param>
+        /// <returns></returns>
         internal static bool IsVoiceAllowed ( VoiceDetails voice, IEnumerable<string> localeFilters )
         {
             var filters = localeFilters?
@@ -21,9 +27,11 @@ namespace EddiSpeechService.SpeechProviders
 
             var locales = voice.supportedLocales?.Count > 0
                 ? voice.supportedLocales
-                : [ voice.culturecode ];
+                : voice.culturecode != null
+                    ? [ voice.culturecode ]
+                    : [];
 
-            return locales.Any( locale => filters.Any( filter => LocaleMatches( locale, filter ) ) );
+            return locales.Count == 0 || locales.Any( locale => filters.Any( filter => LocaleMatches( locale, filter ) ) );
         }
 
         private static bool LocaleMatches ( string locale, string filter )
